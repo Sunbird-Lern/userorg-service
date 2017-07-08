fda1c4aba1d593cb61d1318507868115a13be6a4
vijethas <vijetha.nayak@riflexions.com>
2017-07-08 19:48:35 +0530

    Issue #330 feat:Initial Organization API changes for testing

--- app/controllers/BaseController.java
+++ app/controllers/BaseController.java
@@ -1,4 +1,5 @@
 package controllers;
+
 import java.text.MessageFormat;
 
 import org.sunbird.common.exception.ProjectCommonException;
@@ -25,135 +26,145 @@ import play.mvc.Result;
 import play.mvc.Results;
 import util.AuthenticationHelper;
 import util.Global;
+
 /**
- * This controller we can use for writing some
- * common method.
+ * This controller we can use for writing some common method.
+ * 
  * @author Manzarul
  */
 public class BaseController extends Controller {
-	private static final LogHelper logger = LogHelper.getInstance(BaseController.class.getName());
-	public static final int Akka_wait_time =6;
-	private static ActorSelection selection=null;
-	static {
-		ActorSystem system = ActorSystem.create("ActorApplication", ConfigFactory.load().getConfig("ActorConfig"));
-		String path = "akka.tcp://RemoteMiddlewareSystem@127.0.0.1:8088/user/RequestRouterActor";
-		try {
-			path = play.Play.application().configuration().getString("remote.actor.path");
-			if (!ProjectUtil.isStringNullOREmpty(System.getenv(JsonKey.SUNBIRD_ACTOR_IP))
-					&& !ProjectUtil.isStringNullOREmpty(System.getenv(JsonKey.SUNBIRD_ACTOR_PORT))) {
-			  logger.debug("value is taking from system evn");
-				path = MessageFormat.format(play.Play.application().configuration().getString("remote.actor.env.path"),
-						System.getenv(JsonKey.SUNBIRD_ACTOR_IP), System.getenv(JsonKey.SUNBIRD_ACTOR_PORT));
-			}
-			
-			 logger.debug("Actor path is ==" + path);
-			
-		} catch (Exception e) {
+  private static final LogHelper logger = LogHelper.getInstance(BaseController.class.getName());
+  public static final int Akka_wait_time = 6;
+  private static ActorSelection selection = null;
+  static {
+    ActorSystem system =
+        ActorSystem.create("ActorApplication", ConfigFactory.load().getConfig("ActorConfig"));
+    String path = "akka.tcp://RemoteMiddlewareSystem@127.0.1.1:8088/user/RequestRouterActor";
+    try {
+      path = play.Play.application().configuration().getString("remote.actor.path");
+      if (!ProjectUtil.isStringNullOREmpty(System.getenv(JsonKey.SUNBIRD_ACTOR_IP))
+          && !ProjectUtil.isStringNullOREmpty(System.getenv(JsonKey.SUNBIRD_ACTOR_PORT))) {
+          logger.debug("value is taking from system evn");
+        path = MessageFormat.format(
+            play.Play.application().configuration().getString("remote.actor.env.path"),
+            System.getenv(JsonKey.SUNBIRD_ACTOR_IP), System.getenv(JsonKey.SUNBIRD_ACTOR_PORT));
+      }
+       logger.debug("Actor path is ==" + path);
+    } catch (Exception e) {
 
-		}
+    }
 
-		selection = system.actorSelection(path);
-	}
-	
-	/**
-	 * This method will provide remote Actor selection
-	 * @return
-	 */
-	public ActorSelection getRemoteActor() {
-		return selection;
-	}
-	/**
-	 * This method will create failure response
-	 * @param request Request
-	 * @param code ResponseCode
-	 * @param headerCode ResponseCode
-	 * @return Response
-	 */
-	public static Response createFailureResponse(Request request,ResponseCode code , ResponseCode headerCode) {
-		Response response = new Response();
-		response.setVer(getApiVersion(request.path()));
-		response.setId(getApiResponseId(request));
-		response.setTs(ProjectUtil.getFormattedDate());
-		response.setResponseCode(headerCode);
-		response.setParams(createResponseParamObj(code));
-		return response;
-	}
-   
-    /**
-     * This method will create response param 
-     * @param code ResponseCode
-     * @return ResponseParams
-     */
-	public static ResponseParams createResponseParamObj(ResponseCode code) {
-		ResponseParams params = new ResponseParams();
-		if(code.getResponseCode()!=200) {
-		params.setErr(code.getErrorCode());
-		params.setErrmsg(code.getErrorMessage());
-		}
-		params.setMsgid(ExecutionContext.getRequestId());
-		params.setStatus(ResponseCode.getHeaderResponseCode(code.getResponseCode()).name());
-		return params;
-	}
-	
-	/**
-	 * This method will create data for success response.
-	 * @param request String
-	 * @param response Response
-	 * @return Response
-	 */
-	public static Response createSuccessResponse(play.mvc.Http.Request request, Response response) {
-	    if(request !=null) { 
-		response.setVer(getApiVersion(request.path()));
-	    } else {
-	      response.setVer(""); 
-	    }
-		response.setId(getApiResponseId(request));
-		response.setTs(ProjectUtil.getFormattedDate());
-		ResponseCode code = ResponseCode.getResponse(ResponseCode.success.getErrorCode());
-		code.setResponseCode(ResponseCode.OK.getResponseCode());
-		response.setParams(createResponseParamObj(code));
-		return response;
-	}
-	
-	/**
-	 * This method will provide api version.
-	 * @param request String
-	 * @return String
-	 */
-  public static String getApiVersion(String request) {
-	    return request.split("[/]")[1];
+    selection = system.actorSelection(path);
   }
-  
-  
+
+  /**
+   * This method will provide remote Actor selection
+   * 
+   * @return
+   */
+  public ActorSelection getRemoteActor() {
+    return selection;
+  }
+
+  /**
+   * This method will create failure response
+   * 
+   * @param request Request
+   * @param code ResponseCode
+   * @param headerCode ResponseCode
+   * @return Response
+   */
+  public static Response createFailureResponse(Request request, ResponseCode code,
+      ResponseCode headerCode) {
+    Response response = new Response();
+    response.setVer(getApiVersion(request.path()));
+    response.setId(getApiResponseId(request));
+    response.setTs(ProjectUtil.getFormattedDate());
+    response.setResponseCode(headerCode);
+    response.setParams(createResponseParamObj(code));
+    return response;
+  }
+
+  /**
+   * This method will create response param
+   * 
+   * @param code ResponseCode
+   * @return ResponseParams
+   */
+  public static ResponseParams createResponseParamObj(ResponseCode code) {
+    ResponseParams params = new ResponseParams();
+    if (code.getResponseCode() != 200) {
+      params.setErr(code.getErrorCode());
+      params.setErrmsg(code.getErrorMessage());
+    }
+    params.setMsgid(ExecutionContext.getRequestId());
+    params.setStatus(ResponseCode.getHeaderResponseCode(code.getResponseCode()).name());
+    return params;
+  }
+
+  /**
+   * This method will create data for success response.
+   * 
+   * @param request String
+   * @param response Response
+   * @return Response
+   */
+  public static Response createSuccessResponse(play.mvc.Http.Request request, Response response) {
+    if (request != null) {
+      response.setVer(getApiVersion(request.path()));
+    } else {
+      response.setVer("");
+    }
+    response.setId(getApiResponseId(request));
+    response.setTs(ProjectUtil.getFormattedDate());
+    ResponseCode code = ResponseCode.getResponse(ResponseCode.success.getErrorCode());
+    code.setResponseCode(ResponseCode.OK.getResponseCode());
+    response.setParams(createResponseParamObj(code));
+    return response;
+  }
+
+  /**
+   * This method will provide api version.
+   * 
+   * @param request String
+   * @return String
+   */
+  public static String getApiVersion(String request) {
+    return request.split("[/]")[1];
+  }
+
+
   /**
    * This method will handle response in case of exception
+   * 
    * @param request String
    * @param exception ProjectCommonException
    * @return Response
    */
-	public static Response createResponseOnException(play.mvc.Http.Request request, ProjectCommonException exception) {
-		Response response = new Response();
-		if(request !=null) {
-		  response.setVer(getApiVersion(request.path()));
-		}else {
-		  response.setVer("");
-		}
-		response.setId(getApiResponseId(request));
-		response.setTs(ProjectUtil.getFormattedDate());
-		response.setResponseCode(ResponseCode.getHeaderResponseCode(exception.getResponseCode()));
-		ResponseCode code = ResponseCode.getResponse(exception.getCode());
-		response.setParams(createResponseParamObj(code));
-		return response;
-	}
-	
-	
-	 /**
-     * 
-     * @param path
-     * @param method
-     * @param t
-     * @return
-     */
+  public static Response createResponseOnException(play.mvc.Http.Request request,
+      ProjectCommonException exception) {
+    Response response = new Response();
+    if (request != null) {
+      response.setVer(getApiVersion(request.path()));
+    } else {
+      response.setVer("");
+    }
+    response.setId(getApiResponseId(request));
+    response.setTs(ProjectUtil.getFormattedDate());
+    response.setResponseCode(ResponseCode.getHeaderResponseCode(exception.getResponseCode()));
+    ResponseCode code = ResponseCode.getResponse(exception.getCode());
+    response.setParams(createResponseParamObj(code));
+    return response;
+  }
+
+
+  /**
+   * 
+   * @param path
+   * @param method
+   * @param t
+   * @return
+   */
   public static Response createResponseOnException(String path, String method,
       ProjectCommonException exception) {
     Response response = new Response();
@@ -165,62 +176,64 @@ public class BaseController extends Controller {
     response.setParams(createResponseParamObj(code));
     return response;
   }
-	
-	
-	/**
-	 * This method will create common response for all controller method
-	 * @param response Object
-	 * @return Result
-	 */
-	public Result createCommonResponse(Object response,String key,play.mvc.Http.Request request) {
-		if (response instanceof Response) {
-			Response courseResponse = (Response) response;
-			if(!ProjectUtil.isStringNullOREmpty(key)){
-				Object value = courseResponse.getResult().get(JsonKey.RESPONSE);
-				courseResponse.getResult().remove(JsonKey.RESPONSE);
-				courseResponse.getResult().put(key, value);
-			}
-		    return Results.ok(Json.toJson(BaseController.createSuccessResponse(request, (Response) courseResponse)));
-		} else {
-			 ProjectCommonException exception = (ProjectCommonException) response;
-			 return Results.ok(Json.toJson(BaseController.createResponseOnException(request, exception)));
-		}
-	}
-	
-	
-	/**
-     * Common exception response handler method.
-     * @param e Exception
-     * @return Result
-     */
-    public Result createCommonExceptionResponse (Exception e,play.mvc.Http.Request requerst) {
-        logger.error(e);
-        if(requerst == null) {
-          requerst = request();
-        }
-        ProjectCommonException exception = null;
-        if(e instanceof ProjectCommonException) {
-            exception = (ProjectCommonException) e;
-        }else {
-         exception = new ProjectCommonException(ResponseCode.internalError.getErrorCode(),
-                ResponseCode.internalError.getErrorMessage(), ResponseCode.SERVER_ERROR.getResponseCode());
-        }
-        return Results.ok(Json.toJson(BaseController.createResponseOnException(request(), exception)));
+
+
+  /**
+   * This method will create common response for all controller method
+   * 
+   * @param response Object
+   * @return Result
+   */
+  public Result createCommonResponse(Object response, String key, play.mvc.Http.Request request) {
+    if (response instanceof Response) {
+      Response courseResponse = (Response) response;
+      if (!ProjectUtil.isStringNullOREmpty(key)) {
+        Object value = courseResponse.getResult().get(JsonKey.RESPONSE);
+        courseResponse.getResult().remove(JsonKey.RESPONSE);
+        courseResponse.getResult().put(key, value);
+      }
+      return Results.ok(
+          Json.toJson(BaseController.createSuccessResponse(request, (Response) courseResponse)));
+    } else {
+      ProjectCommonException exception = (ProjectCommonException) response;
+      return Results.ok(Json.toJson(BaseController.createResponseOnException(request, exception)));
     }
-	
-    
-   
-    
-    
-    
-	/**
-	 * This method will make a call to Akka actor and return promise.
-	 * @param selection ActorSelection
-	 * @param request Request
-	 * @param timeout Timeout
-	 * @param responseKey String
-	 * @return Promise<Result>
-	 */
+  }
+
+
+  /**
+   * Common exception response handler method.
+   * 
+   * @param e Exception
+   * @return Result
+   */
+  public Result createCommonExceptionResponse(Exception e, play.mvc.Http.Request requerst) {
+    logger.error(e);
+    if (requerst == null) {
+      requerst = request();
+    }
+    ProjectCommonException exception = null;
+    if (e instanceof ProjectCommonException) {
+      exception = (ProjectCommonException) e;
+    } else {
+      exception = new ProjectCommonException(ResponseCode.internalError.getErrorCode(),
+          ResponseCode.internalError.getErrorMessage(),
+          ResponseCode.SERVER_ERROR.getResponseCode());
+    }
+    return Results.ok(Json.toJson(BaseController.createResponseOnException(request(), exception)));
+  }
+
+
+
+  /**
+   * This method will make a call to Akka actor and return promise.
+   * 
+   * @param selection ActorSelection
+   * @param request Request
+   * @param timeout Timeout
+   * @param responseKey String
+   * @return Promise<Result>
+   */
   public Promise<Result> actorResponseHandler(ActorSelection selection,
       org.sunbird.common.request.Request request, Timeout timeout, String responseKey,
       play.mvc.Http.Request httpReq) {
@@ -241,28 +254,29 @@ public class BaseController extends Controller {
     return res;
   }
 
-	/**
-	 * This method will provide environment id.
-	 * @return int
-	 */
-	public int getEnvironment() {
-		if (Global.env != null) {
-			return Global.env.getValue();
-		}
-		return ProjectUtil.Environment.prod.getValue();
-	}
-	
-	public String getUserIdByAuthToken(String token){
-		return AuthenticationHelper.verifyUserAccesToken(token);
-		
-	}
+  /**
+   * This method will provide environment id.
+   * 
+   * @return int
+   */
+  public int getEnvironment() {
+    if (Global.env != null) {
+      return Global.env.getValue();
+    }
+    return ProjectUtil.Environment.prod.getValue();
+  }
 
-	
-/**
- * 	
- * @param requerst play.mvc.Http.Request
- * @return 
- */
+  public String getUserIdByAuthToken(String token) {
+    return AuthenticationHelper.verifyUserAccesToken(token);
+
+  }
+
+
+  /**
+   * 
+   * @param requerst play.mvc.Http.Request
+   * @return
+   */
   private static String getApiResponseId(play.mvc.Http.Request requerst) {
     String val = "";
     if (requerst != null) {
@@ -281,12 +295,12 @@ public class BaseController extends Controller {
     return val;
   }
 
-  
+
   /**
-   *  
+   * 
    * @param path String
    * @param method String
-   * @return  Stirng
+   * @return Stirng
    */
   private static String getApiResponseId(String path, String method) {
     String val = "";
@@ -302,12 +316,12 @@ public class BaseController extends Controller {
     }
     return val;
   }
-     
-/**
- * 	
- * @param splited String []
- * @return String 
- */
+
+  /**
+   * 
+   * @param splited String []
+   * @return String
+   */
   private static String removelastvalue(String splited[]) {
     StringBuilder builder = new StringBuilder();
     if (splited != null && splited.length > 0) {
@@ -317,4 +331,5 @@ public class BaseController extends Controller {
     }
     return builder.toString();
   }
+
 }
--- app/util/Global.java
+++ app/util/Global.java
@@ -32,156 +32,156 @@ import play.mvc.Results;
  *
  */
 public class Global extends GlobalSettings {
-	private LogHelper logger = LogHelper.getInstance(Global.class.getName());
-	public static ProjectUtil.Environment env;
-	private static ConcurrentHashMap<String , Short> apiHeaderIgnoreMap = new ConcurrentHashMap<>();
-	public static Map<String,String> apiMap = new HashMap<>(); 
-	
-	 private class ActionWrapper extends Action.Simple {
-		    public ActionWrapper(Action<?> action) {
-		      this.delegate = action;
-		    }
-
-			@Override
-		    public Promise<Result> call(Http.Context ctx) throws java.lang.Throwable {
-		      Promise<Result> result = this.delegate.call(ctx);
-		      Http.Response response = ctx.response();
-		      response.setHeader("Access-Control-Allow-Origin", "*");
-		      String message = verifyRequestData(ctx.request(),RequestMethod.GET.name());
-				if (!ProjectUtil.isStringNullOREmpty(message)) {
-					result = onDataValidationError(ctx.request(), message);
-				} else {
-					result = delegate.call(ctx);
-				}
-		      return result;
-		    }
-		  }
-
-	
-	/**
-	 * 
-	 * @author Manzarul
-	 *
-	 */
-	public enum RequestMethod {
-		GET,POST,PUT,DELETE;
-	}
-	/**
-	 * This method will be called on application start up.
-	 * it will be called only time in it's life cycle.
-	 * @param app Application
-	 */
-	public void onStart(Application app) {
-		   setEnvironment();
-		   addApiListToMap();
-		   createApiMap();
-		logger.info("Server started.. with Environment --" + env.name());
-	}
+    private LogHelper logger = LogHelper.getInstance(Global.class.getName());
+    public static ProjectUtil.Environment env;
+    private static ConcurrentHashMap<String , Short> apiHeaderIgnoreMap = new ConcurrentHashMap<>();
+    public static Map<String,String> apiMap = new HashMap<>(); 
     
-	/**
-	 * This method will be called on each request.
-	 * @param request Request
-	 * @param actionMethod Method
-	 * @return Action
-	 */
-	@SuppressWarnings("rawtypes")
-	public Action onRequest(Request request, Method actionMethod) {
-		String messageId = request.getHeader(JsonKey.MESSAGE_ID);
-		logger.info("method call start.." + request.path() + " " + actionMethod + " " + messageId);
-		if (ProjectUtil.isStringNullOREmpty(messageId)) {
-			UUID uuid = UUID.randomUUID();
-			messageId = uuid.toString();
-			logger.info("message id is not provided by client.." + messageId);
-		}
-		ExecutionContext.setRequestId(messageId);
-		return new ActionWrapper(super.onRequest(request, actionMethod));
-	}
-	
-	
-	 /**
-	  *This method will do request data validation for GET method only.
+     private class ActionWrapper extends Action.Simple {
+            public ActionWrapper(Action<?> action) {
+              this.delegate = action;
+            }
+
+            @Override
+            public Promise<Result> call(Http.Context ctx) throws java.lang.Throwable {
+              Promise<Result> result = this.delegate.call(ctx);
+              Http.Response response = ctx.response();
+              response.setHeader("Access-Control-Allow-Origin", "*");
+              String message = verifyRequestData(ctx.request(),RequestMethod.GET.name());
+                if (!ProjectUtil.isStringNullOREmpty(message)) {
+                    result = onDataValidationError(ctx.request(), message);
+                } else {
+                    result = delegate.call(ctx);
+                }
+              return result;
+            }
+          }
+
+    
+    /**
+     * 
+     * @author Manzarul
+     *
+     */
+    public enum RequestMethod {
+        GET,POST,PUT,DELETE;
+    }
+    /**
+     * This method will be called on application start up.
+     * it will be called only time in it's life cycle.
+     * @param app Application
+     */
+    public void onStart(Application app) {
+           setEnvironment();
+           addApiListToMap();
+           createApiMap();
+        logger.info("Server started.. with Environment --" + env.name());
+    }
+    
+    /**
+     * This method will be called on each request.
+     * @param request Request
+     * @param actionMethod Method
+     * @return Action
+     */
+    @SuppressWarnings("rawtypes")
+    public Action onRequest(Request request, Method actionMethod) {
+        String messageId = request.getHeader(JsonKey.MESSAGE_ID);
+        logger.info("method call start.." + request.path() + " " + actionMethod + " " + messageId);
+        if (ProjectUtil.isStringNullOREmpty(messageId)) {
+            UUID uuid = UUID.randomUUID();
+            messageId = uuid.toString();
+            logger.info("message id is not provided by client.." + messageId);
+        }
+        ExecutionContext.setRequestId(messageId);
+        return new ActionWrapper(super.onRequest(request, actionMethod));
+    }
+    
+    
+     /**
+      *This method will do request data validation for GET method only.
      * As a GET request user must send some key in header.
      */
       public Promise<Result> onDataValidationError(Request request,String errorMessage) {
-    	  logger.info("Data error found--");
-    	  ResponseCode code = ResponseCode.getResponse(errorMessage);
-    	  ResponseCode headerCode = ResponseCode.CLIENT_ERROR;
-    	  Response resp = BaseController.createFailureResponse(request, code,headerCode);
-	  return   Promise.<Result>pure(Results.ok(Json.toJson(resp)));
+          logger.info("Data error found--");
+          ResponseCode code = ResponseCode.getResponse(errorMessage);
+          ResponseCode headerCode = ResponseCode.CLIENT_ERROR;
+          Response resp = BaseController.createFailureResponse(request, code,headerCode);
+      return   Promise.<Result>pure(Results.ok(Json.toJson(resp)));
       }
-	
-	
-	 /**
+    
+    
+     /**
      * This method will be used to send the request header missing error message.
      */
     @Override
     public Promise<Result> onError(Http.RequestHeader request, Throwable t) {
-    	Response response = null;
-    	if(t instanceof ProjectCommonException){
-    		response = BaseController.createResponseOnException(request.path(),request.method(), (ProjectCommonException)t);
-    	}else {
-    	 ProjectCommonException	exception = new ProjectCommonException(ResponseCode.internalError.getErrorCode(),
-    				ResponseCode.internalError.getErrorMessage(), ResponseCode.SERVER_ERROR.getResponseCode());
-    	 response = BaseController.createResponseOnException(request.path(),request.method(), exception);
-    	}
-	  return   Promise.<Result>pure(Results.internalServerError(Json.toJson(response)));
+        Response response = null;
+        if(t instanceof ProjectCommonException){
+            response = BaseController.createResponseOnException(request.path(),request.method(), (ProjectCommonException)t);
+        }else {
+         ProjectCommonException exception = new ProjectCommonException(ResponseCode.internalError.getErrorCode(),
+                    ResponseCode.internalError.getErrorMessage(), ResponseCode.SERVER_ERROR.getResponseCode());
+         response = BaseController.createResponseOnException(request.path(),request.method(), exception);
+        }
+      return   Promise.<Result>pure(Results.internalServerError(Json.toJson(response)));
     }
-	
-	
-	/**
-	 * This method will do the get request header mandatory value check
-	 * it will check all the mandatory value under header , if any value is
-	 * missing then it will send missing key name in response. 
-	 * @param request Request
-	 * @param method String 
-	 * @return String
-	 */
-	@SuppressWarnings("deprecation")
-	private String verifyRequestData(Request request, String method) {
-		if (ProjectUtil.isStringNullOREmpty(request.getHeader(HeaderParam.X_Consumer_ID.getName().toLowerCase()))) {
-			return ResponseCode.customerIdRequired.getErrorCode();
-		} else if (ProjectUtil.isStringNullOREmpty(request.getHeader(HeaderParam.X_Device_ID.getName().toLowerCase()))) {
-			return ResponseCode.deviceIdRequired.getErrorCode();
-		} else if (ProjectUtil.isStringNullOREmpty(request.getHeader(HeaderParam.ts.getName()))) {
-			return ResponseCode.timeStampRequired.getErrorCode();
-		} else if (!apiHeaderIgnoreMap.containsKey(request.path())) {
-			if (ProjectUtil.isStringNullOREmpty(request.getHeader(HeaderParam.X_Authenticated_Userid.getName()))) {
-				return ResponseCode.authTokenRequired.getErrorCode();
-			}
-			String userId = AuthenticationHelper.verifyUserAccesToken(request.getHeader(HeaderParam.X_Authenticated_Userid.getName()));
-			if (ProjectUtil.isStringNullOREmpty(userId)) {
-				return ResponseCode.invalidAuthToken.getErrorCode();
-			} 
-		}
-		return "";
-	}
-	
-	/**
-	 * This method will identify the environment and 
-	 * update with enum.
-	 * @return Environment
-	 */
-	public Environment setEnvironment() {
-		if (play.Play.isDev()) {
-			return env = Environment.dev;
-		} else if (play.Play.isTest()) {
-			return env = Environment.qa;
-		} else {
-			return env = Environment.prod;
-		}
-	}
-	/**
-	 * 
-	 */
-	private void addApiListToMap() {
-		short var =1;
-		apiHeaderIgnoreMap.put("/v1/user/create", var);
-		apiHeaderIgnoreMap.put("/v1/user/login", var);
-	}
+    
+    
+    /**
+     * This method will do the get request header mandatory value check
+     * it will check all the mandatory value under header , if any value is
+     * missing then it will send missing key name in response. 
+     * @param request Request
+     * @param method String 
+     * @return String
+     */
+    @SuppressWarnings("deprecation")
+    private String verifyRequestData(Request request, String method) {
+        if (ProjectUtil.isStringNullOREmpty(request.getHeader(HeaderParam.X_Consumer_ID.getName().toLowerCase()))) {
+            return ResponseCode.customerIdRequired.getErrorCode();
+        } else if (ProjectUtil.isStringNullOREmpty(request.getHeader(HeaderParam.X_Device_ID.getName().toLowerCase()))) {
+            return ResponseCode.deviceIdRequired.getErrorCode();
+        } else if (ProjectUtil.isStringNullOREmpty(request.getHeader(HeaderParam.ts.getName()))) {
+            return ResponseCode.timeStampRequired.getErrorCode();
+        } else if (!apiHeaderIgnoreMap.containsKey(request.path())) {
+            if (ProjectUtil.isStringNullOREmpty(request.getHeader(HeaderParam.X_Authenticated_Userid.getName()))) {
+                return ResponseCode.authTokenRequired.getErrorCode();
+            }
+            String userId = AuthenticationHelper.verifyUserAccesToken(request.getHeader(HeaderParam.X_Authenticated_Userid.getName()));
+            if (ProjectUtil.isStringNullOREmpty(userId)) {
+                return ResponseCode.invalidAuthToken.getErrorCode();
+            } 
+        }
+        return "";
+    }
+    
+    /**
+     * This method will identify the environment and 
+     * update with enum.
+     * @return Environment
+     */
+    public Environment setEnvironment() {
+        if (play.Play.isDev()) {
+            return env = Environment.dev;
+        } else if (play.Play.isTest()) {
+            return env = Environment.qa;
+        } else {
+            return env = Environment.prod;
+        }
+    }
+    /**
+     * 
+     */
+    private void addApiListToMap() {
+        short var =1;
+        apiHeaderIgnoreMap.put("/v1/user/create", var);
+        apiHeaderIgnoreMap.put("/v1/user/login", var);
+    }
 
  /**
   * This method will create api url and id for the url.
-  * this id we will use while sending api response to client.	
+  * this id we will use while sending api response to client.   
   */
   private static void createApiMap() {
     apiMap.put("/v1/user/courses/enroll", "api.course.enroll");
@@ -202,6 +202,8 @@ public class Global extends GlobalSettings {
     apiMap.put("/v1/course/create", "api.user.create");
     apiMap.put("/v1/org/create", "api.org.create");
     apiMap.put("/v1/org/update", "api.org.update");
+    apiMap.put("/v1/org/updateStatus", "api.org.update.status");
+    apiMap.put("/v1/org/approve", "api.org.approve");
     apiMap.put("/v1/org/read", "api.org.read");
     apiMap.put("/v1/page/create", "api.page.create");
     apiMap.put("/v1/page/update", "api.page.update");
