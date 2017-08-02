119dd78b8caccdccaeb7443a775208d88066af2e
arvindyadav108 <arvind.glaitm108@gmail.com>
2017-08-02 18:25:31 +0530

    Issue #1 feat: merge

--- app/controllers/bulkapimanagement/BulkUserUploadController.java
+++ app/controllers/bulkapimanagement/BulkUserUploadController.java
@@ -76,5 +76,56 @@ public class BulkUserUploadController extends BaseController {
     }
   }
 
+  /**
+   * This method will allow to upload bulk organisation.
+   *
+   * @return Promise<Result>
+   */
+  public Promise<Result> uploadOrg1() {
+
+    try {
+      JsonNode requestData = request().body().asJson();
+      ProjectLogger.log("getting bulk organisation upload data request = " + requestData,
+          LoggerEnum.INFO.name());
+      Request reqObj = (Request) mapper.RequestMapper.mapRequest(requestData, Request.class);
+      RequestValidator.validateUploadUser(reqObj);
+      reqObj.setOperation(ActorOperations.BULK_UPLOAD.getValue());
+      reqObj.setRequest_id(ExecutionContext.getRequestId());
+      reqObj.setEnv(getEnvironment());
+      HashMap<String, Object> map = new HashMap<>();
+      map.put(JsonKey.DATA, reqObj.getRequest());
+      reqObj.getRequest().put(JsonKey.OBJECT_TYPE, JsonKey.ORGANISATION);
+      reqObj.getRequest().put(JsonKey.CREATED_BY,
+          getUserIdByAuthToken(request().getHeader(HeaderParam.X_Authenticated_Userid.getName())));
+      reqObj.setRequest(map);
+
+      MultipartFormData body = request().body().asMultipartFormData();
+      File file = null;
+      if (body != null) {
+        List<FilePart> filePart = body.getFiles();
+        file = new File("BULK_FILE");
+        InputStream is = new FileInputStream(filePart.get(0).getFile());
+        byte[] byteArray = IOUtils.toByteArray(is);
+        FileUtils.writeByteArrayToFile(file, byteArray);
+      } else{
+        //read data as string from request
+      }
+      reqObj.getRequest().put(JsonKey.FILE, file);
+
+
+      Timeout timeout = new Timeout(300, TimeUnit.SECONDS);
+      return actorResponseHandler(getRemoteActor(), reqObj, timeout, null, request());
+    } catch (Exception e) {
+      return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
+    }
+  }
+
+
+  public Result uploadOrg() {
+
+    
+
+    return badRequest("Expecting text/plain request body");
+  }
 
 }
