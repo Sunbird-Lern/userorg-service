c5f3b22c71600575e72d954f364e301dea481958
vijethas <vijetha.nayak@riflexions.com>
2017-08-07 17:03:04 +0530

    Issue #393 feat:Org-Admin consumption,Course dashboard api mockup

---
+++ app/controllers/metrics/UserMetricsController.java
@@ -0,0 +1,54 @@
+package controllers.metrics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.sunbird.common.models.util.ActorOperations;
+import org.sunbird.common.request.ExecutionContext;
+import org.sunbird.common.request.Request;
+
+import akka.util.Timeout;
+import controllers.BaseController;
+import play.libs.F.Promise;
+import play.mvc.Result;
+
+public class UserMetricsController extends BaseController {
+  
+  public Promise<Result> userCreation() {
+    try {
+      Map<String, Object> map = new HashMap<>();
+      Request request = new Request();
+      request.setEnv(getEnvironment());
+      request.setRequest(map);
+      request.setOperation(ActorOperations.USER_CREATION_METRICS.getValue());
+      request.setRequest(map);
+      Timeout timeout = new Timeout(Akka_wait_time, TimeUnit.SECONDS);
+      request.setRequest_id(ExecutionContext.getRequestId());
+      Promise<Result> res =
+          actorResponseHandler(getRemoteActor(), request, timeout, null, request());
+      return res;
+    } catch (Exception e) {
+      return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
+    }
+  }
+  
+  public Promise<Result> userConsumption() {
+    try {
+      Map<String, Object> map = new HashMap<>();
+      Request request = new Request();
+      request.setEnv(getEnvironment());
+      request.setRequest(map);
+      request.setOperation(ActorOperations.USER_CONSUMPTION_METRICS.getValue());
+      request.setRequest(map);
+      Timeout timeout = new Timeout(Akka_wait_time, TimeUnit.SECONDS);
+      request.setRequest_id(ExecutionContext.getRequestId());
+      Promise<Result> res =
+          actorResponseHandler(getRemoteActor(), request, timeout, null, request());
+      return res;
+    } catch (Exception e) {
+      return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
+    }
+  }
+
+}
