02a23e0aa1e5ed85ca2971d3b26d56704220b3c6
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-09-08 16:47:34 +0530

    Merge branch 'beta'

--- service/app/controllers/metrics/CourseMetricsController.java
+++ service/app/controllers/metrics/CourseMetricsController.java
@@ -1,16 +1,14 @@
 package controllers.metrics;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.request.ExecutionContext;
+import org.sunbird.common.request.HeaderParam;
 import org.sunbird.common.request.Request;
-
-import com.fasterxml.jackson.databind.JsonNode;
 
 import akka.util.Timeout;
 import controllers.BaseController;
@@ -18,20 +16,17 @@ import play.libs.F.Promise;
 import play.mvc.Result;
 
 public class CourseMetricsController extends BaseController {
-  
-  @SuppressWarnings("unchecked")
-  public Promise<Result> courseProgress() {
+
+  public Promise<Result> courseProgress(String batchId) {
     try {
+      String periodStr = request().getQueryString("period");
       Map<String, Object> map = new HashMap<>();
-      JsonNode requestData = request().body().asJson();
-      Request reqObj = (Request) mapper.RequestMapper.mapRequest(requestData, Request.class);
       Request request = new Request();
       request.setEnv(getEnvironment());
-      Map<String,Object> data = (Map<String,Object>)reqObj.getRequest().get(JsonKey.FILTER);
-      List<Map<String,Object>> dataList = (List<Map<String,Object>>) data.get(JsonKey.COURSE);
-      data = dataList.get(0);
-      map.put(JsonKey.COURSE_ID, data.get(JsonKey.COURSE_ID));
-      map.put(JsonKey.PERIOD, reqObj.getRequest().get(JsonKey.PERIOD));
+      map.put(JsonKey.BATCH_ID, batchId);
+      map.put(JsonKey.PERIOD, periodStr);
+      map.put(JsonKey.REQUESTED_BY,
+          getUserIdByAuthToken(request().getHeader(HeaderParam.X_Authenticated_Userid.getName())));
       request.setRequest(map);
       request.setOperation(ActorOperations.COURSE_PROGRESS_METRICS.getValue());
       request.setRequest(map);
@@ -44,21 +39,18 @@ public class CourseMetricsController extends BaseController {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
     }
   }
-  
-  @SuppressWarnings("unchecked")
-  public Promise<Result> courseCreation() {
+
+  public Promise<Result> courseCreation(String courseId) {
     try {
+      String periodStr = request().getQueryString("period");
       Map<String, Object> map = new HashMap<>();
-      JsonNode requestData = request().body().asJson();
-      Request reqObj = (Request) mapper.RequestMapper.mapRequest(requestData, Request.class);
       Request request = new Request();
       request.setEnv(getEnvironment());
       request.setOperation(ActorOperations.COURSE_CREATION_METRICS.getValue());
-      Map<String,Object> data = (Map<String,Object>)reqObj.getRequest().get(JsonKey.FILTER);
-      List<Map<String,Object>> dataList = (List<Map<String,Object>>) data.get(JsonKey.COURSE);
-      data = dataList.get(0);
-      map.put(JsonKey.COURSE_ID, data.get(JsonKey.COURSE_ID));
-      map.put(JsonKey.PERIOD, reqObj.getRequest().get(JsonKey.PERIOD));
+      map.put(JsonKey.COURSE_ID, courseId);
+      map.put(JsonKey.PERIOD, periodStr);
+      map.put(JsonKey.REQUESTED_BY,
+          getUserIdByAuthToken(request().getHeader(HeaderParam.X_Authenticated_Userid.getName())));
       request.setRequest(map);
       Timeout timeout = new Timeout(Akka_wait_time, TimeUnit.SECONDS);
       request.setRequest_id(ExecutionContext.getRequestId());
@@ -69,7 +61,52 @@ public class CourseMetricsController extends BaseController {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
     }
   }
-  
-  
+
+  public Promise<Result> courseProgressReport(String batchId) {
+    try {
+      String periodStr = request().getQueryString(JsonKey.PERIOD);
+      String reportType = request().getQueryString(JsonKey.FORMAT);
+      Map<String, Object> map = new HashMap<>();
+      Request request = new Request();
+      request.setEnv(getEnvironment());
+      map.put(JsonKey.BATCH_ID, batchId);
+      map.put(JsonKey.PERIOD, periodStr);
+      map.put(JsonKey.FORMAT , reportType);
+      map.put(JsonKey.REQUESTED_BY,
+          getUserIdByAuthToken(request().getHeader(HeaderParam.X_Authenticated_Userid.getName())));
+      request.setRequest(map);
+      request.setOperation(ActorOperations.COURSE_PROGRESS_METRICS_REPORT.getValue());
+      request.setRequest(map);
+      Timeout timeout = new Timeout(Akka_wait_time, TimeUnit.SECONDS);
+      request.setRequest_id(ExecutionContext.getRequestId());
+      Promise<Result> res =
+          actorResponseHandler(getRemoteActor(), request, timeout, null, request());
+      return res;
+    } catch (Exception e) {
+      return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
+    }
+  }
+
+  public Promise<Result> courseCreationReport(String courseId) {
+    try {
+      String periodStr = request().getQueryString("period");
+      Map<String, Object> map = new HashMap<>();
+      Request request = new Request();
+      request.setEnv(getEnvironment());
+      request.setOperation(ActorOperations.COURSE_CREATION_METRICS_REPORT.getValue());
+      map.put(JsonKey.COURSE_ID, courseId);
+      map.put(JsonKey.PERIOD, periodStr);
+      request.setRequest(map);
+      Timeout timeout = new Timeout(Akka_wait_time, TimeUnit.SECONDS);
+      request.setRequest_id(ExecutionContext.getRequestId());
+      Promise<Result> res =
+          actorResponseHandler(getRemoteActor(), request, timeout, null, request());
+      return res;
+    } catch (Exception e) {
+      return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
+    }
+  }
+
+
 
 }
--- service/app/controllers/metrics/OrganisationMetricsController.java
+++ service/app/controllers/metrics/OrganisationMetricsController.java
@@ -6,7 +6,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.request.ExecutionContext;
+import org.sunbird.common.request.HeaderParam;
 import org.sunbird.common.request.Request;
 
 import akka.util.Timeout;
@@ -17,6 +19,7 @@ import play.mvc.Result;
 public class OrganisationMetricsController extends BaseController {
 
   public Promise<Result> orgCreation(String orgId) {
+    ProjectLogger.log("Start Org Metrics Creation Contoller");
     try {
       String periodStr = request().getQueryString("period");
       Map<String, Object> map = new HashMap<>();
@@ -31,6 +34,7 @@ public class OrganisationMetricsController extends BaseController {
       request.setRequest_id(ExecutionContext.getRequestId());
       Promise<Result> res =
           actorResponseHandler(getRemoteActor(), request, timeout, null, request());
+      ProjectLogger.log("Return from Org Metrics Creation Contoller");
       return res;
     } catch (Exception e) {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
@@ -38,6 +42,7 @@ public class OrganisationMetricsController extends BaseController {
   }
   
   public Promise<Result> orgConsumption(String orgId) {
+    ProjectLogger.log("Start Org Metrics Consumption Contoller");
     try {
       String periodStr = request().getQueryString("period");
       Map<String, Object> map = new HashMap<>();
@@ -52,6 +57,59 @@ public class OrganisationMetricsController extends BaseController {
       request.setRequest_id(ExecutionContext.getRequestId());
       Promise<Result> res =
           actorResponseHandler(getRemoteActor(), request, timeout, null, request());
+      ProjectLogger.log("Return from Org Metrics Consumption Contoller");
+      return res;
+    } catch (Exception e) {
+      return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
+    }
+  }
+
+  public Promise<Result> orgCreationReport(String orgId) {
+    ProjectLogger.log("Start Org Metrics Creation Contoller");
+    try {
+      String periodStr = request().getQueryString("period");
+      String format = request().getQueryString(JsonKey.FORMAT);
+      Map<String, Object> map = new HashMap<>();
+      map.put(JsonKey.ORG_ID, orgId);
+      map.put(JsonKey.PERIOD, periodStr);
+      map.put(JsonKey.FORMAT, format);
+      map.put(JsonKey.REQUESTED_BY,
+          getUserIdByAuthToken(request().getHeader(HeaderParam.X_Authenticated_Userid.getName())));
+      Request request = new Request();
+      request.setEnv(getEnvironment());
+      request.setOperation(ActorOperations.ORG_CREATION_METRICS_REPORT.getValue());
+      request.setRequest(map);
+      Timeout timeout = new Timeout(Akka_wait_time, TimeUnit.SECONDS);
+      request.setRequest_id(ExecutionContext.getRequestId());
+      Promise<Result> res =
+          actorResponseHandler(getRemoteActor(), request, timeout, null, request());
+      ProjectLogger.log("Return from Org Metrics Creation Contoller");
+      return res;
+    } catch (Exception e) {
+      return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
+    }
+  }
+
+  public Promise<Result> orgConsumptionReport(String orgId) {
+    ProjectLogger.log("Start Org Metrics Consumption Contoller");
+    try {
+      String periodStr = request().getQueryString("period");
+      String format = request().getQueryString(JsonKey.FORMAT);
+      Map<String, Object> map = new HashMap<>();
+      map.put(JsonKey.ORG_ID, orgId);
+      map.put(JsonKey.PERIOD, periodStr);
+      map.put(JsonKey.FORMAT, format);
+      map.put(JsonKey.REQUESTED_BY,
+          getUserIdByAuthToken(request().getHeader(HeaderParam.X_Authenticated_Userid.getName())));
+      Request request = new Request();
+      request.setEnv(getEnvironment());
+      request.setOperation(ActorOperations.ORG_CONSUMPTION_METRICS_REPORT.getValue());
+      request.setRequest(map);
+      Timeout timeout = new Timeout(Akka_wait_time, TimeUnit.SECONDS);
+      request.setRequest_id(ExecutionContext.getRequestId());
+      Promise<Result> res =
+          actorResponseHandler(getRemoteActor(), request, timeout, null, request());
+      ProjectLogger.log("Return from Org Metrics Consumption Contoller");
       return res;
     } catch (Exception e) {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
