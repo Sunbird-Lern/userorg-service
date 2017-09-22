1f898de8d7774934ecb5a04b2767a646d4a378c4
AMIT KUMAR <amit.kumar@tarento.com>
2017-09-22 16:01:04 +0530

    Issue #SB-39 feat : change for id value in response for v1/user/getuser uri

--- service/app/util/Global.java
+++ service/app/util/Global.java
@@ -298,7 +298,7 @@ public class Global extends GlobalSettings {
     apiMap.put("/v1/assessment/update", "api.assessment.save");
     apiMap.put("/v1/assessment/result/read", "api.assessment.result");
     apiMap.put("/v1/role/read", "api.role.read");
-    apiMap.put("/v1/user/getuser", "api.role.getuser");
+    apiMap.put("/v1/user/getuser", "api.user.getuser");
     apiMap.put("/v1/user/block", "api.user.inactive");
     apiMap.put("/v1/user/unblock", "api.user.active");
     apiMap.put("/v1/user/assign/role", "api.user.assign.role");
