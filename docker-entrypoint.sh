94e2a40b076275fec82b61e43d33f8c17a259bbc
manoj v <manojv@ilimi.in>
2017-11-15 17:18:35 +0530

    Issue #0 chore : sleep 60 seconds

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 nohup java  -cp '/home/sunbird/learner/learning-service-1.0-SNAPSHOT/lib/*' play.core.server.ProdServerStart  /home/sunbird/learner/learning-service-1.0-SNAPSHOT &
+sleep 60
 ln -sf /dev/stdout /home/sunbird/learner/logs/learningServiceProject.log
