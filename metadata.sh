b4a96a74bd771baa0a8c61206822ef00bd8e34e4
manoj v <manojv@ilimi.in>
2017-11-29 14:41:19 +0530

    Issue #11 chore : changed from andriod-slave to general-production

--- metadata.sh
+++ metadata.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # return version
-echo '{"name":"learner_service","version":"0.0.2","org":"sunbird","hubuser":"purplesunbird"}'
+echo '{"name":"learner_service","version":"0.0.3","org":"sunbird","hubuser":"purplesunbird"}'
