365c3f5d38fcb977f2a277e96476d65048a1289b
arvindyadav108 <arvind.glaitm108@gmail.com>
2018-03-06 16:23:29 +0530

    Issue # SB-1799 Developement : Create badge issuer.

---
+++ service/app/controllers/badging/BadgeIssuerController.java
@@ -0,0 +1,85 @@
+package controllers.badging;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import controllers.BaseController;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.apache.commons.io.IOUtils;
+import org.apache.tika.Tika;
+import org.sunbird.common.exception.ProjectCommonException;
+import org.sunbird.common.models.util.ActorOperations;
+import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.LoggerEnum;
+import org.sunbird.common.models.util.ProjectLogger;
+import org.sunbird.common.request.BadgeRequestValidator;
+import org.sunbird.common.request.ExecutionContext;
+import org.sunbird.common.request.Request;
+import org.sunbird.common.responsecode.ResponseCode;
+import play.libs.F.Promise;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.mvc.Result;
+
+/**
+ * Created by arvind on 2/3/18.
+ * This controller is for handling the badge issuer apis.
+ */
+public class BadgeIssuerController extends BaseController {
+
+  /**
+   * This method will add badges to user profile.
+   * @return Promise<Result>
+   */
+  public Promise<Result> createBadgeIssuer() {
+    try {
+      Request reqObj = new Request();
+      Map<String, Object> map = new HashMap<>();
+      byte[] byteArray = null;
+      MultipartFormData body = request().body().asMultipartFormData();
+      JsonNode requestData = request().body().asJson();
+      Tika tika = new Tika();
+      ProjectLogger.log("call to create badge issuer api." + requestData, LoggerEnum.INFO.name());
+      if (body != null) {
+        Map<String, String[]> data = body.asFormUrlEncoded();
+        for (Entry<String, String[]> entry : data.entrySet()) {
+          map.put(entry.getKey(), entry.getValue()[0]);
+        }
+        List<FilePart> filePart = body.getFiles();
+        if(filePart != null && !filePart.isEmpty()){
+          File f = filePart.get(0).getFile();
+          String mimeType = tika.detect(f);
+          InputStream is = new FileInputStream(f);
+          byteArray = IOUtils.toByteArray(is);
+          map.put(JsonKey.FILE_NAME , filePart.get(0).getFilename());
+        }
+      }else if (null != requestData) {
+        reqObj =
+            (Request) mapper.RequestMapper.mapRequest(request().body().asJson(), Request.class);
+        map.putAll(reqObj.getRequest());
+      } else {
+        ProjectCommonException e = new ProjectCommonException(
+            ResponseCode.invalidData.getErrorCode(), ResponseCode.invalidData.getErrorMessage(),
+            ResponseCode.CLIENT_ERROR.getResponseCode());
+        return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
+      }
+      map.put(JsonKey.IMAGE, byteArray);
+      reqObj.getRequest().putAll(map);
+      BadgeRequestValidator.validateCreateBadgeIssuer(reqObj);
+      reqObj.setOperation(ActorOperations.CREATE_BADGE_ISSUER.getValue());
+      reqObj.setRequestId(ExecutionContext.getRequestId());
+      reqObj.getRequest().put(JsonKey.CREATED_BY,ctx().flash().get(JsonKey.USER_ID));
+      reqObj.setEnv(getEnvironment());
+      return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
+    } catch (Exception e) {
+      return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
+    }
+  }
+
+}
---
+++ service/test/controllers/badging/BadgeIssuerControllerTest.java
@@ -0,0 +1,116 @@
+package controllers.badging;
+
+import static org.junit.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static play.test.Helpers.route;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import controllers.BaseController;
+import controllers.DummyActor;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.ProjectLogger;
+import org.sunbird.common.request.HeaderParam;
+import play.libs.Json;
+import play.mvc.Http.RequestBuilder;
+import play.mvc.Result;
+import play.test.FakeApplication;
+import play.test.Helpers;
+import util.RequestInterceptor;
+
+/**
+ * Created by arvind on 5/3/18.
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(RequestInterceptor.class)
+@PowerMockIgnore("javax.management.*")
+public class BadgeIssuerControllerTest {
+
+  private static FakeApplication app;
+  private static Map<String,String[]> headerMap;
+  private static ActorSystem system;
+  private static final Props props = Props.create(DummyActor.class);
+
+  @BeforeClass
+  public static void startApp() {
+    app = Helpers.fakeApplication();
+    Helpers.start(app);
+    headerMap = new HashMap<String, String[]>();
+    headerMap.put(HeaderParam.X_Consumer_ID.getName(), new String[]{"Service test consumer"});
+    headerMap.put(HeaderParam.X_Device_ID.getName(), new String[]{"Some Device Id"});
+    headerMap.put(HeaderParam.X_Authenticated_Userid.getName(), new String[]{"Authenticated user id"});
+    headerMap.put(JsonKey.MESSAGE_ID, new String[]{"Unique Message id"});
+
+    system = ActorSystem.create("system");
+    ActorRef subject = system.actorOf(props);
+    BaseController.setActorRef(subject);
+  }
+
+  @Test
+  public void testCreateBadgeIssuer() {
+    PowerMockito.mockStatic(RequestInterceptor.class);
+    when( RequestInterceptor.verifyRequestData(Mockito.anyObject()) ).thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
+    Map<String , Object> requestMap = new HashMap<>();
+    Map<String , Object> innerMap = new HashMap<>();
+    innerMap.put(JsonKey.NAME , "goldBadge");
+    innerMap.put(JsonKey.DESCRIPTION , "golden award");
+    innerMap.put(JsonKey.EMAIL , "abc@gmail.com");
+    innerMap.put(JsonKey.URL , "dummy/url");
+    requestMap.put(JsonKey.REQUEST , innerMap);
+    String data = mapToJson(requestMap);
+
+    JsonNode json = Json.parse(data);
+    RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/issuer/issuers").method("POST");
+    req.headers(headerMap);
+    Result result = route(req);
+    assertEquals(200, result.status());
+  }
+
+  @Test
+  public void testCreateBadgeIssuerInvalidRequestData() {
+    PowerMockito.mockStatic(RequestInterceptor.class);
+    when( RequestInterceptor.verifyRequestData(Mockito.anyObject()) ).thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
+    Map<String , Object> requestMap = new HashMap<>();
+    Map<String , Object> innerMap = new HashMap<>();
+    innerMap.put(JsonKey.NAME , "goldBadge");
+    innerMap.put(JsonKey.DESCRIPTION , "golden award");
+    innerMap.put(JsonKey.EMAIL , "abc@gmail.com");
+    requestMap.put(JsonKey.REQUEST , innerMap);
+    String data = mapToJson(requestMap);
+
+    JsonNode json = Json.parse(data);
+    RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/issuer/issuers").method("POST");
+    req.headers(headerMap);
+    Result result = route(req);
+    assertEquals(400, result.status());
+  }
+
+  private static String mapToJson(Map map){
+    ObjectMapper mapperObj = new ObjectMapper();
+    String jsonResp = "";
+    try {
+      jsonResp = mapperObj.writeValueAsString(map);
+    } catch (IOException e) {
+      ProjectLogger.log(e.getMessage(),e);
+    }
+    return jsonResp;
+  }
+
+}
