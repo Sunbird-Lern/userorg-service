4d9a2110849a3097c1b0b7395751b9dd9f2ed776
AMIT KUMAR <amit.kumar@tarento.com>
2018-03-21 15:27:53 +0530

    Issue #SB-1305 feat:code clean up

--- service/app/controllers/badging/validator/BadgeIssuerRequestValidator.java
+++ service/app/controllers/badging/validator/BadgeIssuerRequestValidator.java
@@ -11,51 +11,55 @@ import org.sunbird.common.responsecode.ResponseCode;
  */
 public class BadgeIssuerRequestValidator {
 
-  private static final int ERROR_CODE = ResponseCode.CLIENT_ERROR.getResponseCode();
-  private static final String COMMA_SEPERATOR = ",";
+    private BadgeIssuerRequestValidator() {}
 
-  /**
-   * Method to validate noteId
-   *
-   * @param request
-   */
-  public static void validateCreateBadgeIssuer(Request request) {
-    StringBuilder builder = new StringBuilder("");
-    Boolean flag = false;
-    if (ProjectUtil.isStringNullOREmpty((String)request.getRequest().get(JsonKey.NAME))) {
-      builder = builder.append(JsonKey.NAME+COMMA_SEPERATOR);
-      flag = true;
-    }
-    if (ProjectUtil.isStringNullOREmpty((String)request.getRequest().get(JsonKey.DESCRIPTION))) {
-      builder = builder.append(JsonKey.DESCRIPTION + COMMA_SEPERATOR);
-      flag = true;
-    }
-    if (ProjectUtil.isStringNullOREmpty((String)request.getRequest().get(JsonKey.URL))) {
-      builder = builder.append(JsonKey.URL + COMMA_SEPERATOR);
-      flag = true;
-    }
-    if (!ProjectUtil.isEmailvalid((String)request.getRequest().get(JsonKey.EMAIL))) {
-      builder = builder.append(JsonKey.EMAIL + COMMA_SEPERATOR);
-      flag = true;
+    private static final int ERROR_CODE = ResponseCode.CLIENT_ERROR.getResponseCode();
+
+    /**
+     * Method to validate noteId
+     *
+     * @param request
+     */
+    public static void validateCreateBadgeIssuer(Request request) {
+        if (ProjectUtil.isStringNullOREmpty((String) request.getRequest().get(JsonKey.NAME))) {
+            throw createExceptionInstance(
+                    ResponseCode.invalidDataForCreateBadgeIssuer.getErrorCode(),
+                    "name is required.");
+        }
+        if (ProjectUtil
+                .isStringNullOREmpty((String) request.getRequest().get(JsonKey.DESCRIPTION))) {
+            throw createExceptionInstance(
+                    ResponseCode.invalidDataForCreateBadgeIssuer.getErrorCode(),
+                    "Description is required.");
+        }
+        if (ProjectUtil.isStringNullOREmpty((String) request.getRequest().get(JsonKey.URL))) {
+            throw createExceptionInstance(
+                    ResponseCode.invalidDataForCreateBadgeIssuer.getErrorCode(),
+                    "url is required.");
+        }
+        if (ProjectUtil.isStringNullOREmpty((String) request.getRequest().get(JsonKey.EMAIL))) {
+            throw createExceptionInstance(
+                    ResponseCode.invalidDataForCreateBadgeIssuer.getErrorCode(),
+                    "email is required.");
+        }
+
+        if (!ProjectUtil.isEmailvalid((String) request.getRequest().get(JsonKey.EMAIL))) {
+            throw createExceptionInstance(
+                    ResponseCode.invalidDataForCreateBadgeIssuer.getErrorCode(),
+                    "email is invalid.");
+        }
     }
 
-    if(flag){
-      builder.deleteCharAt(builder.length()-1);
-      throw createExceptionInstance(ResponseCode.invalidDataForCreateBadgeIssuer.getErrorCode(),builder.toString());
+    public static void validateGetBadgeIssuerDetail(Request request) {
+        if (ProjectUtil.isStringNullOREmpty((String) request.getRequest().get(JsonKey.SLUG))) {
+            throw new ProjectCommonException(ResponseCode.slugRequired.getErrorCode(),
+                    ResponseCode.slugRequired.getErrorMessage(), ERROR_CODE);
+        }
     }
-  }
 
-  public static void validateGetBadgeIssuerDetail(Request request){
-    if(ProjectUtil.isStringNullOREmpty((String)request.getRequest().get(JsonKey.SLUG))){
-      throw new ProjectCommonException(ResponseCode.slugRequired.getErrorCode(),
-          ResponseCode.slugRequired.getErrorMessage(), ERROR_CODE);
+    private static ProjectCommonException createExceptionInstance(String errorCode, String errMsg) {
+        return new ProjectCommonException(ResponseCode.getResponse(errorCode).getErrorCode(),
+                ResponseCode.getResponse(errorCode).getErrorMessage(), ERROR_CODE, errMsg);
     }
-  }
-
-  private static ProjectCommonException createExceptionInstance(String errorCode,
-      String errMsg) {
-    return new ProjectCommonException(ResponseCode.getResponse(errorCode).getErrorCode(),
-        ResponseCode.getResponse(errorCode).getErrorMessage(), ERROR_CODE, errMsg);
-  }
 
 }
