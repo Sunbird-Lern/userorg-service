ad9dca2eb8c7c151a140116c22f88198efd4e073
manzarul <manzarul07@gmail.com>
2018-03-22 23:29:37 +0530

    Issues #SB-1442 fix: code cleaning

--- service/app/controllers/badging/BadgeIssuerController.java
+++ service/app/controllers/badging/BadgeIssuerController.java
@@ -1,26 +1,28 @@
 package controllers.badging;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import controllers.BaseController;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+
 import org.apache.commons.io.IOUtils;
-import org.apache.tika.Tika;
 import org.sunbird.badge.BadgeOperations;
 import org.sunbird.common.exception.ProjectCommonException;
-import org.sunbird.common.models.util.BadgingActorOperations;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
-import controllers.badging.validator.BadgeIssuerRequestValidator;
 import org.sunbird.common.request.ExecutionContext;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import controllers.BaseController;
+import controllers.badging.validator.BadgeIssuerRequestValidator;
 import play.libs.F.Promise;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
@@ -40,24 +42,11 @@ public class BadgeIssuerController extends BaseController {
     try {
       Request reqObj = new Request();
       Map<String, Object> map = new HashMap<>();
-      byte[] byteArray = null;
       MultipartFormData body = request().body().asMultipartFormData();
       JsonNode requestData = request().body().asJson();
-      Tika tika = new Tika();
-      ProjectLogger.log("call to create badge issuer api." + requestData, LoggerEnum.INFO.name());
+      ProjectLogger.log("call to create badge issuer api." + requestData, LoggerEnum.DEBUG.name());
       if (body != null) {
-        Map<String, String[]> data = body.asFormUrlEncoded();
-        for (Entry<String, String[]> entry : data.entrySet()) {
-          map.put(entry.getKey(), entry.getValue()[0]);
-        }
-        List<FilePart> filePart = body.getFiles();
-        if(filePart != null && !filePart.isEmpty()){
-          File f = filePart.get(0).getFile();
-          String mimeType = tika.detect(f);
-          InputStream is = new FileInputStream(f);
-          byteArray = IOUtils.toByteArray(is);
-          map.put(JsonKey.FILE_NAME , filePart.get(0).getFilename());
-        }
+    	  map = readFormData(body, map);
       }else if (null != requestData) {
         reqObj =
             (Request) mapper.RequestMapper.mapRequest(request().body().asJson(), Request.class);
@@ -68,13 +57,10 @@ public class BadgeIssuerController extends BaseController {
             ResponseCode.CLIENT_ERROR.getResponseCode());
         return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
       }
-      map.put(JsonKey.IMAGE, byteArray);
       reqObj.getRequest().putAll(map);
       BadgeIssuerRequestValidator.validateCreateBadgeIssuer(reqObj);
-      reqObj.setOperation(BadgeOperations.createBadgeIssuer.name());
-      reqObj.setRequestId(ExecutionContext.getRequestId());
-      reqObj.getRequest().put(JsonKey.CREATED_BY,ctx().flash().get(JsonKey.USER_ID));
-      reqObj.setEnv(getEnvironment());
+      reqObj = setExtraParam(reqObj, ExecutionContext.getRequestId(), BadgeOperations.createBadgeIssuer.name(),
+				ctx().flash().get(JsonKey.USER_ID), getEnvironment());
       return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
     } catch (Exception e) {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
@@ -85,20 +71,18 @@ public class BadgeIssuerController extends BaseController {
    * This method will add badges to user profile.
    * @return Promise<Result>
    */
-  public Promise<Result> getBadgeIssuer(String issuerId) {
-    try {
-      Request reqObj = new Request();
-      reqObj.setOperation(BadgeOperations.getBadgeIssuer.name());
-      reqObj.setRequestId(ExecutionContext.getRequestId());
-      reqObj.getRequest().put(JsonKey.CREATED_BY,ctx().flash().get(JsonKey.USER_ID));
-      reqObj.getRequest().put(JsonKey.SLUG , issuerId);
-      reqObj.setEnv(getEnvironment());
-      BadgeIssuerRequestValidator.validateGetBadgeIssuerDetail(reqObj);
-      return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
-    } catch (Exception e) {
-      return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
-    }
-  }
+	public Promise<Result> getBadgeIssuer(String issuerId) {
+		try {
+			Request reqObj = new Request();
+			reqObj = setExtraParam(reqObj, ExecutionContext.getRequestId(), BadgeOperations.getBadgeIssuer.name(),
+					ctx().flash().get(JsonKey.USER_ID), getEnvironment());
+			reqObj.getRequest().put(JsonKey.SLUG, issuerId);
+			BadgeIssuerRequestValidator.validateGetBadgeIssuerDetail(reqObj);
+			return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
+		} catch (Exception e) {
+			return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
+		}
+	}
 
   /**
    * This method will add badges to user profile.
@@ -107,10 +91,8 @@ public class BadgeIssuerController extends BaseController {
   public Promise<Result> getAllIssuer() {
     try {
       Request reqObj = new Request();
-      reqObj.setOperation(BadgeOperations.getAllIssuer.name());
-      reqObj.setRequestId(ExecutionContext.getRequestId());
-      reqObj.getRequest().put(JsonKey.CREATED_BY,ctx().flash().get(JsonKey.USER_ID));
-      reqObj.setEnv(getEnvironment());
+      reqObj = setExtraParam(reqObj, ExecutionContext.getRequestId(), BadgeOperations.getAllIssuer.name(),
+				ctx().flash().get(JsonKey.USER_ID), getEnvironment());
       return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
     } catch (Exception e) {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
@@ -124,11 +106,9 @@ public class BadgeIssuerController extends BaseController {
   public Promise<Result> deleteBadgeIssuer(String issuerId) {
     try {
       Request reqObj = new Request();
-      reqObj.setOperation(BadgeOperations.deleteIssuer.name());
-      reqObj.setRequestId(ExecutionContext.getRequestId());
-      reqObj.getRequest().put(JsonKey.CREATED_BY,ctx().flash().get(JsonKey.USER_ID));
+      reqObj = setExtraParam(reqObj, ExecutionContext.getRequestId(), BadgeOperations.deleteIssuer.name(),
+				ctx().flash().get(JsonKey.USER_ID), getEnvironment());
       reqObj.getRequest().put(JsonKey.SLUG , issuerId);
-      reqObj.setEnv(getEnvironment());
       BadgeIssuerRequestValidator.validateGetBadgeIssuerDetail(reqObj);
       return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
     } catch (Exception e) {
@@ -136,4 +116,28 @@ public class BadgeIssuerController extends BaseController {
     }
   }
 
+  /**
+   * This method will read from input data and put all the 
+   * requested key and value inside a map.
+   * @param body MultipartFormData
+   * @param map Map<String,Object>
+   * @return Map<String,Object>
+   * @throws IOException
+   */
+  private Map<String,Object> readFormData (MultipartFormData body , Map<String,Object> map) throws IOException{
+	  Map<String, String[]> data = body.asFormUrlEncoded();
+      for (Entry<String, String[]> entry : data.entrySet()) {
+        map.put(entry.getKey(), entry.getValue()[0]);
+      }
+      List<FilePart> filePart = body.getFiles();
+      if(filePart != null && !filePart.isEmpty()){
+        File f = filePart.get(0).getFile();
+        InputStream is = new FileInputStream(f);
+        byte[] byteArray = IOUtils.toByteArray(is);
+        map.put(JsonKey.FILE_NAME , filePart.get(0).getFilename());
+        map.put(JsonKey.IMAGE, byteArray);
+      } 
+     return map; 
+  }
+  
 }
--- service/app/controllers/telemetry/TelemetryController.java
+++ service/app/controllers/telemetry/TelemetryController.java
@@ -3,12 +3,12 @@
  */
 package controllers.telemetry;
 
-import org.sunbird.common.request.Request;
 import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.request.ExecutionContext;
+import org.sunbird.common.request.Request;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -16,7 +16,7 @@ import controllers.BaseController;
 import mapper.RequestMapper;
 import play.libs.F.Promise;
 import play.mvc.Result;
-import play.mvc.Results;
+
 /**
  * This controller will handle all sunbird telemetry request data.
  * @author Manzarul
@@ -29,19 +29,14 @@ public class TelemetryController extends BaseController {
    * 
    * @return Promise<Result>
    */
-	public Promise<Result>  saveTelemetryData() {
+	public Promise<Result> saveTelemetryData() {
 
 		try {
 			JsonNode requestData = request().body().asJson();
-			ProjectLogger.log("Receiving telemetry Data" + requestData, LoggerEnum.INFO.name());
+			ProjectLogger.log("Receiving telemetry Data" + requestData, LoggerEnum.DEBUG.name());
 			Request reqObj = (Request) RequestMapper.mapRequest(requestData, Request.class);
-			reqObj.setRequestId(ExecutionContext.getRequestId());
-			ProjectLogger.log("Data parsed ==" + reqObj.getParams() + "  " + reqObj.getRequest() + "  "
-					+ reqObj.getRequest().size(), LoggerEnum.INFO.name());
-			reqObj.setOperation(ActorOperations.SAVE_TELEMETRY.getValue());
-			reqObj.setRequestId(ExecutionContext.getRequestId());
-			reqObj.getRequest().put(JsonKey.CREATED_BY, ctx().flash().get(JsonKey.USER_ID));
-			reqObj.setEnv(getEnvironment());
+			reqObj = setExtraParam(reqObj, ExecutionContext.getRequestId(), ActorOperations.SAVE_TELEMETRY.getValue(),
+					ctx().flash().get(JsonKey.USER_ID), getEnvironment());
 			return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
 		} catch (Exception e) {
 			return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
