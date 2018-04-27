63278ee3544e1b07b82fb842ca401140b365071f
arvindyadav108 <arvind.glaitm108@gmail.com>
2018-04-27 13:20:56 +0530

    Issue # SB-2761 Developement : Location API implementation code review fixes.  .

--- service/test/controllers/location/LocationControllerTest.java
+++ service/test/controllers/location/LocationControllerTest.java
@@ -23,6 +23,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.sunbird.common.models.util.GeoLocationJsonKey;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.request.HeaderParam;
 import play.libs.Json;
@@ -32,7 +33,7 @@ import play.test.FakeApplication;
 import play.test.Helpers;
 import util.RequestInterceptor;
 
-/** Created by arvind on 19/4/18. */
+/** @author arvind on 19/4/18. */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(RequestInterceptor.class)
@@ -43,6 +44,15 @@ public class LocationControllerTest {
   private static Map<String, String[]> headerMap;
   private static ActorSystem system;
   private static final Props props = Props.create(DummyActor.class);
+  private static final String USER_TOKEN = "{userId} uuiuhcf784508 8y8c79-fhh";
+  private static final String LOCATION_NAME = "Laddakh";
+  private static final String LOCATION_CODE = "LOC_01";
+  private static final String LOCATION_TYPE = "State";
+  private static final String LOCATION_ID = "123";
+  private static final String CREATE_LOCATION_URL = "/v1/location/create";
+  private static final String UPDATE_LOCATION_URL = "/v1/location/update";
+  private static final String DELETE_LOCATION_URL = "/v1/location/delete";
+  private static final String SEARCH_LOCATION_URL = "/v1/location/search";
 
   @BeforeClass
   public static void startApp() {
@@ -59,45 +69,40 @@ public class LocationControllerTest {
     BaseController.setActorRef(subject);
 
     PowerMockito.mockStatic(RequestInterceptor.class);
-    when(RequestInterceptor.verifyRequestData(Mockito.anyObject()))
-        .thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
+    when(RequestInterceptor.verifyRequestData(Mockito.anyObject())).thenReturn(USER_TOKEN);
   }
 
   @Test
-  public void testcreateLocation() {
+  public void testCreateLocation() {
 
+    when(RequestInterceptor.verifyRequestData(Mockito.anyObject())).thenReturn(USER_TOKEN);
     Map<String, Object> requestMap = new HashMap<>();
-    Map<String, Object> requestBody = new HashMap<>();
     Map<String, Object> locationData = new HashMap<>();
-    locationData.put(JsonKey.NAME, "CAL");
-    locationData.put(JsonKey.CODE, "CA");
-    locationData.put(JsonKey.TYPE, "STATE");
-    requestBody.put(JsonKey.DATA, locationData);
-    requestMap.put(JsonKey.REQUEST, requestBody);
+    locationData.put(JsonKey.NAME, LOCATION_NAME);
+    locationData.put(JsonKey.CODE, LOCATION_CODE);
+    locationData.put(GeoLocationJsonKey.LOCATION_TYPE, LOCATION_TYPE);
+    requestMap.put(JsonKey.REQUEST, locationData);
     String data = TestUtil.mapToJson(requestMap);
     JsonNode json = Json.parse(data);
     RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/location/create").method("POST");
+        new RequestBuilder().bodyJson(json).uri(CREATE_LOCATION_URL).method("POST");
     req.headers(headerMap);
     Result result = route(req);
     assertEquals(200, result.status());
   }
 
   @Test
-  public void testcreateLocationWithInvalidRequest() {
+  public void testCreateLocationWithoutType() {
 
     Map<String, Object> requestMap = new HashMap<>();
-    Map<String, Object> requestBody = new HashMap<>();
     Map<String, Object> locationData = new HashMap<>();
-    locationData.put(JsonKey.NAME, "CAL");
-    locationData.put(JsonKey.CODE, "CA");
-
-    requestBody.put(JsonKey.DATA, locationData);
-    requestMap.put(JsonKey.REQUEST, requestBody);
+    locationData.put(JsonKey.NAME, LOCATION_NAME);
+    locationData.put(JsonKey.CODE, LOCATION_CODE);
+    requestMap.put(JsonKey.REQUEST, locationData);
     String data = TestUtil.mapToJson(requestMap);
     JsonNode json = Json.parse(data);
     RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/location/create").method("POST");
+        new RequestBuilder().bodyJson(json).uri(CREATE_LOCATION_URL).method("POST");
     req.headers(headerMap);
     Result result = route(req);
     assertEquals(400, result.status());
@@ -108,35 +113,32 @@ public class LocationControllerTest {
 
     Map<String, Object> requestMap = new HashMap<>();
     Map<String, Object> requestBody = new HashMap<>();
-    requestBody.put(JsonKey.NAME, "CAL");
-    requestBody.put(JsonKey.CODE, "CA");
-    requestBody.put(JsonKey.TYPE, "STATE");
-    requestBody.put(JsonKey.ID, "123");
+    requestBody.put(JsonKey.NAME, LOCATION_NAME);
+    requestBody.put(JsonKey.CODE, LOCATION_CODE);
+    requestBody.put(JsonKey.ID, LOCATION_ID);
 
     requestMap.put(JsonKey.REQUEST, requestBody);
     String data = TestUtil.mapToJson(requestMap);
     JsonNode json = Json.parse(data);
     RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/location/update").method("PATCH");
+        new RequestBuilder().bodyJson(json).uri(UPDATE_LOCATION_URL).method("PATCH");
     req.headers(headerMap);
     Result result = route(req);
     assertEquals(200, result.status());
   }
 
   @Test
-  public void testUpdateLocationWithInvalidRequest() {
+  public void testUpdateLocationWithType() {
 
     Map<String, Object> requestMap = new HashMap<>();
     Map<String, Object> requestBody = new HashMap<>();
-    requestBody.put(JsonKey.NAME, "CAL");
-    requestBody.put(JsonKey.CODE, "CA");
-    requestBody.put(JsonKey.TYPE, "STATE");
-
+    requestBody.put(GeoLocationJsonKey.LOCATION_TYPE, LOCATION_TYPE);
+    requestBody.put(JsonKey.ID, LOCATION_ID);
     requestMap.put(JsonKey.REQUEST, requestBody);
     String data = TestUtil.mapToJson(requestMap);
     JsonNode json = Json.parse(data);
     RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/location/update").method("PATCH");
+        new RequestBuilder().bodyJson(json).uri(UPDATE_LOCATION_URL).method("PATCH");
     req.headers(headerMap);
     Result result = route(req);
     assertEquals(400, result.status());
@@ -144,18 +146,11 @@ public class LocationControllerTest {
 
   @Test
   public void testDeleteLocation() {
-    RequestBuilder req = new RequestBuilder().uri("/v1/location/delete/123").method("DELETE");
+    RequestBuilder req =
+        new RequestBuilder().uri(DELETE_LOCATION_URL + "/" + LOCATION_ID).method("DELETE");
     req.headers(headerMap);
     Result result = route(req);
     assertEquals(200, result.status());
-  }
-
-  @Test
-  public void testDeleteLocationWithInvalidRequest() {
-    RequestBuilder req = new RequestBuilder().uri("/v1/location/delete/  ").method("DELETE");
-    req.headers(headerMap);
-    Result result = route(req);
-    assertEquals(400, result.status());
   }
 
   @Test
@@ -164,15 +159,15 @@ public class LocationControllerTest {
     Map<String, Object> requestMap = new HashMap<>();
     Map<String, Object> requestBody = new HashMap<>();
     Map<String, Object> filters = new HashMap<>();
-    filters.put(JsonKey.NAME, "CAL");
-    filters.put(JsonKey.CODE, "CA");
-    filters.put(JsonKey.TYPE, "STATE");
+    filters.put(JsonKey.NAME, LOCATION_NAME);
+    filters.put(JsonKey.CODE, LOCATION_CODE);
+    filters.put(GeoLocationJsonKey.LOCATION_TYPE, LOCATION_TYPE);
     requestBody.put(JsonKey.FILTERS, filters);
     requestMap.put(JsonKey.REQUEST, requestBody);
     String data = TestUtil.mapToJson(requestMap);
     JsonNode json = Json.parse(data);
     RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/location/search").method("POST");
+        new RequestBuilder().bodyJson(json).uri(SEARCH_LOCATION_URL).method("POST");
     req.headers(headerMap);
     Result result = route(req);
     assertEquals(200, result.status());
