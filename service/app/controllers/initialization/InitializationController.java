645405c80da3f500c705da03edc15ebd3b37c090
loganathan.shanmugam@tarento.com <loganathan.shanmugam@tarento.com>
2018-07-13 12:51:09 +0530

    Issue #SB-4881 fix: PR comment fixes

--- service/app/controllers/initialization/InitializationController.java
+++ service/app/controllers/initialization/InitializationController.java
@@ -3,27 +3,20 @@ package controllers.initialization;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import controllers.BaseController;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.models.util.ProjectUtil;
-import org.sunbird.common.models.util.ProjectUtil.EsType;
 import org.sunbird.common.request.ExecutionContext;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.request.RequestValidator;
 import play.libs.F.Promise;
 import play.mvc.Result;
 
-/**
- *
- *
- * @author Loganathan Shanmugam
- */
+/** @author Loganathan Shanmugam */
 public class InitializationController extends BaseController {
   /**
    * This method will do the initialisation process to create the first rootOrg in the system.
@@ -35,10 +28,10 @@ public class InitializationController extends BaseController {
     try {
       JsonNode requestData = request().body().asJson();
       ProjectLogger.log(
-          "InitializationController: initalizeSystem called" + requestData , LoggerEnum.INFO.name());
+          "InitializationController: initalizeSystem called" + requestData, LoggerEnum.INFO.name());
       Request reqObj = (Request) mapper.RequestMapper.mapRequest(requestData, Request.class);
       ProjectUtil.updateMapSomeValueTOLowerCase(reqObj);
-      reqObj.put("remoteAddress",request().remoteAddress());
+      reqObj.put("remoteAddress", request().remoteAddress());
       RequestValidator.validateCreateFirstRootOrg(reqObj);
       reqObj.setOperation(ActorOperations.CREATE_FIRST_ROOTORG.getValue());
       reqObj.setRequestId(ExecutionContext.getRequestId());
@@ -50,6 +43,6 @@ public class InitializationController extends BaseController {
       return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
     } catch (Exception e) {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
-		}
-	}
+    }
+  }
 }
