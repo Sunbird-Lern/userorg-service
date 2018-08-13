949693120f29907a44999aa5cb22bd3f82f1aa9d
Rajat <rajat@mantralabsglobal.com>
2018-08-13 13:51:36 +0530

    review changes

--- service/app/controllers/skills/UserSkillController.java
+++ service/app/controllers/skills/UserSkillController.java
@@ -2,12 +2,9 @@ package controllers.skills;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import controllers.BaseController;
-import org.apache.commons.lang3.StringUtils;
-import org.sunbird.common.exception.ProjectCommonException;
+import controllers.skills.validator.UserSkillRequestValidator;
 import org.sunbird.common.models.util.ActorOperations;
-import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.request.Request;
-import org.sunbird.common.responsecode.ResponseCode;
 import play.libs.F.Promise;
 import play.mvc.Result;
 
@@ -17,19 +14,7 @@ public class UserSkillController extends BaseController {
     try {
       JsonNode bodyJson = request().body().asJson();
       Request reqObj = createAndInitRequest(ActorOperations.UPDATE_SKILL.getValue(), bodyJson);
-      String userId = (String) reqObj.getRequest().get("userID");
-      if (StringUtils.isBlank(userId)) {
-        throw new ProjectCommonException(
-            ResponseCode.mandatoryParamsMissing.getErrorCode(),
-            ResponseCode.mandatoryParamsMissing.getErrorMessage(),
-            ResponseCode.mandatoryParamsMissing.getResponseCode());
-      }
-      if (!userId.equals(ctx().flash().get(JsonKey.USER_ID))) {
-        throw new ProjectCommonException(
-            ResponseCode.invalidParameterValue.getErrorCode(),
-            ResponseCode.invalidParameterValue.getErrorMessage(),
-            ResponseCode.invalidParameterValue.getResponseCode());
-      }
+      new UserSkillRequestValidator().validateUpdateSkillRequest(reqObj);
       return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
     } catch (Exception e) {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
