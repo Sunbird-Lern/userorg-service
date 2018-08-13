c3f7bf2618a29cfad0a8a1c79bedea2965e9c285
Rajat <rajat@mantralabsglobal.com>
2018-08-13 12:57:16 +0530

    add checks

--- service/app/controllers/skills/UserSkillEndorsementController.java
+++ service/app/controllers/skills/UserSkillEndorsementController.java
@@ -1,8 +1,13 @@
 package controllers.skills;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import controllers.BaseController;
+import org.apache.commons.lang.StringUtils;
+import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.util.ActorOperations;
+import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.request.Request;
+import org.sunbird.common.responsecode.ResponseCode;
 import play.libs.F.Promise;
 import play.mvc.Result;
 
@@ -10,7 +15,23 @@ public class UserSkillEndorsementController extends BaseController {
 
   public Promise<Result> addEndorsement() {
     try {
-      Request reqObj = createAndInitRequest(ActorOperations.ADD_USER_SKILL_ENDORSEMENT.getValue());
+      JsonNode bodyJson = request().body().asJson();
+      Request reqObj = createAndInitRequest(ActorOperations.ADD_USER_SKILL_ENDORSEMENT.getValue(), bodyJson);
+      String userId = (String) reqObj.getRequest().get("userID");
+      String endorseUserId = (String) reqObj.getRequest().get("endorseUserId");
+      String skillId = (String) reqObj.getRequest().get("skillId");
+      if(StringUtils.isBlank(userId) || StringUtils.isBlank(endorseUserId) || StringUtils.isBlank(skillId)){
+        throw new ProjectCommonException(
+                ResponseCode.mandatoryParamsMissing.getErrorCode(),
+                ResponseCode.mandatoryParamsMissing.getErrorMessage(),
+                ResponseCode.mandatoryParamsMissing.getResponseCode());
+      }
+      if (!userId.equals(ctx().flash().get(JsonKey.USER_ID))) {
+        throw new ProjectCommonException(
+                ResponseCode.invalidParameterValue.getErrorCode(),
+                ResponseCode.invalidParameterValue.getErrorMessage(),
+                ResponseCode.invalidParameterValue.getResponseCode());
+      }
       return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
     } catch (Exception e) {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
