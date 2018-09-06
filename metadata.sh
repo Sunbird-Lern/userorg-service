2dc952c04415881fd492bbc7ee676a8a4b8525ff
AMIT KUMAR <amit.kumar@tarento.com>
2018-09-06 14:44:01 +0530

    Issue #SB-6890 feat:updated version name in metadata.sh

--- metadata.sh
+++ metadata.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # return version
-echo '{"name":"learner_service","version":"release-1.10","org":"sunbird","hubuser":"purplesunbird"}'
+echo '{"name":"learner_service","version":"release-1.10.0","org":"sunbird","hubuser":"purplesunbird"}'
