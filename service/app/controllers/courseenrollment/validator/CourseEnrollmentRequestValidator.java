ab01829a4c7eabfb6f4e81536de9033a08d7dc92
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2018-09-20 17:40:58 +0530

    Update CourseEnrollmentRequestValidator.java

--- service/app/controllers/courseenrollment/validator/CourseEnrollmentRequestValidator.java
+++ service/app/controllers/courseenrollment/validator/CourseEnrollmentRequestValidator.java
@@ -5,36 +5,17 @@ import org.sunbird.common.request.BaseRequestValidator;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 
-/**
- * This call will do validation for all incoming request data for Course Enrollment.
- *
- * @author Sudhir
- */
 public class CourseEnrollmentRequestValidator extends BaseRequestValidator {
 
   public CourseEnrollmentRequestValidator() {}
 
-  /**
-   * This method will do course enrollment request data validation. if all mandatory data is coming
-   * then it won't do any thing if any mandatory data is missing then it will throw exception.
-   *
-   * @param courseRequestDto CourseRequestDto
-   */
   public void validateEnrollCourse(Request courseRequestDto) {
-
     validateParam(
         (String) courseRequestDto.getRequest().get(JsonKey.COURSE_ID),
         ResponseCode.courseIdRequiredError);
   }
 
-  /**
-   * This method will do course enrollment request data validation. if all mandatory data is coming
-   * then it won't do any thing if any mandatory data is missing then it will throw exception.
-   *
-   * @param courseRequestDto CourseRequestDto
-   */
   public void validateUnenrollCourse(Request courseRequestDto) {
-
     validateParam(
         (String) courseRequestDto.getRequest().get(JsonKey.COURSE_ID),
         ResponseCode.courseIdRequiredError);
@@ -46,4 +27,5 @@ public class CourseEnrollmentRequestValidator extends BaseRequestValidator {
     validateParam(
         (String) courseRequestDto.getRequest().get(JsonKey.USER_ID), ResponseCode.userIdRequired);
   }
+  
 }
