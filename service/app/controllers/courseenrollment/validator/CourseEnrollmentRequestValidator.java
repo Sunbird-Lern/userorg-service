69c6dc4dd9e8096f9ba41f64f186db787059d4d2
sudhirgiri2911 <sudhirgiri2911@gmail.com>
2018-09-20 12:02:42 +0530

    Issue #SB-5958 feat: Unenroll user from Course Batch API

---
+++ service/app/controllers/courseenrollment/validator/CourseEnrollmentRequestValidator.java
@@ -0,0 +1,51 @@
+package controllers.courseenrollment.validator;
+
+import org.sunbird.common.models.util.*;
+import org.sunbird.common.request.BaseRequestValidator;
+import org.sunbird.common.request.Request;
+import org.sunbird.common.responsecode.ResponseCode;
+
+/**
+ * This call will do validation for all incoming request data for Course Enrollment.
+ *
+ * @author Sudhir
+ */
+public class CourseEnrollmentRequestValidator extends BaseRequestValidator {
+
+  public CourseEnrollmentRequestValidator() {}
+
+  /**
+   * This method will do course enrollment request data validation. if all mandatory data is coming
+   * then it won't do any thing if any mandatory data is missing then it will throw exception.
+   *
+   * @param courseRequestDto CourseRequestDto
+   */
+  public void validateEnrollCourse(Request courseRequestDto) {
+
+    validateParam(
+        (String) courseRequestDto.getRequest().get(JsonKey.COURSE_ID),
+        ResponseCode.courseIdRequiredError);
+  }
+
+  /**
+   * This method will do course enrollment request data validation. if all mandatory data is coming
+   * then it won't do any thing if any mandatory data is missing then it will throw exception.
+   *
+   * @param courseRequestDto CourseRequestDto
+   */
+  public void validateUnenrollCourse(Request courseRequestDto) {
+
+    validateParam(
+        (String) courseRequestDto.getRequest().get(JsonKey.COURSE_ID),
+        ResponseCode.courseIdRequiredError);
+
+    validateParam(
+        (String) courseRequestDto.getRequest().get(JsonKey.BATCH_ID),
+        ResponseCode.courseBatchIdRequired);
+
+    validateParam(
+        (String) courseRequestDto.getRequest().get(JsonKey.USER_ID), ResponseCode.userIdRequired);
+
+    validateUserId(courseRequestDto, JsonKey.USER_ID);
+  }
+}
