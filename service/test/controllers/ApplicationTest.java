c73144c41a19db7371418cba33a097623e0dfc76
Manzarul <manzarul07@gmail.com>
2018-10-10 10:53:22 +0530

    Issues #SB-00 feat:refactor some of the controller unit test

--- service/test/controllers/ApplicationTest.java
+++ service/test/controllers/ApplicationTest.java
@@ -7,6 +7,7 @@ import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.response.ResponseParams;
 import org.sunbird.common.responsecode.ResponseCode;
+
 import play.mvc.Result;
 
 /**
@@ -30,18 +31,18 @@ public class ApplicationTest {
     assertEquals(ResponseCode.success.name(), params.getStatus());
   }
 
-  // @Test
+ @Test
   public void checkExceptionResponse() {
     ProjectCommonException exception =
         new ProjectCommonException(
             ResponseCode.courseIdRequiredError.getErrorCode(),
             ResponseCode.courseIdRequiredError.getErrorMessage(),
             ResponseCode.CLIENT_ERROR.getResponseCode());
-    Response response = BaseController.createResponseOnException(null, exception);
+    Response response = BaseController.createResponseOnException("v1/user/create","POST", exception);
     assertEquals(ResponseCode.courseIdRequiredError.getErrorCode(), response.getParams().getErr());
   }
 
-  // @Test
+  @Test
   public void testSuccessResponse() {
     Response response = new Response();
     response = BaseController.createSuccessResponse(null, response);
--- service/test/controllers/LearnerControllerTest.java
+++ service/test/controllers/LearnerControllerTest.java
@@ -1,219 +1,194 @@
 package controllers;
 
 import static org.junit.Assert.assertEquals;
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.junit.Assert.assertTrue;
 import static play.test.Helpers.route;
 
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Props;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
+
 import org.junit.Test;
-import org.junit.Ignore;
-import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.Mock;
 import org.sunbird.common.models.util.JsonKey;
-import org.sunbird.common.models.util.ProjectLogger;
-import org.sunbird.common.request.HeaderParam;
 import org.sunbird.common.responsecode.ResponseCode;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
 import play.libs.Json;
 import play.mvc.Http.RequestBuilder;
 import play.mvc.Result;
-import play.test.FakeApplication;
 import play.test.Helpers;
-import util.RequestInterceptor;
 
 /** @author arvind */
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(RequestInterceptor.class)
-@PowerMockIgnore("javax.management.*")
-@Ignore
-public class LearnerControllerTest {
+public class LearnerControllerTest  extends BaseTestHelper{
 
-  private static FakeApplication app;
-  private static Map<String, String[]> headerMap;
-  private static ActorSystem system;
-  private static final Props props = Props.create(controllers.DummyActor.class);
-  private static final String COURSE_ID = "course-123";
-  private static final String USER_ID = "user-123";
-  private static final String CONTENT_ID = "content-123";
-  private static final String BATCH_ID = "batch-123";
+	private static final String COURSE_ID = "course-123";
+	private static final String USER_ID = "user-123";
+	private static final String CONTENT_ID = "content-123";
+	private static final String BATCH_ID = "batch-123";
+	@Mock
+	private org.sunbird.cassandra.CassandraOperation cassandraOperation;
 
-  @BeforeClass
-  public static void startApp() {
-    app = Helpers.fakeApplication();
-    Helpers.start(app);
-    headerMap = new HashMap<String, String[]>();
-    headerMap.put(HeaderParam.X_Consumer_ID.getName(), new String[] {"Service test consumer"});
-    headerMap.put(HeaderParam.X_Device_ID.getName(), new String[] {"Some Device Id"});
-    headerMap.put(
-        HeaderParam.X_Authenticated_Userid.getName(), new String[] {"Authenticated user id"});
-    headerMap.put(JsonKey.MESSAGE_ID, new String[] {"Unique Message id"});
+	@Test
+	public void testCourseBatchEnrollemtSuccess() {
+		Map<String, Object> requestMap = new HashMap<>();
+		Map<String, Object> innerMap = new HashMap<>();
+		innerMap.put("courseId", COURSE_ID);
+		innerMap.put("batchId", BATCH_ID);
+		innerMap.put("userId", "{userId} uuiuhcf784508 8y8c79-fhh");
+		requestMap.put(JsonKey.REQUEST, innerMap);
+		String data = mapToJson(requestMap);
+		System.out.println(data);
+		JsonNode json = Json.parse(data);
+		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
+		req.headers(headerMap);
+		Result result = route(req);
+		assertEquals(200, result.status());
+	}
 
-    system = ActorSystem.create("system");
-    ActorRef subject = system.actorOf(props);
-    BaseController.setActorRef(subject);
-  }
+	@Test
+	public void testCourseBatchEnrollemtFailureWithOutCourseId() {
+		Map<String, Object> requestMap = new HashMap<>();
+		Map<String, Object> innerMap = new HashMap<>();
+		innerMap.put("batchId", BATCH_ID);
+		innerMap.put("userId", "{userId} uuiuhcf784508 8y8c79-fhh");
+		requestMap.put(JsonKey.REQUEST, innerMap);
+		String data = mapToJson(requestMap);
+		JsonNode json = Json.parse(data);
+		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
+		req.headers(headerMap);
+		Result result = route(req);
+		String response = Helpers.contentAsString(result);
+		assertEquals(400, result.status());
+		assertTrue(response.contains("Mandatory parameter courseId is missing."));
+	}
 
-  @Test
-  public void testEnrolCourse() {
-    PowerMockito.mockStatic(RequestInterceptor.class);
-    when(RequestInterceptor.verifyRequestData(Mockito.anyObject()))
-        .thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
-    Map<String, Object> requestMap = new HashMap<>();
-    Map<String, Object> innerMap = new HashMap<>();
-    innerMap.put("courseId", COURSE_ID);
-    requestMap.put(JsonKey.REQUEST, innerMap);
-    String data = mapToJson(requestMap);
-    System.out.println(data);
-    JsonNode json = Json.parse(data);
-    RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
-    req.headers(headerMap);
-    Result result = route(req);
-    assertEquals(200, result.status());
-  }
+	@Test
+	public void testCourseBatchEnrollemtFailureWithOutBatchId() {
+		Map<String, Object> requestMap = new HashMap<>();
+		Map<String, Object> innerMap = new HashMap<>();
+		innerMap.put("courseId", COURSE_ID);
+		innerMap.put("userId", "{userId} uuiuhcf784508 8y8c79-fhh");
+		requestMap.put(JsonKey.REQUEST, innerMap);
+		String data = mapToJson(requestMap);
+		System.out.println(data);
+		JsonNode json = Json.parse(data);
+		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
+		req.headers(headerMap);
+		Result result = route(req);
+		String response = Helpers.contentAsString(result);
+		assertEquals(400, result.status());
+		assertTrue(response.contains("Mandatory parameter batchId is missing."));
+	}
 
-  @Test
-  public void testEnrolCourseWithInvalidData() {
-    PowerMockito.mockStatic(RequestInterceptor.class);
-    when(RequestInterceptor.verifyRequestData(Mockito.anyObject()))
-        .thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
-    Map<String, Object> requestMap = new HashMap<>();
-    Map<String, Object> innerMap = new HashMap<>();
-    innerMap.put("courseId", null);
-    requestMap.put(JsonKey.REQUEST, innerMap);
-    String data = mapToJson(requestMap);
-    System.out.println(data);
-    JsonNode json = Json.parse(data);
-    RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
-    req.headers(headerMap);
-    Result result = route(req);
-    assertEquals(400, result.status());
-  }
+	@Test
+	public void testCourseBatchEnrollemtFailureWithOutUserId() {
+		Map<String, Object> requestMap = new HashMap<>();
+		Map<String, Object> innerMap = new HashMap<>();
+		innerMap.put("courseId", COURSE_ID);
+		innerMap.put("batchId", BATCH_ID);
+		requestMap.put(JsonKey.REQUEST, innerMap);
+		String data = mapToJson(requestMap);
+		JsonNode json = Json.parse(data);
+		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
+		req.headers(headerMap);
+		Result result = route(req);
+		String response = Helpers.contentAsString(result);
+		assertEquals(400, result.status());
+		assertTrue(response.contains("Mandatory parameter userId is missing."));
+	}
 
-  @Test
-  public void testGetEnroledCourses() {
-    PowerMockito.mockStatic(RequestInterceptor.class);
-    when(RequestInterceptor.verifyRequestData(Mockito.anyObject()))
-        .thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
-    RequestBuilder req = new RequestBuilder().uri("/v1/user/courses/userid").method("GET");
-    req.headers(headerMap);
-    Result result = route(req);
-    assertEquals(200, result.status());
-  }
+	@Test
+	public void testCourseBatchEnrollemtFailureWithOutAnyParam() {
+		Map<String, Object> requestMap = new HashMap<>();
+		Map<String, Object> innerMap = new HashMap<>();
+		requestMap.put(JsonKey.REQUEST, innerMap);
+		String data = mapToJson(requestMap);
+		JsonNode json = Json.parse(data);
+		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
+		req.headers(headerMap);
+		Result result = route(req);
+		String response = Helpers.contentAsString(result);
+		assertEquals(400, result.status());
+		assertTrue(response.contains("Mandatory parameter courseId is missing."));
+	}
 
-  @Test
-  public void testUpdateContentState() {
-    PowerMockito.mockStatic(RequestInterceptor.class);
-    when(RequestInterceptor.verifyRequestData(Mockito.anyObject()))
-        .thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
-    Map<String, Object> requestMap = new HashMap<>();
-    Map<String, Object> innerMap = new HashMap<>();
-    List list = new ArrayList();
-    Map map = new HashMap();
-    map.put(JsonKey.CONTENT_ID, CONTENT_ID);
-    map.put(JsonKey.STATUS, "Active");
-    map.put(JsonKey.LAST_UPDATED_TIME, "2017-12-18 10:47:30:707+0530");
-    list.add(map);
-    innerMap.put("contents", list);
-    innerMap.put("courseId", COURSE_ID);
-    requestMap.put(JsonKey.REQUEST, innerMap);
-    String data = mapToJson(requestMap);
+	@Test
+	public void testGetEnroledCoursesSuccess() {
+		RequestBuilder req = new RequestBuilder().uri("/v1/user/courses/list/userid").method("GET");
+		req.headers(headerMap);
+		Result result = route(req);
+		assertEquals(200, result.status());
+	}
 
-    JsonNode json = Json.parse(data);
-    RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/content/state/update").method("PATCH");
-    req.headers(headerMap);
-    Result result = route(req);
-    assertEquals(200, result.status());
-  }
+	@Test
+	public void testUpdateContentStateSuccess() {
+		Map<String, Object> requestMap = new HashMap<>();
+		Map<String, Object> innerMap = new HashMap<>();
+		List<Object> list = new ArrayList<>();
+		Map<String, Object> map = new HashMap<>();
+		map.put(JsonKey.CONTENT_ID, CONTENT_ID);
+		map.put(JsonKey.STATUS, "Active");
+		map.put(JsonKey.LAST_UPDATED_TIME, "2017-12-18 10:47:30:707+0530");
+		list.add(map);
+		innerMap.put("contents", list);
+		innerMap.put("courseId", COURSE_ID);
+		requestMap.put(JsonKey.REQUEST, innerMap);
+		String data = mapToJson(requestMap);
+		JsonNode json = Json.parse(data);
+		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/content/state/update").method("PATCH");
+		req.headers(headerMap);
+		Result result = route(req);
+		String response = Helpers.contentAsString(result);
+		System.out.println(response);
+		assertEquals(200, result.status());
+	}
 
-  @Test
-  public void testGetContentState() {
-    PowerMockito.mockStatic(RequestInterceptor.class);
-    when(RequestInterceptor.verifyRequestData(Mockito.anyObject()))
-        .thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
-    Map<String, Object> requestMap = new HashMap<>();
-    Map<String, Object> innerMap = new HashMap<>();
-    innerMap.put(JsonKey.BATCH_ID, BATCH_ID);
-    innerMap.put(JsonKey.USER_ID, USER_ID);
-    requestMap.put(JsonKey.REQUEST, innerMap);
-    String data = mapToJson(requestMap);
+	@Test
+	public void testGetContentStateSuccess() {
+		Map<String, Object> requestMap = new HashMap<>();
+		Map<String, Object> innerMap = new HashMap<>();
+		innerMap.put(JsonKey.BATCH_ID, BATCH_ID);
+		innerMap.put(JsonKey.USER_ID, USER_ID);
+		requestMap.put(JsonKey.REQUEST, innerMap);
+		String data = mapToJson(requestMap);
+		JsonNode json = Json.parse(data);
+		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
+		req.headers(headerMap);
+		Result result = route(req);
+		assertEquals(200, result.status());
+	}
 
-    JsonNode json = Json.parse(data);
-    RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
+	@Test
+	public void testGetContentStateFailureWithInvalidFieldType() {
+		Map<String, Object> requestMap = new HashMap<>();
+		Map<String, Object> innerMap = new HashMap<>();
+		innerMap.put("courseId", COURSE_ID);
+		innerMap.put(JsonKey.COURSE_IDS, new HashMap<>());
+		requestMap.put(JsonKey.REQUEST, innerMap);
+		String data = mapToJson(requestMap);
 
-    req.headers(headerMap);
-    Result result = route(req);
-    assertEquals(200, result.status());
-  }
+		JsonNode json = Json.parse(data);
+		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
 
-  @Test
-  public void testGetContentStateWithoutUserId() {
-    PowerMockito.mockStatic(RequestInterceptor.class);
-    when(RequestInterceptor.verifyRequestData(Mockito.anyObject()))
-        .thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
-    Map<String, Object> requestMap = new HashMap<>();
-    Map<String, Object> innerMap = new HashMap<>();
-    innerMap.put("courseId", COURSE_ID);
-    requestMap.put(JsonKey.REQUEST, innerMap);
-    String data = mapToJson(requestMap);
+		req.headers(headerMap);
+		Result result = route(req);
+		assertEquals(ResponseCode.CLIENT_ERROR.getResponseCode(), result.status());
+	}
 
-    JsonNode json = Json.parse(data);
-    RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
-
-    req.headers(headerMap);
-    Result result = route(req);
-    assertEquals(ResponseCode.CLIENT_ERROR.getResponseCode(), result.status());
-  }
-
-  @Test
-  public void testGetContentStateWithInvalidFieldType() {
-    PowerMockito.mockStatic(RequestInterceptor.class);
-    when(RequestInterceptor.verifyRequestData(Mockito.anyObject()))
-        .thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
-    Map<String, Object> requestMap = new HashMap<>();
-    Map<String, Object> innerMap = new HashMap<>();
-    innerMap.put("courseId", COURSE_ID);
-    innerMap.put(JsonKey.COURSE_IDS, new HashMap<>());
-    requestMap.put(JsonKey.REQUEST, innerMap);
-    String data = mapToJson(requestMap);
-
-    JsonNode json = Json.parse(data);
-    RequestBuilder req =
-        new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
-
-    req.headers(headerMap);
-    Result result = route(req);
-    assertEquals(ResponseCode.CLIENT_ERROR.getResponseCode(), result.status());
-  }
-
-  private static String mapToJson(Map map) {
-    ObjectMapper mapperObj = new ObjectMapper();
-    String jsonResp = "";
-    try {
-      jsonResp = mapperObj.writeValueAsString(map);
-    } catch (IOException e) {
-      ProjectLogger.log(e.getMessage(), e);
-    }
-    return jsonResp;
-  }
+	@Test
+	public void testGetContentStateFailureWithoutUserId() {
+		Map<String, Object> requestMap = new HashMap<>();
+		Map<String, Object> innerMap = new HashMap<>();
+		innerMap.put("courseId", COURSE_ID);
+		requestMap.put(JsonKey.REQUEST, innerMap);
+		String data = mapToJson(requestMap);
+		JsonNode json = Json.parse(data);
+		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
+		req.headers(headerMap);
+		Result result = route(req);
+		assertEquals(ResponseCode.CLIENT_ERROR.getResponseCode(), result.status());
+	}
 }
