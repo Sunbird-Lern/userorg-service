09453dc0abfeaa1628fe0da32da8883302279165
Manzarul <manzarul07@gmail.com>
2018-10-15 12:22:05 +0530

    Issues #SB-00 fix: running code formatter

--- service/test/controllers/LearnerControllerTest.java
+++ service/test/controllers/LearnerControllerTest.java
@@ -4,191 +4,197 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static play.test.Helpers.route;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.junit.Test;
 import org.mockito.Mock;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.responsecode.ResponseCode;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
 import play.libs.Json;
 import play.mvc.Http.RequestBuilder;
 import play.mvc.Result;
 import play.test.Helpers;
 
 /** @author arvind */
-public class LearnerControllerTest  extends BaseTestHelper{
+public class LearnerControllerTest extends BaseTestHelper {
 
-	private static final String COURSE_ID = "course-123";
-	private static final String USER_ID = "user-123";
-	private static final String CONTENT_ID = "content-123";
-	private static final String BATCH_ID = "batch-123";
-	@Mock
-	private org.sunbird.cassandra.CassandraOperation cassandraOperation;
+  private static final String COURSE_ID = "course-123";
+  private static final String USER_ID = "user-123";
+  private static final String CONTENT_ID = "content-123";
+  private static final String BATCH_ID = "batch-123";
+  @Mock private org.sunbird.cassandra.CassandraOperation cassandraOperation;
 
-	@Test
-	public void testCourseBatchEnrollemtSuccess() {
-		Map<String, Object> requestMap = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		innerMap.put("courseId", COURSE_ID);
-		innerMap.put("batchId", BATCH_ID);
-		innerMap.put("userId", "{userId} uuiuhcf784508 8y8c79-fhh");
-		requestMap.put(JsonKey.REQUEST, innerMap);
-		String data = mapToJson(requestMap);
-		System.out.println(data);
-		JsonNode json = Json.parse(data);
-		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
-		req.headers(headerMap);
-		Result result = route(req);
-		assertEquals(200, result.status());
-	}
+  @Test
+  public void testCourseBatchEnrollemtSuccess() {
 
-	@Test
-	public void testCourseBatchEnrollemtFailureWithOutCourseId() {
-		Map<String, Object> requestMap = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		innerMap.put("batchId", BATCH_ID);
-		innerMap.put("userId", "{userId} uuiuhcf784508 8y8c79-fhh");
-		requestMap.put(JsonKey.REQUEST, innerMap);
-		String data = mapToJson(requestMap);
-		JsonNode json = Json.parse(data);
-		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
-		req.headers(headerMap);
-		Result result = route(req);
-		String response = Helpers.contentAsString(result);
-		assertEquals(400, result.status());
-		assertTrue(response.contains("Mandatory parameter courseId is missing."));
-	}
+    Map<String, Object> requestMap = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    innerMap.put("courseId", COURSE_ID);
+    innerMap.put("batchId", BATCH_ID);
+    innerMap.put("userId", "{userId} uuiuhcf784508 8y8c79-fhh");
+    requestMap.put(JsonKey.REQUEST, innerMap);
+    String data = mapToJson(requestMap);
+    System.out.println(data);
+    JsonNode json = Json.parse(data);
+    RequestBuilder req =
+        new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
+    req.headers(headerMap);
+    Result result = route(req);
+    assertEquals(200, result.status());
+  }
 
-	@Test
-	public void testCourseBatchEnrollemtFailureWithOutBatchId() {
-		Map<String, Object> requestMap = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		innerMap.put("courseId", COURSE_ID);
-		innerMap.put("userId", "{userId} uuiuhcf784508 8y8c79-fhh");
-		requestMap.put(JsonKey.REQUEST, innerMap);
-		String data = mapToJson(requestMap);
-		System.out.println(data);
-		JsonNode json = Json.parse(data);
-		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
-		req.headers(headerMap);
-		Result result = route(req);
-		String response = Helpers.contentAsString(result);
-		assertEquals(400, result.status());
-		assertTrue(response.contains("Mandatory parameter batchId is missing."));
-	}
+  @Test
+  public void testCourseBatchEnrollemtFailureWithOutCourseId() {
+    Map<String, Object> requestMap = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    innerMap.put("batchId", BATCH_ID);
+    innerMap.put("userId", "{userId} uuiuhcf784508 8y8c79-fhh");
+    requestMap.put(JsonKey.REQUEST, innerMap);
+    String data = mapToJson(requestMap);
+    JsonNode json = Json.parse(data);
+    RequestBuilder req =
+        new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
+    req.headers(headerMap);
+    Result result = route(req);
+    String response = Helpers.contentAsString(result);
+    assertEquals(400, result.status());
+    assertTrue(response.contains("Mandatory parameter courseId is missing."));
+  }
 
-	@Test
-	public void testCourseBatchEnrollemtFailureWithOutUserId() {
-		Map<String, Object> requestMap = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		innerMap.put("courseId", COURSE_ID);
-		innerMap.put("batchId", BATCH_ID);
-		requestMap.put(JsonKey.REQUEST, innerMap);
-		String data = mapToJson(requestMap);
-		JsonNode json = Json.parse(data);
-		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
-		req.headers(headerMap);
-		Result result = route(req);
-		String response = Helpers.contentAsString(result);
-		assertEquals(400, result.status());
-		assertTrue(response.contains("Mandatory parameter userId is missing."));
-	}
+  @Test
+  public void testCourseBatchEnrollemtFailureWithOutBatchId() {
+    Map<String, Object> requestMap = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    innerMap.put("courseId", COURSE_ID);
+    innerMap.put("userId", "{userId} uuiuhcf784508 8y8c79-fhh");
+    requestMap.put(JsonKey.REQUEST, innerMap);
+    String data = mapToJson(requestMap);
+    System.out.println(data);
+    JsonNode json = Json.parse(data);
+    RequestBuilder req =
+        new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
+    req.headers(headerMap);
+    Result result = route(req);
+    String response = Helpers.contentAsString(result);
+    assertEquals(400, result.status());
+    assertTrue(response.contains("Mandatory parameter batchId is missing."));
+  }
 
-	@Test
-	public void testCourseBatchEnrollemtFailureWithOutAnyParam() {
-		Map<String, Object> requestMap = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		requestMap.put(JsonKey.REQUEST, innerMap);
-		String data = mapToJson(requestMap);
-		JsonNode json = Json.parse(data);
-		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
-		req.headers(headerMap);
-		Result result = route(req);
-		String response = Helpers.contentAsString(result);
-		assertEquals(400, result.status());
-		assertTrue(response.contains("Mandatory parameter courseId is missing."));
-	}
+  @Test
+  public void testCourseBatchEnrollemtFailureWithOutUserId() {
+    Map<String, Object> requestMap = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    innerMap.put("courseId", COURSE_ID);
+    innerMap.put("batchId", BATCH_ID);
+    requestMap.put(JsonKey.REQUEST, innerMap);
+    String data = mapToJson(requestMap);
+    JsonNode json = Json.parse(data);
+    RequestBuilder req =
+        new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
+    req.headers(headerMap);
+    Result result = route(req);
+    String response = Helpers.contentAsString(result);
+    assertEquals(400, result.status());
+    assertTrue(response.contains("Mandatory parameter userId is missing."));
+  }
 
-	@Test
-	public void testGetEnroledCoursesSuccess() {
-		RequestBuilder req = new RequestBuilder().uri("/v1/user/courses/list/userid").method("GET");
-		req.headers(headerMap);
-		Result result = route(req);
-		assertEquals(200, result.status());
-	}
+  @Test
+  public void testCourseBatchEnrollemtFailureWithOutAnyParam() {
+    Map<String, Object> requestMap = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    requestMap.put(JsonKey.REQUEST, innerMap);
+    String data = mapToJson(requestMap);
+    JsonNode json = Json.parse(data);
+    RequestBuilder req =
+        new RequestBuilder().bodyJson(json).uri("/v1/user/courses/enroll").method("POST");
+    req.headers(headerMap);
+    Result result = route(req);
+    String response = Helpers.contentAsString(result);
+    assertEquals(400, result.status());
+    assertTrue(response.contains("Mandatory parameter courseId is missing."));
+  }
 
-	@Test
-	public void testUpdateContentStateSuccess() {
-		Map<String, Object> requestMap = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		List<Object> list = new ArrayList<>();
-		Map<String, Object> map = new HashMap<>();
-		map.put(JsonKey.CONTENT_ID, CONTENT_ID);
-		map.put(JsonKey.STATUS, "Active");
-		map.put(JsonKey.LAST_UPDATED_TIME, "2017-12-18 10:47:30:707+0530");
-		list.add(map);
-		innerMap.put("contents", list);
-		innerMap.put("courseId", COURSE_ID);
-		requestMap.put(JsonKey.REQUEST, innerMap);
-		String data = mapToJson(requestMap);
-		JsonNode json = Json.parse(data);
-		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/content/state/update").method("PATCH");
-		req.headers(headerMap);
-		Result result = route(req);
-		String response = Helpers.contentAsString(result);
-		System.out.println(response);
-		assertEquals(200, result.status());
-	}
+  @Test
+  public void testGetEnroledCoursesSuccess() {
+    RequestBuilder req = new RequestBuilder().uri("/v1/user/courses/list/userid").method("GET");
+    req.headers(headerMap);
+    Result result = route(req);
+    assertEquals(200, result.status());
+  }
 
-	@Test
-	public void testGetContentStateSuccess() {
-		Map<String, Object> requestMap = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		innerMap.put(JsonKey.BATCH_ID, BATCH_ID);
-		innerMap.put(JsonKey.USER_ID, USER_ID);
-		requestMap.put(JsonKey.REQUEST, innerMap);
-		String data = mapToJson(requestMap);
-		JsonNode json = Json.parse(data);
-		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
-		req.headers(headerMap);
-		Result result = route(req);
-		assertEquals(200, result.status());
-	}
+  @Test
+  public void testUpdateContentStateSuccess() {
+    Map<String, Object> requestMap = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    List<Object> list = new ArrayList<>();
+    Map<String, Object> map = new HashMap<>();
+    map.put(JsonKey.CONTENT_ID, CONTENT_ID);
+    map.put(JsonKey.STATUS, "Active");
+    map.put(JsonKey.LAST_UPDATED_TIME, "2017-12-18 10:47:30:707+0530");
+    list.add(map);
+    innerMap.put("contents", list);
+    innerMap.put("courseId", COURSE_ID);
+    requestMap.put(JsonKey.REQUEST, innerMap);
+    String data = mapToJson(requestMap);
+    JsonNode json = Json.parse(data);
+    RequestBuilder req =
+        new RequestBuilder().bodyJson(json).uri("/v1/content/state/update").method("PATCH");
+    req.headers(headerMap);
+    Result result = route(req);
+    String response = Helpers.contentAsString(result);
+    System.out.println(response);
+    assertEquals(200, result.status());
+  }
 
-	@Test
-	public void testGetContentStateFailureWithInvalidFieldType() {
-		Map<String, Object> requestMap = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		innerMap.put("courseId", COURSE_ID);
-		innerMap.put(JsonKey.COURSE_IDS, new HashMap<>());
-		requestMap.put(JsonKey.REQUEST, innerMap);
-		String data = mapToJson(requestMap);
+  @Test
+  public void testGetContentStateSuccess() {
+    Map<String, Object> requestMap = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    innerMap.put(JsonKey.BATCH_ID, BATCH_ID);
+    innerMap.put(JsonKey.USER_ID, USER_ID);
+    requestMap.put(JsonKey.REQUEST, innerMap);
+    String data = mapToJson(requestMap);
+    JsonNode json = Json.parse(data);
+    RequestBuilder req =
+        new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
+    req.headers(headerMap);
+    Result result = route(req);
+    assertEquals(200, result.status());
+  }
 
-		JsonNode json = Json.parse(data);
-		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
+  @Test
+  public void testGetContentStateFailureWithInvalidFieldType() {
+    Map<String, Object> requestMap = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    innerMap.put("courseId", COURSE_ID);
+    innerMap.put(JsonKey.COURSE_IDS, new HashMap<>());
+    requestMap.put(JsonKey.REQUEST, innerMap);
+    String data = mapToJson(requestMap);
 
-		req.headers(headerMap);
-		Result result = route(req);
-		assertEquals(ResponseCode.CLIENT_ERROR.getResponseCode(), result.status());
-	}
+    JsonNode json = Json.parse(data);
+    RequestBuilder req =
+        new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
 
-	@Test
-	public void testGetContentStateFailureWithoutUserId() {
-		Map<String, Object> requestMap = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		innerMap.put("courseId", COURSE_ID);
-		requestMap.put(JsonKey.REQUEST, innerMap);
-		String data = mapToJson(requestMap);
-		JsonNode json = Json.parse(data);
-		RequestBuilder req = new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
-		req.headers(headerMap);
-		Result result = route(req);
-		assertEquals(ResponseCode.CLIENT_ERROR.getResponseCode(), result.status());
-	}
+    req.headers(headerMap);
+    Result result = route(req);
+    assertEquals(ResponseCode.CLIENT_ERROR.getResponseCode(), result.status());
+  }
+
+  @Test
+  public void testGetContentStateFailureWithoutUserId() {
+    Map<String, Object> requestMap = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    innerMap.put("courseId", COURSE_ID);
+    requestMap.put(JsonKey.REQUEST, innerMap);
+    String data = mapToJson(requestMap);
+    JsonNode json = Json.parse(data);
+    RequestBuilder req =
+        new RequestBuilder().bodyJson(json).uri("/v1/content/state/read").method("POST");
+    req.headers(headerMap);
+    Result result = route(req);
+    assertEquals(ResponseCode.CLIENT_ERROR.getResponseCode(), result.status());
+  }
 }
