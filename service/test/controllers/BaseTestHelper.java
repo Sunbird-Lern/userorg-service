8cbc427606c9d479990e8931aae8ee49bfe8bd33
Manzarul <manzarul07@gmail.com>
2018-10-15 12:23:40 +0530

    Issues #SB-00 fix: running code formatter

--- service/test/controllers/BaseTestHelper.java
+++ service/test/controllers/BaseTestHelper.java
@@ -1,9 +1,12 @@
 package controllers;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
@@ -23,79 +26,83 @@ import org.sunbird.middleware.Application;
 import org.sunbird.services.sso.SSOManager;
 import org.sunbird.services.sso.SSOServiceFactory;
 import org.sunbird.services.sso.impl.KeyCloakServiceImpl;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Props;
 import play.test.FakeApplication;
 import play.test.Helpers;
 import util.AuthenticationHelper;
 import util.RequestInterceptor;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({RequestInterceptor.class,AuthenticationHelper.class,Application.class,SSOServiceFactory.class,KeyCloakConnectionProvider.class,Util.class})
-@SuppressStaticInitializationFor({"util.AuthenticationHelper","util.Global"})
+@PrepareForTest({
+  RequestInterceptor.class,
+  AuthenticationHelper.class,
+  Application.class,
+  SSOServiceFactory.class,
+  KeyCloakConnectionProvider.class,
+  Util.class
+})
+@SuppressStaticInitializationFor({"util.AuthenticationHelper", "util.Global"})
 @PowerMockIgnore("javax.management.*")
 public class BaseTestHelper {
 
-	public static FakeApplication app;
-	public static Map<String, String[]> headerMap;
-	public static ActorSystem system;
-	public static final Props props = Props.create(DummyActor.class);
+  public static FakeApplication app;
+  public static Map<String, String[]> headerMap;
+  public static ActorSystem system;
+  public static final Props props = Props.create(DummyActor.class);
 
-	@BeforeClass
-	public static void startApp() {
-		app = Helpers.fakeApplication();
-		Helpers.start(app);
-		headerMap = new HashMap<String, String[]>();
-		headerMap.put(HeaderParam.X_Consumer_ID.getName(), new String[] { "Service test consumer" });
-		headerMap.put(HeaderParam.X_Device_ID.getName(), new String[] { "Some Device Id" });
-		headerMap.put(HeaderParam.X_Authenticated_Userid.getName(), new String[] { "Authenticated user id" });
-		headerMap.put(JsonKey.MESSAGE_ID, new String[] { "Unique Message id" });
+  @BeforeClass
+  public static void startApp() {
 
-		system = ActorSystem.create("system");
-		ActorRef subject = system.actorOf(props);
-		BaseController.setActorRef(subject);
-	}
+    app = Helpers.fakeApplication();
+    Helpers.start(app);
+    headerMap = new HashMap<String, String[]>();
+    headerMap.put(HeaderParam.X_Consumer_ID.getName(), new String[] {"Service test consumer"});
+    headerMap.put(HeaderParam.X_Device_ID.getName(), new String[] {"Some Device Id"});
+    headerMap.put(
+        HeaderParam.X_Authenticated_Userid.getName(), new String[] {"Authenticated user id"});
+    headerMap.put(JsonKey.MESSAGE_ID, new String[] {"Unique Message id"});
 
-	@Before
-	public void doResourceMock() {
-		SSOManager impl = Mockito.mock(KeyCloakServiceImpl.class);
-		Keycloak keycloak = Mockito.mock(Keycloak.class);
-		PowerMockito.mockStatic(SSOServiceFactory.class);
-		PowerMockito.when(SSOServiceFactory.getInstance()).thenReturn(impl);
-		PowerMockito.mockStatic(KeyCloakConnectionProvider.class);
-		try {
-			Mockito.when(KeyCloakConnectionProvider.initialiseConnection()).thenReturn(keycloak);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		PowerMockito.mockStatic(AuthenticationHelper.class);
-		Mockito.when(AuthenticationHelper.verifyUserAccesToken(Mockito.anyString())).thenReturn("userId");
-		PowerMockito.mockStatic(RequestInterceptor.class);
-		Mockito.when(RequestInterceptor.verifyRequestData(Mockito.anyObject()))
-				.thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
-		PowerMockito.mockStatic(Util.class);
-		PowerMockito.mockStatic(Application.class);
-		try {
-			PowerMockito.doNothing().when(Util.class, "checkCassandraDbConnections");
-			PowerMockito.doNothing().when(Application.class, "checkCassandraConnection");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public static String mapToJson(Map map) {
-		ObjectMapper mapperObj = new ObjectMapper();
-		String jsonResp = "";
-		try {
-			jsonResp = mapperObj.writeValueAsString(map);
-		} catch (IOException e) {
-			ProjectLogger.log(e.getMessage(), e);
-		}
-		return jsonResp;
-	}
+    system = ActorSystem.create("system");
+    ActorRef subject = system.actorOf(props);
+    BaseController.setActorRef(subject);
+  }
+
+  @Before
+  public void doResourceMock() {
+    SSOManager impl = Mockito.mock(KeyCloakServiceImpl.class);
+    Keycloak keycloak = Mockito.mock(Keycloak.class);
+    PowerMockito.mockStatic(SSOServiceFactory.class);
+    PowerMockito.when(SSOServiceFactory.getInstance()).thenReturn(impl);
+    PowerMockito.mockStatic(KeyCloakConnectionProvider.class);
+    try {
+      Mockito.when(KeyCloakConnectionProvider.initialiseConnection()).thenReturn(keycloak);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    PowerMockito.mockStatic(AuthenticationHelper.class);
+    Mockito.when(AuthenticationHelper.verifyUserAccesToken(Mockito.anyString()))
+        .thenReturn("userId");
+    PowerMockito.mockStatic(RequestInterceptor.class);
+    Mockito.when(RequestInterceptor.verifyRequestData(Mockito.anyObject()))
+        .thenReturn("{userId} uuiuhcf784508 8y8c79-fhh");
+    PowerMockito.mockStatic(Util.class);
+    PowerMockito.mockStatic(Application.class);
+    try {
+      PowerMockito.doNothing().when(Util.class, "checkCassandraDbConnections");
+      PowerMockito.doNothing().when(Application.class, "checkCassandraConnection");
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  public static String mapToJson(Map map) {
+    ObjectMapper mapperObj = new ObjectMapper();
+    String jsonResp = "";
+    try {
+      jsonResp = mapperObj.writeValueAsString(map);
+    } catch (IOException e) {
+      ProjectLogger.log(e.getMessage(), e);
+    }
+    return jsonResp;
+  }
 }
--- service/test/controllers/RequestMapperTest.java
+++ service/test/controllers/RequestMapperTest.java
@@ -1,8 +1,10 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
-
+import mapper.RequestMapper;
 import org.junit.Assert;
 import org.junit.Test;
 import org.sunbird.common.exception.ProjectCommonException;
@@ -10,48 +12,43 @@ import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import mapper.RequestMapper;
-
-
 public class RequestMapperTest {
-	@Test
-	public void testMapRequestFailure() {
-		try {
-			RequestMapper.mapRequest(null, Request.class);
-		} catch (ProjectCommonException e) {
-			Assert.assertEquals(ResponseCode.contentTypeRequiredError.getErrorMessage(), e.getMessage());
-			Assert.assertEquals(ResponseCode.CLIENT_ERROR.getResponseCode(), e.getResponseCode());
-		}
-	}
+  @Test
+  public void testMapRequestFailure() {
+    try {
+      RequestMapper.mapRequest(null, Request.class);
+    } catch (ProjectCommonException e) {
+      Assert.assertEquals(ResponseCode.contentTypeRequiredError.getErrorMessage(), e.getMessage());
+      Assert.assertEquals(ResponseCode.CLIENT_ERROR.getResponseCode(), e.getResponseCode());
+    }
+  }
 
-	@Test
-	public void testMapRequestSuccess() {
-		Request request = null;
-		JsonNode node = new ObjectMapper().convertValue(testRequestData(JsonKey.REQUEST), JsonNode.class);
-		request = (Request) RequestMapper.mapRequest(node, Request.class);
-		Assert.assertNotNull(request);
-		Assert.assertEquals("xyz", request.getRequest().get(JsonKey.FIRST_NAME));
-	}
+  @Test
+  public void testMapRequestSuccess() {
+    Request request = null;
+    JsonNode node =
+        new ObjectMapper().convertValue(testRequestData(JsonKey.REQUEST), JsonNode.class);
+    request = (Request) RequestMapper.mapRequest(node, Request.class);
+    Assert.assertNotNull(request);
+    Assert.assertEquals("xyz", request.getRequest().get(JsonKey.FIRST_NAME));
+  }
 
-	@Test
-	public void testMapRequestFailureWithInvalidKey() {
-		Request request = null;
-		JsonNode node = new ObjectMapper().convertValue(testRequestData("invalidKey"), JsonNode.class);
-		request = (Request) RequestMapper.mapRequest(node, Request.class);
-		Assert.assertNotNull(request);
-		Assert.assertEquals(null, request.getRequest().get(JsonKey.FIRST_NAME));
-	}
+  @Test
+  public void testMapRequestFailureWithInvalidKey() {
+    Request request = null;
+    JsonNode node = new ObjectMapper().convertValue(testRequestData("invalidKey"), JsonNode.class);
+    request = (Request) RequestMapper.mapRequest(node, Request.class);
+    Assert.assertNotNull(request);
+    Assert.assertEquals(null, request.getRequest().get(JsonKey.FIRST_NAME));
+  }
 
-	private Map<String, Object> testRequestData(String requestKey) {
-		Map<String, Object> map = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		innerMap.put(JsonKey.FIRST_NAME, "xyz");
-		innerMap.put(JsonKey.PHONE, "1234567890");
-		innerMap.put(JsonKey.EMAIL, "xyz@gmail.com");
-		map.put(requestKey, innerMap);
-		return map;
-	}
+  private Map<String, Object> testRequestData(String requestKey) {
+    Map<String, Object> map = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    innerMap.put(JsonKey.FIRST_NAME, "xyz");
+    innerMap.put(JsonKey.PHONE, "1234567890");
+    innerMap.put(JsonKey.EMAIL, "xyz@gmail.com");
+    map.put(requestKey, innerMap);
+    return map;
+  }
 }
