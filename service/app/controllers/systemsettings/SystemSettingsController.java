d1c23210481ea501779f0fdd906d38c022af98e5
rajat <rajatgupta0889@gmail.com>
2018-11-14 22:44:58 +0530

    Issue #SB-8330 fix: change in api

--- service/app/controllers/systemsettings/SystemSettingsController.java
+++ service/app/controllers/systemsettings/SystemSettingsController.java
@@ -1,13 +1,8 @@
 package controllers.systemsettings;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import controllers.BaseController;
-import java.util.HashMap;
-import java.util.Map;
 import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.models.util.JsonKey;
-import org.sunbird.common.models.util.LoggerEnum;
-import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.request.Request;
 import org.sunbird.validator.systemsettings.SystemSettingsRequestValidator;
 import play.libs.F.Promise;
@@ -21,13 +16,7 @@ public class SystemSettingsController extends BaseController {
   @SuppressWarnings("unchecked")
   public Promise<Result> getSystemSetting(String field) {
     try {
-      ProjectLogger.log(
-          "SystemSettingsController: getSystemSetting called", LoggerEnum.INFO.name());
-      Request reqObj = createAndInitRequest(ActorOperations.GET_SYSTEM_SETTING.getValue());
-      Map<String, Object> map = new HashMap();
-      map.put(JsonKey.FIELD, field);
-      reqObj.setRequest(map);
-      return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
+      return handleRequest(ActorOperations.GET_SYSTEM_SETTING.getValue(), field, JsonKey.FIELD);
     } catch (Exception e) {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
     }
@@ -36,13 +25,13 @@ public class SystemSettingsController extends BaseController {
   @SuppressWarnings("unchecked")
   public Promise<Result> setSystemSetting() {
     try {
-      ProjectLogger.log(
-          "SystemSettingsController: setSystemSetting called", LoggerEnum.INFO.name());
-      JsonNode requestJson = request().body().asJson();
-      Request reqObj =
-          createAndInitRequest(ActorOperations.SET_SYSTEM_SETTING.getValue(), requestJson);
-      systemSettingsRequestValidator.validateSetSystemSetting(reqObj);
-      return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
+      return handleRequest(
+          ActorOperations.SET_SYSTEM_SETTING.getValue(),
+          request().body().asJson(),
+          (request) -> {
+            systemSettingsRequestValidator.validateSetSystemSetting((Request) request);
+            return null;
+          });
     } catch (Exception e) {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
     }
@@ -51,10 +40,7 @@ public class SystemSettingsController extends BaseController {
   @SuppressWarnings("unchecked")
   public Promise<Result> getAllSystemSettings() {
     try {
-      ProjectLogger.log(
-          "SystemSettingsController: getAllSystemSettings called", LoggerEnum.INFO.name());
-      Request reqObj = createAndInitRequest(ActorOperations.GET_ALL_SYSTEM_SETTINGS.getValue());
-      return actorResponseHandler(getActorRef(), reqObj, timeout, null, request());
+      return handleRequest(ActorOperations.GET_ALL_SYSTEM_SETTINGS.getValue());
     } catch (Exception e) {
       return Promise.<Result>pure(createCommonExceptionResponse(e, request()));
     }
