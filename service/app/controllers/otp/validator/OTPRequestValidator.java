ee5021babcc6df30213913de85b60d2c46d1fd81
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2018-12-13 15:04:28 +0530

    Update OTPRequestValidator.java

--- service/app/controllers/otp/validator/OTPRequestValidator.java
+++ service/app/controllers/otp/validator/OTPRequestValidator.java
@@ -7,7 +7,6 @@ import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 
 public class OTPRequestValidator extends BaseRequestValidator {
-  private static final int ERROR_CODE = ResponseCode.CLIENT_ERROR.getResponseCode();
 
   public void validateGenerateOTPRequest(Request otpRequest) {
     validateParam(
@@ -23,15 +22,18 @@ public class OTPRequestValidator extends BaseRequestValidator {
   }
 
   private void validateKeyFormat(Request otpRequest) {
-    if (JsonKey.EMAIL.equalsIgnoreCase((String) otpRequest.getRequest().get(JsonKey.TYPE))) {
-      validateEmail((String) otpRequest.getRequest().get(JsonKey.KEY));
-    } else if (JsonKey.PHONE.equalsIgnoreCase((String) otpRequest.getRequest().get(JsonKey.TYPE))) {
-      validatePhone((String) otpRequest.getRequest().get(JsonKey.KEY));
+    Map<String, Object> requestMap = otpRequest.getRequest();
+
+    String type = (String) requestMap.get(JsonKey.TYPE);
+    String key = (String) requestMap.get(JsonKey.KEY);
+
+    if (JsonKey.EMAIL.equalsIgnoreCase(type)) {
+      validateEmail(key);
+    } else if (JsonKey.PHONE.equalsIgnoreCase(type)) {
+      validatePhone(key);
     } else {
-      throw new ProjectCommonException(
-          ResponseCode.invalidTypeValue.getErrorCode(),
-          ResponseCode.invalidTypeValue.getErrorMessage(),
-          ERROR_CODE);
+      ProjectCommonException.throwClientErrorException(ResponseCode.invalidTypeValue);
     }
   }
+
 }
