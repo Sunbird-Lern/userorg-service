51346f2e149da7f6999c2cd2e3fdf85903943904
Mayank Morya <moryamayank9@gmail.com>
2018-12-20 13:07:25 +0530

    SB:9795: user-framework-update-test added

--- service/test/controllers/usermanagement/UserControllerTest.java
+++ service/test/controllers/usermanagement/UserControllerTest.java
@@ -118,6 +118,64 @@ public class UserControllerTest extends BaseControllerTest {
     assertEquals(getResponseCode(result), ResponseCode.loginIdRequired.getErrorCode());
     assertTrue(getResponseStatus(result) == 400);
   }
+  @Test
+  public void testUpdateUserFrameworkSuccess() {
+    Result result =
+        performTest(
+            "/v1/user/update",
+            "PATCH",
+            (Map) UpdateUserFrameworkRequest(userId, "NCF",true));
+    assertEquals(getResponseCode(result), ResponseCode.success.getErrorCode().toLowerCase());
+    assertTrue(getResponseStatus(result) == 200);
+  }
+  @Test
+  public void testUpdateUserFrameworkFailure() {
+    Result result =
+        performTest(
+            "/v1/user/update",
+            "PATCH",
+            (Map) UpdateUserFrameworkRequest(userId, "NCF",false));
+    assertEquals(getResponseCode(result), ResponseCode.mandatoryParamsMissing.getErrorCode());
+    assertTrue(getResponseStatus(result) == 400);
+  }
+  
+
+  private Map UpdateUserFrameworkRequest(String userId, String frameworkId, boolean success) {
+    Map<String, Object> request = new HashMap<>();
+    Map<String, Object> innerMap = new HashMap<>();
+    Map<String, Object> frameworkMap;
+    frameworkMap = getFrameworkDetails(frameworkId, success);
+    innerMap.put(JsonKey.USER_ID, userId);
+    innerMap.put(JsonKey.FRAMEWORK, frameworkMap);
+    innerMap.put(JsonKey.PHONE_VERIFIED, true);
+    innerMap.put(JsonKey.PHONE, phoneNumber);
+    innerMap.put(JsonKey.COUNTRY_CODE, "+91");
+    innerMap.put(JsonKey.EMAIL, emailId);
+    request.put(JsonKey.REQUEST, innerMap);
+    return request;
+  }
+
+  private Map<String, Object> getFrameworkDetails(String frameworkId, boolean success) {
+    Map<String, Object> frameworkMap = new HashMap<>();
+    List<String> medium = new ArrayList<>();
+    medium.add("English");
+    List<String> gradeLevel = new ArrayList<>();
+    gradeLevel.add("Grade 3");
+    List<String> board = new ArrayList<>();
+    board.add("NCERT");
+    if (success) {
+      frameworkMap.put(JsonKey.ID, frameworkId);
+    } else {
+      frameworkMap.put(JsonKey.ID, "");
+    }
+    frameworkMap.put("medium", medium);
+    frameworkMap.put("gradeLevel", gradeLevel);
+    frameworkMap.put("board", board);
+    return frameworkMap;
+  }
+  
+  
+  
 
   private Object createOrUpdateUserRequest(
       String userName, String phoneNumber, String userId, boolean isContentType) {
