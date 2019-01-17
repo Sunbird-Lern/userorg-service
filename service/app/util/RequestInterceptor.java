12fbf90ab661b2a8d568a79723f246fff5b848cb
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2019-01-17 14:32:44 +0530

    Update RequestInterceptor.java

--- service/app/util/RequestInterceptor.java
+++ service/app/util/RequestInterceptor.java
@@ -109,7 +109,7 @@ public class RequestInterceptor {
     String accessToken = request.getHeader(HeaderParam.X_Authenticated_User_Token.getName());
     String authClientToken = request.getHeader(HeaderParam.X_Authenticated_Client_Token.getName());
     String authClientId = request.getHeader(HeaderParam.X_Authenticated_Client_Id.getName());
-    if (!isRequestInExcludeList(request.path()) && !isRequestIsPrivate(request.path())) {
+    if (!isRequestInExcludeList(request.path()) && !isRequestPrivate(request.path())) {
       if (StringUtils.isNotBlank(accessToken)) {
         clientId = AuthenticationHelper.verifyUserAccesToken(accessToken);
       } else if (StringUtils.isNotBlank(authClientToken) && StringUtils.isNotBlank(authClientId)) {
@@ -139,7 +139,7 @@ public class RequestInterceptor {
     }
   }
 
-  private static boolean isRequestIsPrivate(String path) {
+  private static boolean isRequestPrivate(String path) {
     if (path.contains(JsonKey.PRIVATE)) {
       return true;
     }
