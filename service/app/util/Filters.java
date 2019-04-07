7a61a2fe29d76605748d6256f5e92c3a1b27c882
Mayank Morya <moryamayank9@gmail.com>
2019-04-07 17:00:53 +0530

    configurable gzip filter added to learner searvice

--- service/app/util/Filters.java
+++ service/app/util/Filters.java
@@ -1,22 +1,69 @@
 package util;
 
-import filters.LoggingFilter;
 import javax.inject.Inject;
+
 import play.api.mvc.EssentialFilter;
 import play.http.HttpFilters;
+import play.filters.gzip.GzipFilter;
+import play.filters.gzip.GzipFilterConfig;
+import akka.stream.Materializer;
+import filters.LoggingFilter;
+import play.api.mvc.EssentialAction;
+import play.api.mvc.RequestHeader;
+import play.api.mvc.ResponseHeader;
+import play.filters.gzip.Gzip;
+import play.filters.gzip.GzipFilter;
+import scala.Function2;
+import scala.runtime.AbstractFunction2;
+import org.apache.commons.lang3.StringUtils;
+import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.LoggerEnum;
+import org.sunbird.common.models.util.ProjectLogger;
+import org.sunbird.common.models.util.PropertiesCache;
 
-/** @author Mahesh Kumar Gangula */
 public class Filters implements HttpFilters {
+  private EssentialFilter[] filters;
+  
+  private static boolean GzipFilterEnabled = Boolean.parseBoolean(
+      PropertiesCache.getInstance()
+          .getProperty(JsonKey.SUNBIRD_GZIP_FILTER_ENABLED));
+
+  private Function2<RequestHeader, ResponseHeader, Object> shouldGzip = new AbstractFunction2<RequestHeader, ResponseHeader, Object>() {
+
+    @Override
+    public Boolean apply(RequestHeader v1, ResponseHeader v2) {
+
+      return shouldGzipFunction(v1, v2);
+    }
+
+  };
+  private final GzipFilter filter;
 
   private final LoggingFilter loggingFilter;
 
   @Inject
-  public Filters(LoggingFilter loggingFilter) {
+  public Filters(LoggingFilter loggingFilter, GzipFilter filter) {
     this.loggingFilter = loggingFilter;
+    this.filter = new GzipFilter(Gzip.gzip(8192), 102400, shouldGzip);
+  }
+
+  private boolean shouldGzipFunction(RequestHeader v1, ResponseHeader v2) {
+    ProjectLogger.log("********************************* : "+GzipFilterEnabled, LoggerEnum.INFO);
+    if (GzipFilterEnabled && (v1.headers().get("Accept-Encoding") != null)) {
+      if (v1.headers().get("Accept-Encoding").toString().toLowerCase().contains("gzip")) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public EssentialAction apply(EssentialAction next) {
+    return filter.apply(next);
   }
 
   @Override
   public EssentialFilter[] filters() {
-    return new EssentialFilter[] {loggingFilter};
+    return new EssentialFilter[] { loggingFilter, filter };
   }
+
 }
