92fa63ce1931696df37cfee5988ffff555f1c346
anmol2302 <anmolgupta23021997@gmail.com>
2019-05-13 15:49:21 +0530

    caching added

--- service/app/controllers/BaseController.java
+++ service/app/controllers/BaseController.java
@@ -4,6 +4,7 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.pattern.Patterns;
 import akka.util.Timeout;
+import cacher.ApiCacher;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.*;
@@ -622,6 +623,12 @@ public class BaseController extends Controller {
         };
 
     if (actorRef instanceof ActorRef) {
+      Object obj = ApiCacher.getDataFromCache(operation, request.getRequest());
+      if (obj != null) {
+        Response response = new Response();
+        response.put("response", obj);
+        return Promise.pure(createCommonResponse(response, responseKey, httpReq));
+      }
       return Promise.wrap(Patterns.ask((ActorRef) actorRef, request, timeout)).map(function);
     } else {
       return Promise.wrap(Patterns.ask((ActorSelection) actorRef, request, timeout)).map(function);
