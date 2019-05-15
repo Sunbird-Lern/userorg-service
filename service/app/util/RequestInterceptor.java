899cada3a919648173e9ae8da0751f844b4ff893
Manzarul <manzarul07@gmail.com>
2019-05-15 18:08:04 +0530

    Issues #SB-12347 fix: chnages for user search from keycloak to Lms service

--- service/app/util/RequestInterceptor.java
+++ service/app/util/RequestInterceptor.java
@@ -100,6 +100,7 @@ public class RequestInterceptor {
     apiHeaderIgnoreMap.put("/v1/course/batch/search", var);
     apiHeaderIgnoreMap.put("/v1/user/mock/read", var);
     apiHeaderIgnoreMap.put("/v1/cache/clear", var);
+    apiHeaderIgnoreMap.put("/private/user/v1/search", var);
   }
 
   /**
