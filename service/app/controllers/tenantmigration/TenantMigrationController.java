0fc1d00e2f932068922cb6313027e27d46642d9f
AMIT KUMAR <amit.kumar@tarento.com>
2019-05-30 19:50:07 +0530

    Issue #SB-12693 feat:user migration

--- service/app/controllers/tenantmigration/TenantMigrationController.java
+++ service/app/controllers/tenantmigration/TenantMigrationController.java
@@ -1,12 +1,11 @@
 package controllers.tenantmigration;
 
 import controllers.BaseController;
-import org.apache.commons.lang3.StringUtils;
-import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.request.Request;
-import play.libs.Json;
+import org.sunbird.common.request.UserTenantMigrationRequestValidator;
+import play.libs.F.Promise;
 import play.mvc.Result;
-import play.mvc.Results;
 
 /** @author Amit Kumar This controller will handle all the request related for user migration. */
 public class TenantMigrationController extends BaseController {
@@ -16,21 +15,17 @@ public class TenantMigrationController extends BaseController {
    *
    * @return Result
    */
-  public Result userTenantMigrate() {
-    Request request = createAndInitRequest("migrate", request().body().asJson());
-
-    if (StringUtils.isBlank((String) request.getRequest().get(JsonKey.CHANNEL))) {
-      return Results.badRequest(
-          Json.parse(
-              "{\"id\":\"api.private.user.migrate\",\"ver\":\"v1\",\"ts\":\"2019-05-29 09:10:09:807+0000\",\"params\":{\"resmsgid\":null,\"msgid\":\"94c06447-8c72-44d3-abad-3149832271ae\",\"err\":\"MANDATORY_PARAMETER_MISSING\",\"status\":\"MANDATORY_PARAMETER_MISSING\",\"errmsg\":\"Mandatory parameter channel is missing.\"},\"responseCode\":\"CLIENT_ERROR\",\"result\":{}}"));
-    }
-    if (StringUtils.isBlank((String) request.getRequest().get(JsonKey.USER_ID))) {
-      return Results.badRequest(
-          Json.parse(
-              "{\"id\":\"api.private.user.migrate\",\"ver\":\"v1\",\"ts\":\"2019-05-29 09:10:09:807+0000\",\"params\":{\"resmsgid\":null,\"msgid\":\"94c06447-8c72-44d3-abad-3149832271ae\",\"err\":\"MANDATORY_PARAMETER_MISSING\",\"status\":\"MANDATORY_PARAMETER_MISSING\",\"errmsg\":\"Mandatory parameter userId is missing.\"},\"responseCode\":\"CLIENT_ERROR\",\"result\":{}}"));
-    }
-    return Results.ok(
-        Json.parse(
-            "{\"id\":\"api.private.user.migrate\",\"ver\":\"v1\",\"ts\":\"2019-01-17 16:53:26:286+0530\",\"params\":{\"resmsgid\":null,\"msgid\":\"8e27cbf5-e299-43b0-bca7-8347f7ejk5abcf\",\"err\":null,\"status\":\"success\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"response\":{\"response\":\"SUCCESS\",\"errors\":[]}}}"));
+  public Promise<Result> userTenantMigrate() {
+    return handleRequest(
+        ActorOperations.USER_TENANT_MIGRATE.getValue(),
+        request().body().asJson(),
+        req -> {
+          Request request = (Request) req;
+          new UserTenantMigrationRequestValidator().validateUserTenantMigrateRequest(request);
+          return null;
+        },
+        null,
+        null,
+        true);
   }
 }
