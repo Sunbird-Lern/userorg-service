03ee4fbce33a5cf6dd792acac4d28f366f4c901a
anmol2302 <anmolgupta23021997@gmail.com>
2019-07-24 17:53:57 +0530

    Issue#SC-13803 added dummy response for user freeup controller

--- service/app/controllers/usermanagement/UserFreeUpController.java
+++ service/app/controllers/usermanagement/UserFreeUpController.java
@@ -2,11 +2,17 @@ package controllers.usermanagement;
 
 import controllers.BaseController;
 import org.sunbird.common.models.response.Response;
+import org.sunbird.common.models.util.JsonKey;
 import play.libs.F.Promise;
 import play.libs.Json;
 import play.mvc.Result;
 import play.mvc.Results;
 
+/**
+ * this action method is responsible to freeup the user account.
+ *
+ * @author anmolgupta
+ */
 public class UserFreeUpController extends BaseController {
 
     public Promise<Result> freeUp(){
@@ -18,9 +24,8 @@ public class UserFreeUpController extends BaseController {
      * @return string
      */
     public Response getDummyResponse() {
-
         Response response=new Response();
-        response.put("message","success");
+        response.put(JsonKey.MESSAGE, JsonKey.SUCCESS);
         response.setId("api.user.freeup");
         response.setVer("v1");
         return response;
