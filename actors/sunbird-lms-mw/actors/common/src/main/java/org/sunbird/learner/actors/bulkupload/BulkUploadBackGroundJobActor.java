93fd597f4b3893c46853e0988e1eb709f66c5a17
Rahul K <rahul.kumar@tarento.com>
2019-07-29 02:28:31 +0530

    fix for user check

--- actors/sunbird-lms-mw/actors/common/src/main/java/org/sunbird/learner/actors/bulkupload/BulkUploadBackGroundJobActor.java
+++ actors/sunbird-lms-mw/actors/common/src/main/java/org/sunbird/learner/actors/bulkupload/BulkUploadBackGroundJobActor.java
@@ -185,7 +185,7 @@ public class BulkUploadBackGroundJobActor extends BaseActor {
                     user -> (String) user.get(JsonKey.ID), user -> getRootOrgFromUserMap(user)));
     // check whether can update user or not
     for (String userId : userIds) {
-      if (!userToRootOrg.containsKey(userId) || createdFor.contains(userToRootOrg.get(userId))) {
+      if (!userToRootOrg.containsKey(userId) || !createdFor.contains(userToRootOrg.get(userId))) {
         map = new HashMap<>();
         map.put(userId, ResponseCode.userNotAssociatedToOrg.getErrorMessage());
         failedUserList.add(map);
