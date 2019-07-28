42f791db84d743b744a26129f3758a1d5e1d0cde
Rahul K <rahul.kumar@tarento.com>
2019-07-29 01:29:45 +0530

    correcting es type for bulk enrollment

--- actors/sunbird-lms-mw/actors/common/src/main/java/org/sunbird/learner/actors/bulkupload/BulkUploadBackGroundJobActor.java
+++ actors/sunbird-lms-mw/actors/common/src/main/java/org/sunbird/learner/actors/bulkupload/BulkUploadBackGroundJobActor.java
@@ -113,7 +113,7 @@ public class BulkUploadBackGroundJobActor extends BaseActor {
 
       String batchId = (String) batchMap.get(JsonKey.BATCH_ID);
       Future<Map<String, Object>> resultF =
-          esService.getDataByIdentifier(ProjectUtil.EsType.course.getTypeName(), batchId);
+          esService.getDataByIdentifier(ProjectUtil.EsType.courseBatch.getTypeName(), batchId);
       Map<String, Object> courseBatchObject =
           (Map<String, Object>) ElasticSearchHelper.getResponseFromFuture(resultF);
       String msg = validateBatchInfo(courseBatchObject);
