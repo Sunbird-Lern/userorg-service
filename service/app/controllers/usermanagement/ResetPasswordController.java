bb34e346919f12cf0882f5dc08d60c60b17edd4c
amit <amit.kumar@tarento.com>
2019-07-29 16:45:13 +0530

    Issue #SB-14036 feat: reset password api impl

---
+++ service/app/controllers/usermanagement/ResetPasswordController.java
@@ -0,0 +1,34 @@
+package controllers.usermanagement;
+
+import controllers.BaseController;
+import controllers.usermanagement.validator.ResetPasswordRequestValidator;
+import org.sunbird.common.models.util.ActorOperations;
+import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.request.Request;
+import play.libs.F.Promise;
+import play.libs.Json;
+import play.mvc.Result;
+import play.mvc.Results;
+
+/**
+ * 
+ * This controller contains method for reset the user password.
+ *
+ */
+public class ResetPasswordController extends BaseController{
+
+	/**
+	 * This method will reset the password for given userId in request.
+	 * @return Promise
+	 */
+	public Promise<Result> resetPassword() {
+	    return handleRequest(
+	        ActorOperations.RESET_PASSWORD.getValue(),
+	        request().body().asJson(),
+	        (request) -> {
+	          new ResetPasswordRequestValidator().validateResetPasswordRequest((Request) request);
+	          return null;
+	        },
+	        getAllRequestHeaders(request()));
+	  }
+}
