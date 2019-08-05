08d76786740aeb3222b1be8a3770802ad541b446
Harikumar Palemkota <harip@ilimi.in>
2019-08-05 15:21:14 +0530

    Issue #SC-1258 added controller and config changes

--- service/app/controllers/certmanagement/CertificateController.java
+++ service/app/controllers/certmanagement/CertificateController.java
@@ -1,4 +1,20 @@
 package controllers.certmanagement;
 
-public class CertificateController {
+import controllers.BaseController;
+import org.sunbird.common.models.util.ActorOperations;
+import org.sunbird.common.request.Request;
+import org.sunbird.common.request.UserRequestValidator;
+import play.libs.F;
+import play.mvc.Result;
+
+public class CertificateController extends BaseController {
+
+   public F.Promise<Result> validateCertificate() {
+     return handleRequest(ActorOperations.VALIDATE_CERTIFICATE.getValue(),request().body().asJson(),
+             req -> {
+               Request request = (Request) req;
+               new UserRequestValidator().validateCertValidationRequest(request);
+               return null;
+             });
+   }
 }
