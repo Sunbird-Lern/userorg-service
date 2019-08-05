842eb03f77eca1dbd85816dd7f009ca5dcad6aa2
Harikumar Palemkota <harip@ilimi.in>
2019-08-05 18:29:45 +0530

    Issue #SC-1258 review code fixes

--- service/app/controllers/certmanagement/CertificateController.java
+++ service/app/controllers/certmanagement/CertificateController.java
@@ -7,8 +7,14 @@ import org.sunbird.common.request.UserRequestValidator;
 import play.libs.F;
 import play.mvc.Result;
 
+/**
+ * This Controller is used for user-certificate related purposes
+ */
 public class CertificateController extends BaseController {
 
+  /** This method helps in validating the access code and retrieves the certificate details
+   * @return json and pdf links as response
+   */
    public F.Promise<Result> validateCertificate() {
      return handleRequest(ActorOperations.VALIDATE_CERTIFICATE.getValue(),request().body().asJson(),
              req -> {
