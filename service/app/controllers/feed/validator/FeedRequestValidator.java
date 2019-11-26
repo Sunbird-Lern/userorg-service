9bd3d564e3c80c33dda5b27a276f0f3b05fe53d8
amit <amit.kumar@tarento.com>
2019-11-26 17:54:33 +0530

    Issue #SC-1511 feat: test case and code review fix

--- service/app/controllers/feed/validator/FeedRequestValidator.java
+++ service/app/controllers/feed/validator/FeedRequestValidator.java
@@ -6,7 +6,7 @@ import org.sunbird.common.exception.ProjectCommonException;
 /** This call will validate the Feed API request */
 public class FeedRequestValidator {
   public static boolean userIdValidation(String accessTokenUserId, String requestUserId) {
-    if (!StringUtils.endsWithIgnoreCase(accessTokenUserId, requestUserId)) {
+    if (!StringUtils.equalsIgnoreCase(accessTokenUserId, requestUserId)) {
       ProjectCommonException.throwUnauthorizedErrorException();
     }
     return true;
