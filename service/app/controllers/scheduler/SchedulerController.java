dfc383a515531b237db36972b2676c50d0e65831
Hari-stackroute <40484996+Hari-stackroute@users.noreply.github.com>
2020-01-31 12:45:12 +0530

    Issue #SC-1654 Initial draft for Trigger scheduler on demand (#378)
    * Issue #SC-1654 Intial draft for Trigger scheduler on demand
    
    * Issue #SC-1654 code changes done after review comments
    
    * Issue #SC-1654 added test case for scheduler controller
    
    * Issue #SC-1654 added space to run build
    
    Co-authored-by: manzarul Haque <manzarul07@gmail.com>

---
+++ service/app/controllers/scheduler/SchedulerController.java
@@ -0,0 +1,22 @@
+package controllers.scheduler;
+
+import controllers.BaseController;
+import org.sunbird.common.models.util.ActorOperations;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import java.util.concurrent.CompletionStage;
+
+public class SchedulerController extends BaseController {
+ public CompletionStage<Result> startScheduler(Http.Request httpRequest) {
+  return handleRequest(
+          ActorOperations.ONDEMAND_START_SCHEDULER.getValue(),
+          httpRequest.body().asJson(),
+          null,
+          null,
+          null,
+          true,
+          httpRequest);
+  
+ }
+}
