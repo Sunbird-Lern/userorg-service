6ccb49ee9081192e2b4e747dde45272453524397
Reshmi V Nair <revas.nair@gmail.com>
2020-05-19 23:20:30 +0530

    SH-136 Accept Terms and Conditions API – changes to be made to take additional 'userId' param

--- service/app/controllers/tac/validator/UserTnCRequestValidator.java
+++ service/app/controllers/tac/validator/UserTnCRequestValidator.java
@@ -1,9 +1,14 @@
 package controllers.tac.validator;
 
+import org.apache.commons.lang3.StringUtils;
+import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.ProjectUtil;
 import org.sunbird.common.request.BaseRequestValidator;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
+
+import java.text.MessageFormat;
 
 public class UserTnCRequestValidator extends BaseRequestValidator {
 
@@ -12,5 +17,14 @@ public class UserTnCRequestValidator extends BaseRequestValidator {
         (String) request.get(JsonKey.VERSION),
         ResponseCode.mandatoryParamsMissing,
         JsonKey.VERSION);
+
+    //When managedProfile terms and conditions accepted, pick userId from request
+    String requestUserId = (String) request.getRequest().get(JsonKey.USER_ID);
+    if (StringUtils.isNotBlank(requestUserId) && !ProjectUtil.validateUUID(requestUserId)){
+      throw new ProjectCommonException(
+              ResponseCode.invalidPropertyError.getErrorCode(),
+              MessageFormat.format(ResponseCode.invalidPropertyError.getErrorMessage(), JsonKey.USER_ID),
+              ResponseCode.CLIENT_ERROR.getResponseCode());
+    }
   }
 }
