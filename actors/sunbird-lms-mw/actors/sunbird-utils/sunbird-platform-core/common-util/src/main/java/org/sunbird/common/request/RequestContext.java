906619a59efe8f3f542c3a19ef4e1e753e3afb2e
AMIT KUMAR <amit.kumar@tarento.com>
2020-08-09 07:45:27 +0530

    Issue #TG-426 feat: changed method signature of cassandra and fixed test cases

--- actors/sunbird-lms-mw/actors/sunbird-utils/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/request/RequestContext.java
+++ actors/sunbird-lms-mw/actors/sunbird-utils/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/request/RequestContext.java
@@ -1,25 +1,25 @@
 package org.sunbird.common.request;
 
-public class RequestContext {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
 
-  private String traceId;
+public class RequestContext {
+  private String uid;
+  private String did;
+  private String sid;
+  private String appId;
+  private String appVer;
+  private String reqId;
   private String logLevel;
   private String actorOperation;
+  private final String pid = "learner-service";
 
-  public RequestContext() {}
-
-  public RequestContext(String traceId, String logLevel, String actorOperation) {
-    this.traceId = traceId;
-    this.logLevel = logLevel;
-    this.actorOperation = actorOperation;
+  public String getReqId() {
+    return reqId;
   }
 
-  public String getTraceId() {
-    return traceId;
-  }
-
-  public void setTraceId(String traceId) {
-    this.traceId = traceId;
+  public void setReqId(String reqId) {
+    this.reqId = reqId;
   }
 
   public String getLogLevel() {
@@ -36,5 +36,53 @@ public class RequestContext {
 
   public void setActorOperation(String actorOperation) {
     this.actorOperation = actorOperation;
+  }
+
+  public String getUid() {
+    return uid;
+  }
+
+  public void setUid(String uid) {
+    this.uid = uid;
+  }
+
+  public String getDid() {
+    return did;
+  }
+
+  public void setDid(String did) {
+    this.did = did;
+  }
+
+  public String getSid() {
+    return sid;
+  }
+
+  public void setSid(String sid) {
+    this.sid = sid;
+  }
+
+  public String getAppId() {
+    return appId;
+  }
+
+  public void setAppId(String appId) {
+    this.appId = appId;
+  }
+
+  public String getAppVer() {
+    return appVer;
+  }
+
+  public void setAppVer(String appVer) {
+    this.appVer = appVer;
+  }
+
+  public Map<String, Object> toMap() throws Exception {
+    ObjectMapper mapper = new ObjectMapper();
+    Map<String, Object> reqMap = mapper.convertValue(this, Map.class);
+    reqMap.remove("logLevel");
+    reqMap.keySet().removeIf(key -> null == reqMap.get(key));
+    return reqMap;
   }
 }
