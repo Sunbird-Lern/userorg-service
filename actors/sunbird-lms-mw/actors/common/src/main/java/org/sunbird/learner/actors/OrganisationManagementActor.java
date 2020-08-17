14615d0e358267ee55f6e86fba1418e134e330da
satish Nirmalkar <satishkumar.nirmalkar@tarento.com>
2020-08-17 19:23:05 +0530

    SB-200253 fix test case

--- actors/sunbird-lms-mw/actors/common/src/main/java/org/sunbird/learner/actors/OrganisationManagementActor.java
+++ actors/sunbird-lms-mw/actors/common/src/main/java/org/sunbird/learner/actors/OrganisationManagementActor.java
@@ -1343,8 +1343,7 @@ public class OrganisationManagementActor extends BaseActor {
     } else if (StringUtils.isNotBlank((String) data.get(JsonKey.USER_EXTERNAL_ID))
         && StringUtils.isNotBlank((String) data.get(JsonKey.USER_PROVIDER))
         && StringUtils.isNotBlank((String) data.get(JsonKey.USER_ID_TYPE))) {
-
-      /* Depreciated: No more externalId is stored as an encrypted format
+      // not used any more as externalids are not store in encrypted format now
       requestDbMap.put(JsonKey.PROVIDER, data.get(JsonKey.USER_PROVIDER));
       requestDbMap.put(JsonKey.ID_TYPE, data.get(JsonKey.USER_ID_TYPE));
       requestDbMap.put(
@@ -1353,7 +1352,7 @@ public class OrganisationManagementActor extends BaseActor {
       result =
           cassandraOperation.getRecordsByProperties(
               JsonKey.SUNBIRD, JsonKey.USR_EXT_IDNT_TABLE, requestDbMap);
-       */
+
       fromExtId = true;
     } else {
       usrDbInfo = Util.dbInfoMap.get(JsonKey.USER_DB);
