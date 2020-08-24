768b5a0c21afe25466280c78bc5072dfa44b728a
AMIT KUMAR <amit.kumar@tarento.com>
2020-08-24 06:51:30 +0530

    Issue #TG-426 feat:change for request treace

--- actors/sunbird-lms-mw/actors/sunbird-utils/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/azure/AzureCloudService.java
+++ actors/sunbird-lms-mw/actors/sunbird-utils/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/azure/AzureCloudService.java
@@ -10,32 +10,32 @@ public class AzureCloudService implements CloudService {
   @Override
   public String uploadFile(
       String containerName, String fileName, String fileLocation, RequestContext context) {
-    return AzureFileUtility.uploadFile(containerName, fileName, fileLocation);
+    return AzureFileUtility.uploadFile(containerName, fileName, fileLocation, context);
   }
 
   @Override
   public boolean downLoadFile(
       String containerName, String fileName, String downloadFolder, RequestContext context) {
-    return AzureFileUtility.downloadFile(containerName, fileName, downloadFolder);
+    return AzureFileUtility.downloadFile(containerName, fileName, downloadFolder, context);
   }
 
   @Override
   public String uploadFile(String containerName, File file, RequestContext context) {
-    return AzureFileUtility.uploadFile(containerName, file);
+    return AzureFileUtility.uploadFile(containerName, file, context);
   }
 
   @Override
   public boolean deleteFile(String containerName, String fileName, RequestContext context) {
-    return AzureFileUtility.deleteFile(containerName, fileName);
+    return AzureFileUtility.deleteFile(containerName, fileName, context);
   }
 
   @Override
   public List<String> listAllFiles(String containerName, RequestContext context) {
-    return AzureFileUtility.listAllBlobbs(containerName);
+    return AzureFileUtility.listAllBlobbs(containerName, context);
   }
 
   @Override
   public boolean deleteContainer(String containerName, RequestContext context) {
-    return AzureFileUtility.deleteContainer(containerName);
+    return AzureFileUtility.deleteContainer(containerName, context);
   }
 }
