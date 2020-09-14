565690723afed3f90a0b2bba83f5bd8842521dde
aishwa8141 <a.aish1997@gmail.com>
2020-09-14 10:05:12 +0530

    SB-20561: added test case for generate otp

--- actors/sunbird-lms-mw/actors/common/src/test/java/org/sunbird/learner/util/OTPUtilTest.java
+++ actors/sunbird-lms-mw/actors/common/src/test/java/org/sunbird/learner/util/OTPUtilTest.java
@@ -6,6 +6,13 @@ import org.junit.Test;
 public class OTPUtilTest {
 
   @Test
+  public void otpLengthTest() {
+    String code = OTPUtil.generateOTP();
+    code = OTPUtil.checkOtpCodeLength(code);
+    Assert.assertTrue(code.length() >= 4);
+  }
+
+  @Test
   public void generateOtpTest() {
     for (int i = 0; i < 1000; i++) {
       String code = OTPUtil.generateOTP();
