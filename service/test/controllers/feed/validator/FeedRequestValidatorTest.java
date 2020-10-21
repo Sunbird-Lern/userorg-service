fdf4a41354a91899eab466b745948f859fee960d
Hari-stackroute <40484996+Hari-stackroute@users.noreply.github.com>
2020-10-21 11:07:46 +0530

    SB-20928 create user-feed api (#569)

--- service/test/controllers/feed/validator/FeedRequestValidatorTest.java
+++ service/test/controllers/feed/validator/FeedRequestValidatorTest.java
@@ -1,8 +1,13 @@
 package controllers.feed.validator;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 import org.sunbird.common.exception.ProjectCommonException;
+import org.sunbird.common.models.util.ActorOperations;
+import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.request.Request;
 
 public class FeedRequestValidatorTest {
 
@@ -14,5 +19,18 @@ public class FeedRequestValidatorTest {
   @Test(expected = ProjectCommonException.class)
   public void userIdValidationTestFailure() {
     Assert.assertTrue(FeedRequestValidator.userIdValidation("123-456-7890", "123-456-789"));
+  }
+
+  @Test(expected = ProjectCommonException.class)
+  public void validateFeedRequestTestFailure() {
+    Request reqObj = new Request();
+    Map<String, Object> requestMap = new HashMap<>();
+    Map<String, Object> dataMap = new HashMap<>();
+    reqObj.setOperation(ActorOperations.CREATE_USER_FEED.getValue());
+    requestMap.put(JsonKey.USER_ID, "someUserId");
+    requestMap.put(JsonKey.CATEGORY, "someCategory");
+    requestMap.put(JsonKey.DATA, dataMap);
+    reqObj.setRequest(requestMap);
+    FeedRequestValidator.validateFeedRequest(reqObj);
   }
 }
