c30c5816be26329ff01bf1a34e77e2835fef2da7
AMIT KUMAR <amit.kumar@tarento.com>
2020-12-09 14:52:12 +0530

    Issue #SC-2129 feat:Removed lookup code to service layer

--- actors/sunbird-lms-mw/actors/user/src/main/java/org/sunbird/user/actors/KeycloakUserLookupActor.java
+++ actors/sunbird-lms-mw/actors/user/src/main/java/org/sunbird/user/actors/KeycloakUserLookupActor.java
@@ -1,6 +1,5 @@
 package org.sunbird.user.actors;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.sunbird.actor.core.BaseActor;
@@ -8,10 +7,8 @@ import org.sunbird.actor.router.ActorConfig;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.request.Request;
-import org.sunbird.learner.util.UserUtility;
-import org.sunbird.user.dao.UserDao;
-import org.sunbird.user.dao.impl.UserDaoImpl;
-import org.sunbird.user.util.UserLookUp;
+import org.sunbird.user.service.UserService;
+import org.sunbird.user.service.impl.UserServiceImpl;
 
 @ActorConfig(
   tasks = {"userSearch"},
@@ -26,34 +23,13 @@ public class KeycloakUserLookupActor extends BaseActor {
   }
 
   private void searchUser(Request request) {
-    UserDao userDao = UserDaoImpl.getInstance();
+    UserService userService = UserServiceImpl.getInstance();
     Map<String, Object> reqMap = request.getRequest();
     List<String> fields = (List<String>) reqMap.get(JsonKey.FIELDS);
     String key = (String) reqMap.get(JsonKey.KEY);
     String value = (String) reqMap.get(JsonKey.VALUE);
-    Response response;
-    if (JsonKey.ID.equalsIgnoreCase(key)) {
-      List<String> ids = new ArrayList<>(2);
-      ids.add(value);
-      response = userDao.getUserPropertiesById(ids, fields, request.getRequestContext());
-    } else {
-      UserLookUp userLookUp = new UserLookUp();
-      List<Map<String, Object>> records =
-          userLookUp.getRecordByType(
-              key.toLowerCase(), value.toLowerCase(), true, request.getRequestContext());
-      List<String> ids = new ArrayList<>();
-      records
-          .stream()
-          .forEach(
-              record -> {
-                ids.add((String) record.get(JsonKey.USER_ID));
-              });
-      response = userDao.getUserPropertiesById(ids, fields, request.getRequestContext());
-    }
-    for (Map<String, Object> userMap :
-        (List<Map<String, Object>>) response.getResult().get(JsonKey.RESPONSE)) {
-      UserUtility.decryptUserDataFrmES(userMap);
-    }
+    Response response =
+        userService.userLookUpByKey(key, value, fields, request.getRequestContext());
     sender().tell(response, self());
   }
 }
