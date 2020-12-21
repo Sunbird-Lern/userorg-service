8bf6f880d4b6c0e39f67c3c7a94985eac3122bde
satish Nirmalkar <satishkumar.nirmalkar@tarento.com>
2020-12-21 15:57:30 +0530

    SB-21901 Adding support for new user Type and subTypeS

--- actors/sunbird-lms-mw/actors/common/src/main/java/org/sunbird/validator/user/UserBulkUploadRequestValidator.java
+++ actors/sunbird-lms-mw/actors/common/src/main/java/org/sunbird/validator/user/UserBulkUploadRequestValidator.java
@@ -1,14 +1,12 @@
 package org.sunbird.validator.user;
 
 import java.text.MessageFormat;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.commons.lang3.StringUtils;
 import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.util.JsonKey;
-import org.sunbird.common.models.util.StringFormatter;
 import org.sunbird.common.responsecode.ResponseCode;
 import org.sunbird.models.user.UserType;
 
@@ -18,15 +16,39 @@ public class UserBulkUploadRequestValidator {
 
   public static void validateUserBulkUploadRequest(Map<String, Object> userMap) {
     validateUserType(userMap);
-    validateOrganisationId(userMap);
+    /*
+        validateOrganisationId(userMap);
+    */
   }
 
   public static void validateUserType(Map<String, Object> userMap) {
+
+    // TODO get list of userType from cache handler
+    // TODO: start
     List<String> userTypes =
         Stream.of(UserType.values()).map(UserType::getTypeName).collect(Collectors.toList());
+    // Get list of user Sub Types belongs a userType
+    Map<String, List<String>> userSubTypesMap = new HashMap<>();
+    // TODO: Harcoding values to be removed
+    List<String> userSubTypeList = Arrays.asList("BRC", "DAO");
+    userSubTypesMap.put(UserType.ADMINISTRATOR.getTypeName(), userSubTypeList);
+    userSubTypesMap.put(UserType.TEACHER.getTypeName(), Arrays.asList());
+    // TODO: END
+
     String userType = (String) userMap.get(JsonKey.USER_TYPE);
-    if (userTypes.contains(userType.trim().toUpperCase())) {
-      userMap.put(JsonKey.USER_TYPE, userType.trim().toUpperCase());
+    if (userTypes.contains(userType)) {
+      userMap.put(JsonKey.USER_TYPE, userType);
+      String userSubType = (String) userMap.get(JsonKey.USER_SUB_TYPE);
+      if (StringUtils.isNotBlank(userSubType)
+          && !userSubTypesMap.get(userType).contains(userSubType)) {
+        ProjectCommonException.throwClientErrorException(
+            ResponseCode.invalidValue,
+            MessageFormat.format(
+                ResponseCode.invalidValue.getErrorMessage(),
+                JsonKey.USER_SUB_TYPE,
+                userSubType,
+                userSubTypesMap.get(userType)));
+      }
     } else {
       ProjectCommonException.throwClientErrorException(
           ResponseCode.invalidValue,
@@ -35,7 +57,7 @@ public class UserBulkUploadRequestValidator {
     }
   }
 
-  public static void validateOrganisationId(Map<String, Object> userMap) {
+  /*public static void validateOrganisationId(Map<String, Object> userMap) {
     String userType = (String) userMap.get(JsonKey.USER_TYPE);
     if (UserType.TEACHER.name().equalsIgnoreCase(userType.trim().toUpperCase())
         && (StringUtils.isBlank((String) userMap.get(JsonKey.ORG_ID))
@@ -46,5 +68,5 @@ public class UserBulkUploadRequestValidator {
               ResponseCode.mandatoryParamsMissing.getErrorMessage(),
               (StringFormatter.joinByOr(JsonKey.ORG_ID, JsonKey.ORG_EXTERNAL_ID))));
     }
-  }
+  }*/
 }
