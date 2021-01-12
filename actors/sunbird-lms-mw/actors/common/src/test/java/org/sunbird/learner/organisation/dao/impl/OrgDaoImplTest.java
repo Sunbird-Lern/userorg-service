99d28fb221cd8d10f60013510600a5218567381a
AMIT KUMAR <amit.kumar@tarento.com>
2021-01-12 10:54:52 +0530

    Issue #SC-000 feat:orgdaoimpl test fix

--- actors/sunbird-lms-mw/actors/common/src/test/java/org/sunbird/learner/organisation/dao/impl/OrgDaoImplTest.java
+++ actors/sunbird-lms-mw/actors/common/src/test/java/org/sunbird/learner/organisation/dao/impl/OrgDaoImplTest.java
@@ -112,9 +112,13 @@ public class OrgDaoImplTest {
   @Test
   public void testGetOrgByExternalId() {
     try {
+      PowerMockito.mockStatic(EsClientFactory.class);
+      ElasticSearchService esService = mock(ElasticSearchRestHighImpl.class);
+      when(EsClientFactory.getInstance(Mockito.anyString())).thenReturn(esService);
       setEsSearchResponse(getOrgSearchResponseMap());
       OrgDao orgDao = OrgDaoImpl.getInstance();
       Map<String, Object> resp = orgDao.esGetOrgByExternalId("1234567890", "provider", null);
+      System.out.println("testGetOrgByExternalId resp map ::::::::::::::::::::" + resp);
       Assert.assertNotNull(resp);
 
     } catch (Exception e) {
@@ -125,9 +129,14 @@ public class OrgDaoImplTest {
   @Test
   public void testGetOrgByExternalIdWithEmptyResponse() {
     try {
+      PowerMockito.mockStatic(EsClientFactory.class);
+      ElasticSearchService esService = mock(ElasticSearchRestHighImpl.class);
+      when(EsClientFactory.getInstance(Mockito.anyString())).thenReturn(esService);
       setEsSearchResponse(getOrgSearchEmptyResponseMap());
       OrgDao orgDao = OrgDaoImpl.getInstance();
       Map<String, Object> resp = orgDao.esGetOrgByExternalId("1234567890", "provider", null);
+      System.out.println(
+          "testGetOrgByExternalIdWithEmptyResponse resp map ::::::::::::::::::::" + resp);
       Assert.assertNotNull(resp);
 
     } catch (Exception e) {
