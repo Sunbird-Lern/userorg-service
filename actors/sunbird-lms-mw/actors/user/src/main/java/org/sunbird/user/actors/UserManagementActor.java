e8ca7d4634b579ca3f20c3a092ac8b9e4e9084bc
satish Nirmalkar <satishkumar.nirmalkar@tarento.com>
2021-01-28 12:28:00 +0530

    SB-22771 Fix update issue due to non availability

--- actors/sunbird-lms-mw/actors/user/src/main/java/org/sunbird/user/actors/UserManagementActor.java
+++ actors/sunbird-lms-mw/actors/user/src/main/java/org/sunbird/user/actors/UserManagementActor.java
@@ -1454,6 +1454,9 @@ public class UserManagementActor extends BaseActor {
       if (CollectionUtils.isEmpty(locationCodes)) {
         // Get location code from user records locations Ids
         List<String> locationIds = (List<String>) userDbRecord.get(JsonKey.LOCATION_IDS);
+        logger.info(
+            String.format(
+                "Locations for userId:%s is:%s", userMap.get(JsonKey.USER_ID), locationIds));
         if (CollectionUtils.isNotEmpty(locationIds)) {
           locations =
               locationClient.getLocationByIds(
@@ -1475,6 +1478,7 @@ public class UserManagementActor extends BaseActor {
             stateCode = location.getCode();
           }
         }
+        logger.info(String.format("Validating UserType for state code:%s", stateCode));
         if (StringUtils.isNotBlank(stateCode)) {
           // Validate UserType and UserSubType configure based on user state config else user
           // default config
@@ -1482,6 +1486,8 @@ public class UserManagementActor extends BaseActor {
         }
       } else {
         // If location is null or empty .Vlidate with default config
+        logger.info(
+            String.format("Validating UserType for state code:%s", JsonKey.DEFAULT_PERSONA));
         validateUserTypeAndSubType(userMap, context, JsonKey.DEFAULT_PERSONA);
       }
     }
