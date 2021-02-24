7d4fbf50a997b0d17707fd6525fbf99b254c5f7c
AMIT KUMAR <amit.kumar@tarento.com>
2021-02-24 10:04:44 +0530

    Issue #SC-2228 feat:Added test case for msg91

--- actors/sunbird-lms-mw/actors/sunbird-utils/sunbird-notification/src/test/java/org/sunbird/notification/utils/DataCacheHandlerTest.java
+++ actors/sunbird-lms-mw/actors/sunbird-utils/sunbird-notification/src/test/java/org/sunbird/notification/utils/DataCacheHandlerTest.java
@@ -7,10 +7,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.commons.collections.CollectionUtils;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -18,7 +19,6 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.sunbird.cassandraimpl.CassandraOperationImpl;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.JsonKey;
-import org.sunbird.common.request.RequestContext;
 import org.sunbird.helper.ServiceFactory;
 
 @RunWith(PowerMockRunner.class)
@@ -35,29 +35,31 @@ import org.sunbird.helper.ServiceFactory;
 })
 public class DataCacheHandlerTest {
 
-  private static CassandraOperationImpl cassandraOperationImpl;
-
-  @Before
-  public void beforeEachTest() {
+  public void before() {
     PowerMockito.mockStatic(ServiceFactory.class);
-    cassandraOperationImpl = mock(CassandraOperationImpl.class);
+    CassandraOperationImpl cassandraOperationImpl = mock(CassandraOperationImpl.class);
     String templateConfig =
-        "[{\\\"OTP to verify your phone number on $installationName is $otp. This is valid for $otpExpiryInMinutes minutes only.\\\":\\\"1\\\"},{\\\"OTP to reset your password on $installationName is $otp. This is valid for $otpExpiryInMinutes minutes only.\\\":\\\"2\\\"},{\\\"Your ward has requested for registration on $installationName using this phone number. Use OTP $otp to agree and create the account. This is valid for $otpExpiryInMinutes minutes only.\\\":\\\"3\\\"}]";
+        "[{\"OTP to verify your phone number on $installationName is $otp. This is valid for $otpExpiryInMinutes minutes only.\":\"1\"},{\"OTP to reset your password on $installationName is $otp. This is valid for $otpExpiryInMinutes minutes only.\":\"2\"},{\"Your ward has requested for registration on $installationName using this phone number. Use OTP $otp to agree and create the account. This is valid for $otpExpiryInMinutes minutes only.\":\"3\"}]";
     Response response = new Response();
     List<Map<String, Object>> responseList = new ArrayList<>();
     Map<String, Object> result = new HashMap<>();
-    result.put(JsonKey.SMS_TEMPLATE_CONFIG, templateConfig);
+    result.put(JsonKey.FIELD, JsonKey.SMS_TEMPLATE_CONFIG);
+    result.put(JsonKey.VALUE, templateConfig);
     responseList.add(result);
     response.getResult().put(JsonKey.RESPONSE, responseList);
     when(ServiceFactory.getInstance()).thenReturn(cassandraOperationImpl);
     when(cassandraOperationImpl.getAllRecords(
-            JsonKey.SUNBIRD, JsonKey.SYSTEM_SETTINGS_DB, new RequestContext()))
+            Mockito.anyString(), Mockito.anyString(), Mockito.any()))
         .thenReturn(response);
   }
 
   @Test
   public void testDataSettings() {
+    before();
+    DataCacheHandler handler = new DataCacheHandler();
+    handler.run();
     List<Map<String, String>> settings = DataCacheHandler.getSmsTemplateConfigList();
     Assert.assertNotNull(settings);
+    Assert.assertTrue(CollectionUtils.isNotEmpty(settings));
   }
 }
