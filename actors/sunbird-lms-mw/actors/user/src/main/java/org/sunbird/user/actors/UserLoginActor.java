30e6ba4989021865d270610f63d38719ceb3777a
AMIT KUMAR <amit.kumar@tarento.com>
2021-03-12 17:23:18 +0530

    Issue #SC-2259 feat:Changed login api to return 200Ok always

--- actors/sunbird-lms-mw/actors/user/src/main/java/org/sunbird/user/actors/UserLoginActor.java
+++ actors/sunbird-lms-mw/actors/user/src/main/java/org/sunbird/user/actors/UserLoginActor.java
@@ -3,12 +3,7 @@ package org.sunbird.user.actors;
 import org.sunbird.actor.router.ActorConfig;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.JsonKey;
-import org.sunbird.common.models.util.PropertiesCache;
-import org.sunbird.common.models.util.TelemetryEnvKey;
 import org.sunbird.common.request.Request;
-import org.sunbird.learner.util.Util;
-import org.sunbird.services.sso.SSOManager;
-import org.sunbird.services.sso.SSOServiceFactory;
 
 @ActorConfig(
   tasks = {"userCurrentLogin"},
@@ -17,31 +12,9 @@ import org.sunbird.services.sso.SSOServiceFactory;
 public class UserLoginActor extends UserBaseActor {
 
   @Override
-  public void onReceive(Request request) throws Throwable {
-    Util.initializeContext(request, TelemetryEnvKey.USER);
-    String operation = request.getOperation();
-
-    if (operation.equalsIgnoreCase("userCurrentLogin")) {
-      updateUserLoginTime(request);
-    } else {
-      onReceiveUnsupportedOperation("UserLoginActor");
-    }
-  }
-
-  /**
-   * Updates user's current login time in Keycloak.
-   *
-   * @param actorMessage Request containing user ID.
-   */
-  private void updateUserLoginTime(Request actorMessage) {
-    String userId = (String) actorMessage.getRequest().get(JsonKey.USER_ID);
+  public void onReceive(Request request) {
     Response response = new Response();
     response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
     sender().tell(response, self());
-    if (Boolean.parseBoolean(PropertiesCache.getInstance().getProperty(JsonKey.IS_SSO_ENABLED))) {
-      SSOManager ssoManager = SSOServiceFactory.getInstance();
-      boolean loginTimeResponse = ssoManager.addUserLoginTime(userId);
-      logger.info("UserLoginActor:updateUserLoginTime: keycloak response = " + loginTimeResponse);
-    }
   }
 }
