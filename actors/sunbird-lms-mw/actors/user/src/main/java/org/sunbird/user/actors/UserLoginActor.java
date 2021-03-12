0e974912e3ba44ea5c845ce593b0cac278e8e7e0
AMIT KUMAR <amit.kumar@tarento.com>
2021-03-12 17:24:51 +0530

    Issue #SC-2259 feat:Changed login api to return 200Ok always

--- actors/sunbird-lms-mw/actors/user/src/main/java/org/sunbird/user/actors/UserLoginActor.java
+++ actors/sunbird-lms-mw/actors/user/src/main/java/org/sunbird/user/actors/UserLoginActor.java
@@ -13,6 +13,7 @@ public class UserLoginActor extends UserBaseActor {
 
   @Override
   public void onReceive(Request request) {
+    // Always return 200 and in future release will depricate these apis
     Response response = new Response();
     response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
     sender().tell(response, self());
