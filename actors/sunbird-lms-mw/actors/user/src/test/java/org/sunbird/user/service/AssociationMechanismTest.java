0815d581cc55502d13e270eae4e442677e64bd95
fathimasherin <fathimasherin362@gmail.com>
2021-04-27 12:22:24 +0530

    added testcases

--- actors/sunbird-lms-mw/actors/user/src/test/java/org/sunbird/user/service/AssociationMechanismTest.java
+++ actors/sunbird-lms-mw/actors/user/src/test/java/org/sunbird/user/service/AssociationMechanismTest.java
@@ -17,4 +17,14 @@ public class AssociationMechanismTest {
     Assert.assertEquals(2, AssociationMechanism.SELF_DECLARATION);
     Assert.assertEquals(4, AssociationMechanism.SYSTEM_UPLOAD);
   }
+
+  @Test
+  public void isAssociationTypeTest() {
+    AssociationMechanism associationMechanism = new AssociationMechanism();
+    associationMechanism.setAssociationType(1);
+    associationMechanism.setAssociationName("SSO");
+    String name = associationMechanism.getAssociationName();
+    Assert.assertEquals("SSO", name);
+    Assert.assertTrue(associationMechanism.isAssociationType(AssociationMechanism.SSO));
+  }
 }
