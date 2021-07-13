2df6be3cb6c149676c08f1fdd38574250cc01f3b
AMIT KUMAR <amit.kumar@tarento.com>
2021-07-13 10:26:37 +0530

    Issue #SC-2185 feat:project restructuring

--- actors/sunbird-lms-mw/actors/common/src/test/java/org/sunbird/validator/user/UserRequestValidatorTest.java
+++ actors/sunbird-lms-mw/actors/common/src/test/java/org/sunbird/validator/user/UserRequestValidatorTest.java
@@ -12,13 +12,13 @@ import java.util.Map;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.sunbird.common.exception.ProjectCommonException;
-import org.sunbird.common.models.util.JsonKey;
-import org.sunbird.common.request.Request;
-import org.sunbird.common.request.RequestContext;
 import org.sunbird.common.request.RequestValidator;
-import org.sunbird.common.responsecode.ResponseCode;
-import org.sunbird.common.responsecode.ResponseMessage;
+import org.sunbird.exception.ProjectCommonException;
+import org.sunbird.exception.ResponseCode;
+import org.sunbird.exception.ResponseMessage;
+import org.sunbird.keys.JsonKey;
+import org.sunbird.request.Request;
+import org.sunbird.request.RequestContext;
 
 public class UserRequestValidatorTest {
 
