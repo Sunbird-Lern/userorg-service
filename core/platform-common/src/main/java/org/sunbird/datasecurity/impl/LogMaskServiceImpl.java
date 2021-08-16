cbb0bb62e03b04a4cbd5879bd4651657f6ed6665
AMIT KUMAR <amit.kumar@tarento.com>
2021-08-16 11:46:16 +0530

    DD-1609 Masked PII data in logs (#909)
    * Issue #DD-1609 feat:Masked PII data in logs

--- core/platform-common/src/main/java/org/sunbird/datasecurity/impl/LogMaskServiceImpl.java
+++ core/platform-common/src/main/java/org/sunbird/datasecurity/impl/LogMaskServiceImpl.java
@@ -7,23 +7,19 @@ public class LogMaskServiceImpl implements DataMaskingService {
    * Mask an email
    *
    * @param email
-   * @return the first 4 or 2 characters in plain and masks the rest. The domain is still in plain
+   * @return the first 2 characters in plain and masks the rest. The domain is still in plain
    */
   public String maskEmail(String email) {
-    if (email.indexOf("@") > 4) {
-      return email.replaceAll("(^[^@]{4}|(?!^)\\G)[^@]", "$1*");
-    } else {
-      return email.replaceAll("(^[^@]{2}|(?!^)\\G)[^@]", "$1*");
-    }
+    return email.replaceAll("(^[^@]{2}|(?!^)\\G)[^@]", "$1*");
   }
 
   /**
    * Mask a phone number
    *
    * @param phone
-   * @return a string with the last digit masked
+   * @return a string with the last 5 digit masked
    */
   public String maskPhone(String phone) {
-    return phone.replaceAll("(^[^*]{9}|(?!^)\\G)[^*]", "$1*");
+    return phone.replaceAll("(^[^*]{5}|(?!^)\\G)[^*]", "$1*");
   }
 }
