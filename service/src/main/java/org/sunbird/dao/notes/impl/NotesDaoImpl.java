95a319730101243fd53f7bfd72fc06eecb0435a2
Amit <amit@Navneets-MacBook-Pro.local>
2021-09-08 11:02:31 +0530

    Issue #SC-2302 feat:Refactored Notes API

---
+++ service/src/main/java/org/sunbird/dao/notes/impl/NotesDaoImpl.java
@@ -0,0 +1,122 @@
+package org.sunbird.dao.notes.impl;
+
+import java.util.*;
+import org.apache.commons.lang3.StringUtils;
+import org.sunbird.cassandra.CassandraOperation;
+import org.sunbird.common.ElasticSearchHelper;
+import org.sunbird.common.factory.EsClientFactory;
+import org.sunbird.common.inf.ElasticSearchService;
+import org.sunbird.dao.notes.NotesDao;
+import org.sunbird.dto.SearchDTO;
+import org.sunbird.helper.ServiceFactory;
+import org.sunbird.keys.JsonKey;
+import org.sunbird.logging.LoggerUtil;
+import org.sunbird.request.RequestContext;
+import org.sunbird.response.Response;
+import org.sunbird.util.ProjectUtil;
+import scala.concurrent.Future;
+
+public class NotesDaoImpl implements NotesDao {
+  public LoggerUtil logger = new LoggerUtil(NotesDaoImpl.class);
+  private CassandraOperation cassandraOperation = ServiceFactory.getInstance();
+  private ElasticSearchService esService = EsClientFactory.getInstance(JsonKey.REST);
+
+  private static final String KEYSPACE_NAME = JsonKey.SUNBIRD;
+  private static final String TABLE_NAME = "user_notes";
+
+  private static NotesDao notesDao;
+
+  public static NotesDao getInstance() {
+    if (notesDao == null) {
+      notesDao = new NotesDaoImpl();
+    }
+    return notesDao;
+  }
+
+  @Override
+  public Response createNote(Map<String, Object> request, RequestContext context) {
+    Response response =
+        cassandraOperation.insertRecord(KEYSPACE_NAME, TABLE_NAME, request, context);
+    String id = (String) request.get(JsonKey.ID);
+    insertDataToElastic(id, request, context);
+    return response;
+  }
+
+  @Override
+  public Response updateNote(Map<String, Object> request, RequestContext context) {
+    Response response =
+        cassandraOperation.updateRecord(KEYSPACE_NAME, TABLE_NAME, request, context);
+    String id = (String) request.get(JsonKey.ID);
+    updateDataToElastic(id, request, context);
+    return response;
+  }
+
+  @Override
+  public Map<String, Object> searchNotes(
+      Map<String, Object> searchQueryMap, RequestContext context) {
+    Map<String, Object> filters = new HashMap<>();
+    if (searchQueryMap.containsKey(JsonKey.FILTERS)) {
+      filters = (Map<String, Object>) searchQueryMap.get(JsonKey.FILTERS);
+    }
+    if (null != searchQueryMap.get(JsonKey.REQUESTED_BY)) {
+      filters.put(JsonKey.USER_ID, searchQueryMap.get(JsonKey.REQUESTED_BY));
+    }
+    filters.put(JsonKey.IS_DELETED, false);
+    searchQueryMap.put(JsonKey.FILTERS, filters);
+    SearchDTO searchDto = ElasticSearchHelper.createSearchDTO(searchQueryMap);
+    List<String> excludedFields = new ArrayList<>();
+    if (null != searchDto.getExcludedFields()) {
+      excludedFields = searchDto.getExcludedFields();
+    }
+    excludedFields.add(JsonKey.IS_DELETED);
+    searchDto.setExcludedFields(excludedFields);
+    Future<Map<String, Object>> resultF =
+        esService.search(searchDto, ProjectUtil.EsType.usernotes.getTypeName(), context);
+    Map<String, Object> result =
+        (Map<String, Object>) ElasticSearchHelper.getResponseFromFuture(resultF);
+    if (result != null) {
+      Object count = result.get(JsonKey.COUNT);
+      Object note = result.get(JsonKey.CONTENT);
+      result = new LinkedHashMap<>();
+      result.put(JsonKey.COUNT, count);
+      result.put(JsonKey.NOTE, note);
+      result.put(JsonKey.CONTENT, note);
+    } else {
+      result = new HashMap<>();
+    }
+    return result;
+  }
+
+  @Override
+  public Map<String, Object> getNoteById(String noteId, RequestContext context) {
+    Future<Map<String, Object>> resultF =
+        esService.getDataByIdentifier(ProjectUtil.EsType.usernotes.getTypeName(), noteId, context);
+    Map<String, Object> result =
+        (Map<String, Object>) ElasticSearchHelper.getResponseFromFuture(resultF);
+    return result;
+  }
+
+  private boolean insertDataToElastic(
+      String identifier, Map<String, Object> data, RequestContext context) {
+    String type = ProjectUtil.EsType.usernotes.getTypeName();
+    Future<String> responseF = esService.save(type, identifier, data, context);
+    String response = (String) ElasticSearchHelper.getResponseFromFuture(responseF);
+    if (!StringUtils.isBlank(response)) {
+      return true;
+    }
+    logger.debug(context, "unable to save the data inside ES with identifier " + identifier);
+    return false;
+  }
+
+  private boolean updateDataToElastic(
+      String identifier, Map<String, Object> data, RequestContext context) {
+    String type = ProjectUtil.EsType.usernotes.getTypeName();
+    Future<Boolean> responseF = esService.update(type, identifier, data, context);
+    boolean response = (boolean) ElasticSearchHelper.getResponseFromFuture(responseF);
+    if (response) {
+      return true;
+    }
+    logger.debug(context, "unable to save the data to ES with identifier " + identifier);
+    return false;
+  }
+}
---
+++ service/src/main/java/org/sunbird/service/notes/NotesService.java
@@ -0,0 +1,115 @@
+package org.sunbird.service.notes;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.sunbird.dao.notes.NotesDao;
+import org.sunbird.dao.notes.impl.NotesDaoImpl;
+import org.sunbird.exception.ProjectCommonException;
+import org.sunbird.exception.ResponseCode;
+import org.sunbird.keys.JsonKey;
+import org.sunbird.request.Request;
+import org.sunbird.request.RequestContext;
+import org.sunbird.response.Response;
+import org.sunbird.service.user.UserService;
+import org.sunbird.service.user.impl.UserServiceImpl;
+import org.sunbird.util.ProjectUtil;
+
+public class NotesService {
+
+  private NotesDao notesDao = NotesDaoImpl.getInstance();
+  private UserService userService = UserServiceImpl.getInstance();
+
+  public Response createNote(Request request) {
+    Map<String, Object> req = request.getRequest();
+    String uniqueId = ProjectUtil.getUniqueIdFromTimestamp(request.getEnv());
+    req.put(JsonKey.ID, uniqueId);
+    req.put(JsonKey.CREATED_DATE, ProjectUtil.getFormattedDate());
+    req.put(JsonKey.UPDATED_DATE, ProjectUtil.getFormattedDate());
+    String updatedBy = (String) request.getRequest().get(JsonKey.REQUESTED_BY);
+    if (StringUtils.isNotBlank(updatedBy)) {
+      req.put(JsonKey.CREATED_BY, updatedBy);
+      req.put(JsonKey.UPDATED_BY, updatedBy);
+    }
+    req.put(JsonKey.IS_DELETED, false);
+    Response result = notesDao.createNote(req, request.getRequestContext());
+    result.getResult().put(JsonKey.ID, uniqueId);
+    result.getResult().remove(JsonKey.RESPONSE);
+    return result;
+  }
+
+  public Response updateNote(Request request) {
+    String noteId = (String) request.getContext().get(JsonKey.NOTE_ID);
+    String userId = (String) request.getContext().get(JsonKey.REQUESTED_BY);
+    Map<String, Object> req = request.getRequest();
+    req.remove(JsonKey.USER_ID);
+    req.remove(JsonKey.COURSE_ID);
+    req.remove(JsonKey.CONTENT_ID);
+    req.remove(JsonKey.IS_DELETED);
+    req.remove(JsonKey.NOTE_ID);
+    req.put(JsonKey.ID, noteId);
+    req.put(JsonKey.UPDATED_BY, userId);
+    req.put(JsonKey.UPDATED_DATE, ProjectUtil.getFormattedDate());
+    Response result = notesDao.updateNote(req, request.getRequestContext());
+    result.getResult().put(JsonKey.ID, noteId);
+    result.getResult().remove(JsonKey.RESPONSE);
+    return result;
+  }
+
+  public Map<String, Object> searchNotes(Map<String, Object> searchQuery, RequestContext context) {
+    return notesDao.searchNotes(searchQuery, context);
+  }
+
+  public Response deleteNote(String noteId, String userId, RequestContext context) {
+    Map<String, Object> req = new HashMap<>();
+    req.put(JsonKey.ID, noteId);
+    req.put(JsonKey.IS_DELETED, true);
+    req.put(JsonKey.UPDATED_BY, userId);
+    req.put(JsonKey.UPDATED_DATE, ProjectUtil.getFormattedDate());
+    Response result = notesDao.updateNote(req, context);
+    result.getResult().remove(JsonKey.RESPONSE);
+    return result;
+  }
+
+  public Map<String, Object> getNoteById(String noteId, RequestContext context) {
+    return notesDao.getNoteById(noteId, context);
+  }
+
+  public Boolean validUser(String userId, RequestContext context) {
+    if (!StringUtils.isBlank(userId)) {
+      try {
+        userService.getUserById(userId, context);
+      } catch (Exception ex) {
+        return false;
+      }
+      return true;
+    }
+    return false;
+  }
+
+  public Boolean noteIdExists(String noteId, RequestContext context) {
+    Boolean result = false;
+    Map<String, Object> list = getNoteById(noteId, context);
+    if (!list.isEmpty()) {
+      result = true;
+    }
+    return result;
+  }
+
+  public Boolean validateUserForNoteUpdate(String userId, String noteId, RequestContext context) {
+    Boolean result = false;
+    Map<String, Object> noteData = getNoteById(noteId, context);
+    if (MapUtils.isEmpty(noteData)) return result;
+    if (!StringUtils.isBlank(userId)) {
+      result = true;
+    }
+    if (!userId.equalsIgnoreCase((String) noteData.get(JsonKey.USER_ID))) {
+      throw new ProjectCommonException(
+          ResponseCode.errorForbidden.getErrorCode(),
+          ResponseCode.errorForbidden.getErrorMessage(),
+          ResponseCode.FORBIDDEN.getResponseCode());
+    }
+    return result;
+  }
+}
