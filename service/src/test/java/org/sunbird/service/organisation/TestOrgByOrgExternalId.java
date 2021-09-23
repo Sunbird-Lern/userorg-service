22151707f93a263c86da35cac87c1acda0664615
AMIT KUMAR <amit.kumar@tarento.com>
2021-09-23 16:53:20 +0530

    Fix feedutil (#955)
    * Issue #SC-000 feat: removed org client call
    
    * Issue #SC-000 feat: fixing test case

--- service/src/test/java/org/sunbird/service/organisation/TestOrgByOrgExternalId.java
+++ service/src/test/java/org/sunbird/service/organisation/TestOrgByOrgExternalId.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -46,17 +46,12 @@ import org.sunbird.service.organisation.impl.OrgServiceImpl;
   "org.xml.*"
 })
 public class TestOrgByOrgExternalId {
-  private CassandraOperation cassandraOperation;
 
-  @Before
-  public void setUp() {
+  @BeforeClass
+  public static void setUp() {
     PowerMockito.mockStatic(ServiceFactory.class);
-    cassandraOperation = mock(CassandraOperationImpl.class);
+    CassandraOperation cassandraOperation = mock(CassandraOperationImpl.class);
     PowerMockito.when(ServiceFactory.getInstance()).thenReturn(cassandraOperation);
-  }
-
-  @Test
-  public void testGetOrgByExternalIdAndProvider() {
     PowerMockito.when(
             cassandraOperation.getRecordsByCompositeKey(
                 Mockito.anyString(), Mockito.anyString(), Mockito.anyMap(), Mockito.any()))
@@ -83,11 +78,6 @@ public class TestOrgByOrgExternalId {
                 Mockito.anyList(),
                 Mockito.any(RequestContext.class)))
         .thenReturn(getRecordsByProperty(false));
-
-    OrgService orgService = OrgServiceImpl.getInstance();
-    Map<String, Object> map =
-        orgService.getOrgByExternalIdAndProvider("extId", "provider", new RequestContext());
-    Assert.assertNotNull(map);
   }
 
   @Test
@@ -97,7 +87,15 @@ public class TestOrgByOrgExternalId {
     Assert.assertNotNull(channel);
   }
 
-  private Response getRecordsByProperty(boolean empty) {
+  @Test
+  public void testGetOrgByExternalIdAndProvider() {
+    OrgService orgService = OrgServiceImpl.getInstance();
+    Map<String, Object> map =
+        orgService.getOrgByExternalIdAndProvider("extId", "provider", new RequestContext());
+    Assert.assertNotNull(map);
+  }
+
+  private static Response getRecordsByProperty(boolean empty) {
     Response res = new Response();
     List<Map<String, Object>> list = new ArrayList<>();
     if (!empty) {
