cb459dbc360d44997a5e94b86a7f64767822c9fa
amit-tarento <amit.kumar@tarento.com>
2021-09-28 10:12:51 +0530

    Issue #SC-000 feat: nic integration

--- core/notification-utils/src/test/java/org/sunbird/notification/sms/providerimpl/NICGatewaySmsProviderTest.java
+++ core/notification-utils/src/test/java/org/sunbird/notification/sms/providerimpl/NICGatewaySmsProviderTest.java
@@ -5,7 +5,9 @@ import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -14,10 +16,8 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
@@ -31,7 +31,6 @@ import org.sunbird.notification.utils.SmsTemplateUtil;
 import org.sunbird.request.RequestContext;
 import org.sunbird.util.ProjectUtil;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(PowerMockRunner.class)
 @PowerMockIgnore({"javax.management.*", "javax.net.ssl.*", "javax.security.*"})
 @PrepareForTest({
@@ -50,6 +49,22 @@ public class NICGatewaySmsProviderTest {
     when(ProjectUtil.getConfigValue("sms_gateway_provider")).thenReturn("NIC");
     PowerMockito.mockStatic(URLEncoder.class);
     when(URLEncoder.encode(Mockito.anyString(), Mockito.anyString())).thenReturn("dfgdgfg");
+
+    initMockRulesFor200();
+    PowerMockito.mockStatic(SmsTemplateUtil.class);
+    Map<String, Map<String, String>> template = new HashMap<>();
+    Map<String, String> template1 = new HashMap<>();
+    template1.put(
+        "OTP to verify your phone number on $installationName is $otp. This is valid for $otpExpiryInMinutes minutes only.",
+        "1");
+    template1.put(
+        "OTP to reset your password on $installationName is $otp. This is valid for $otpExpiryInMinutes minutes only.",
+        "2");
+    template1.put(
+        "Your ward has requested for registration on $installationName using this phone number. Use OTP $otp to agree and create the account. This is valid for $otpExpiryInMinutes minutes only.",
+        "3");
+    template.put(JsonKey.NIC, template1);
+    when(SmsTemplateUtil.getSmsTemplateConfigMap()).thenReturn(template);
   }
 
   private void initMockRulesFor200() {
@@ -76,25 +91,30 @@ public class NICGatewaySmsProviderTest {
 
   @Test
   public void testSendSms() {
-    initMockRulesFor200();
-    PowerMockito.mockStatic(SmsTemplateUtil.class);
-    Map<String, Map<String, String>> template = new HashMap<>();
-    Map<String, String> template1 = new HashMap<>();
-    template1.put(
-        "OTP to verify your phone number on $installationName is $otp. This is valid for $otpExpiryInMinutes minutes only.",
-        "1");
-    template1.put(
-        "OTP to reset your password on $installationName is $otp. This is valid for $otpExpiryInMinutes minutes only.",
-        "2");
-    template1.put(
-        "Your ward has requested for registration on $installationName using this phone number. Use OTP $otp to agree and create the account. This is valid for $otpExpiryInMinutes minutes only.",
-        "3");
-    template.put(JsonKey.NIC, template1);
-    when(SmsTemplateUtil.getSmsTemplateConfigMap()).thenReturn(template);
     ISmsProvider iSmsProvider = SMSFactory.getInstance();
     String sms =
         "OTP to reset your password on instance is 456123. This is valid for 30 minutes only.";
     boolean response = iSmsProvider.send("4321111111", sms, new RequestContext());
     Assert.assertFalse(response);
+  }
+
+  @Test
+  public void testSendSms2() {
+    ISmsProvider iSmsProvider = SMSFactory.getInstance();
+    String sms =
+        "OTP to reset your password on instance is 456123. This is valid for 30 minutes only.";
+    boolean response = iSmsProvider.send("4321111111", "91", sms, new RequestContext());
+    Assert.assertFalse(response);
+  }
+
+  @Test
+  public void testSendSms3() {
+    List<String> phones = new ArrayList<>();
+    phones.add("4321111111");
+    ISmsProvider iSmsProvider = SMSFactory.getInstance();
+    String sms =
+        "OTP to reset your password on instance is 456123. This is valid for 30 minutes only.";
+    boolean response = iSmsProvider.send(phones, sms, new RequestContext());
+    Assert.assertTrue(response);
   }
 }
