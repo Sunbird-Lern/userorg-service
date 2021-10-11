0d9c26beae9c0e3cb8726ea4bc5e5bd1536cd5f9
Reshmi V Nair <revas.nair@gmail.com>
2021-10-11 17:27:38 +0530

    SB-26474 Support for multiple Consent Statuses

--- service/src/main/java/org/sunbird/actor/userconsent/UserConsentActor.java
+++ service/src/main/java/org/sunbird/actor/userconsent/UserConsentActor.java
@@ -117,6 +117,7 @@ public class UserConsentActor extends BaseActor {
     List<Map<String, Object>> consentResponseList =
         consentDBList
             .stream()
+            .filter(consents->((String)consents.get(JsonKey.STATUS)).equalsIgnoreCase(JsonKey.ACTIVE))
             .map(
                 consent -> {
                   Map<String, Object> consentRes = new HashMap<String, Object>();
