b9176d831a7655c517f7b05f42793ca50a420ca0
amit-tarento <amit.kumar@tarento.com>
2021-11-03 14:51:28 +0530

    Issue #SB-27483 feat: Removed duplicates from userProfileTypes

--- service/src/test/java/org/sunbird/actor/user/UserUpdateActorTest.java
+++ service/src/test/java/org/sunbird/actor/user/UserUpdateActorTest.java
@@ -245,12 +245,9 @@ public class UserUpdateActorTest extends UserManagementActorTestBase {
     user.put(JsonKey.EMAIL, "username@gmail.com");
     user.put(JsonKey.USERNAME, "username");
     user.put(JsonKey.ROOT_ORG_ID, "rootOrgId");
-    user.put(JsonKey.USER_TYPE, "teacher");
-    user.put(JsonKey.USER_SUB_TYPE, null);
-    user.put(JsonKey.PROFILE_LOCATION, Arrays.asList("anyLocationCodes"));
     Map<String, Object> usertypes = new HashMap<>();
-    usertypes.put("type", "administrator");
-    usertypes.put("subType", "deo");
+    usertypes.put("type", "teacher");
+    usertypes.put("subType", "crc");
     List<Map<String, Object>> userTypeList = new ArrayList<>();
     userTypeList.add(usertypes);
     user.put(JsonKey.PROFILE_USERTYPES, userTypeList);
