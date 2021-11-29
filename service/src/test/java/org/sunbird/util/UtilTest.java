5b7f4b9a500c8254f8f85e327555f20440c67391
Reshmi V Nair <revas.nair@gmail.com>
2021-11-29 14:46:30 +0530

    SB-27815 Terms & policies page throwing up technical error

--- service/src/test/java/org/sunbird/util/UtilTest.java
+++ service/src/test/java/org/sunbird/util/UtilTest.java
@@ -16,6 +16,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.sunbird.cassandra.CassandraOperation;
 import org.sunbird.cassandraimpl.CassandraOperationImpl;
 import org.sunbird.helper.ServiceFactory;
 import org.sunbird.keys.JsonKey;
@@ -25,7 +26,7 @@ import org.sunbird.response.Response;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({
   ServiceFactory.class,
-  CassandraOperationImpl.class
+  CassandraOperation.class
 })
 @PowerMockIgnore({
   "javax.management.*",
@@ -35,13 +36,13 @@ import org.sunbird.response.Response;
   "javax.crypto.*"
 })
 public class UtilTest {
-  private static CassandraOperationImpl cassandraOperationImpl;
+  private static CassandraOperation cassandraOperation;
 
   @Before
   public void beforeEachTest() {
     PowerMockito.mockStatic(ServiceFactory.class);
-    cassandraOperationImpl = mock(CassandraOperationImpl.class);
-    when(ServiceFactory.getInstance()).thenReturn(cassandraOperationImpl);
+    cassandraOperation = mock(CassandraOperation.class);
+    when(ServiceFactory.getInstance()).thenReturn(cassandraOperation);
   }
   @Test
   public void initializeContextTest(){
@@ -54,12 +55,13 @@ public class UtilTest {
     Response response = new Response();
     response.put(JsonKey.RESPONSE, userList);
 
+    when(cassandraOperation.getRecordById(
+            Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.any()))
+            .thenReturn(response);
+
     Request req = new Request();
     req.getContext().put( JsonKey.ACTOR_TYPE, JsonKey.USER);
     req.getContext().put(JsonKey.REQUESTED_BY,"user1");
-    when(cassandraOperationImpl.getRecordById(
-            Mockito.anyString(), Mockito.anyString(), Mockito.anyMap(), Mockito.any()))
-            .thenReturn(response);
     Util.initializeContext(req,null);
   }
 }
