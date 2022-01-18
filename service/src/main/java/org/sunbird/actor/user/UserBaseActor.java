00c405a8cb6e59bda3d31713c1153499876c1a39
amit-tarento <amit.kumar@tarento.com>
2022-01-18 16:21:29 +0530

    Issue #SB-28164 feat:Remove school as location

--- service/src/main/java/org/sunbird/actor/user/UserBaseActor.java
+++ service/src/main/java/org/sunbird/actor/user/UserBaseActor.java
@@ -133,11 +133,7 @@ public abstract class UserBaseActor extends BaseActor {
         if (location.getType().equalsIgnoreCase(JsonKey.STATE)) {
           stateId = location.getId();
         }
-        if (location.getType().equals(JsonKey.LOCATION_TYPE_SCHOOL)) {
-          userRequest.getRequest().put(JsonKey.ORG_EXTERNAL_ID, location.getCode());
-        } else {
-          set.add(location.getCode());
-        }
+        set.add(location.getCode());
       }
       if (StringUtils.isNotBlank((String) userRequest.getRequest().get(JsonKey.ORG_EXTERNAL_ID))) {
         userRequest.getRequest().put(JsonKey.STATE_ID, stateId);
@@ -218,8 +214,13 @@ public abstract class UserBaseActor extends BaseActor {
           (List<Map<String, String>>) userMap.get(JsonKey.PROFILE_LOCATION);
       List<String> locationCodes = null;
       if (CollectionUtils.isNotEmpty(profLocList)) {
-        locationCodes =
-            profLocList.stream().map(m -> m.get(JsonKey.CODE)).collect(Collectors.toList());
+        for (Map<String, String> location : profLocList) {
+          if (JsonKey.LOCATION_TYPE_SCHOOL.equals(location.get(JsonKey.LOCATION_TYPE))) {
+            userMap.put(JsonKey.ORG_EXTERNAL_ID, location.get(JsonKey.CODE));
+          } else {
+            locationCodes.add(location.get(JsonKey.CODE));
+          }
+        }
         userMap.put(JsonKey.LOCATION_CODES, locationCodes);
       }
       userMap.remove(JsonKey.PROFILE_LOCATION);
--- service/src/main/java/org/sunbird/actor/user/UserUpdateActor.java
+++ service/src/main/java/org/sunbird/actor/user/UserUpdateActor.java
@@ -97,6 +97,7 @@ public class UserUpdateActor extends UserBaseActor {
       populateLocationCodesFromProfileLocation(userMap);
     }
     validateAndGetLocationCodes(actorMessage);
+    convertValidatedLocationCodesToIDs(userMap, actorMessage.getRequestContext());
     if (actorMessage.getOperation().equalsIgnoreCase(ActorOperations.UPDATE_USER.getValue())) {
       userMap.remove(JsonKey.PROFILE_USERTYPES);
       userMap.remove(JsonKey.PROFILE_USERTYPE);
@@ -165,7 +166,6 @@ public class UserUpdateActor extends UserBaseActor {
         user, JsonKey.UPDATE, actorMessage.getRequestContext());
     // not allowing user to update the status,provider,userName
     removeFieldsFrmReq(userMap);
-    convertValidatedLocationCodesToIDs(userMap, actorMessage.getRequestContext());
     userMap.put(JsonKey.UPDATED_DATE, ProjectUtil.getFormattedDate());
     if (StringUtils.isBlank(callerId)) {
       userMap.put(JsonKey.UPDATED_BY, actorMessage.getContext().get(JsonKey.REQUESTED_BY));
