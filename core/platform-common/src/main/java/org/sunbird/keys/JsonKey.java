50011c61e7eebca109bc26ba767355c99e9e78b0
amit-tarento <amit.kumar@tarento.com>
2022-09-06 14:20:03 +0530

    Issue #LR-110 feat:csp change for provider

--- core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
+++ core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
@@ -636,7 +636,7 @@ public final class JsonKey {
   public static final String PASSWORD_RESET_LOGIN_PAGE_URL =
       "sunbird_password_reset_login_page_url";
 
-  public static final String DEFAULT_SERVICE_PROVIDER = "default_service_provider";
+  public static final String CLOUD_SERVICE_PROVIDER = "sunbird_cloud_service_provider";
 
   private JsonKey() {}
 }
