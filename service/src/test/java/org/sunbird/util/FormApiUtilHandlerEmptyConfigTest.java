31d9e94e8c45273c0961f26db9cd51bfe7a9b804
bharathwaj.shankar <bharathwaj.shankar@trustrace.com>
2022-12-07 13:06:45 +0530

    LR-242 - UserOrg-Decoupling the external dependencies - modifying new test cases

--- service/src/test/java/org/sunbird/util/FormApiUtilHandlerEmptyConfigTest.java
+++ service/src/test/java/org/sunbird/util/FormApiUtilHandlerEmptyConfigTest.java
@@ -16,6 +16,9 @@ import org.sunbird.http.HttpClientUtil;
 import org.sunbird.keys.JsonKey;
 import org.sunbird.request.RequestContext;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({HttpClientUtil.class, FormApiUtilHandlerEmptyConfigTest.class})
 @PowerMockIgnore({
@@ -42,6 +45,16 @@ public class FormApiUtilHandlerEmptyConfigTest {
               FormApiUtil.getProfileConfig("locationCode", new RequestContext());
       Assert.assertNull(dataConfigMap);
     }
+    else{
+      Map<String, Object>  formData;
+      try {
+        formData = new ObjectMapper()
+                .readValue(ProjectUtil.getConfigValue(JsonKey.USER_PROFILE_CONFIG_MAP), Map.class);
+      } catch (JsonProcessingException e) {
+        throw new RuntimeException(e);
+      }
+      Assert.assertNotNull(formData);
+    }
   }
 
   @Test
@@ -53,6 +66,16 @@ public class FormApiUtilHandlerEmptyConfigTest {
       Map<String, Object> dataConfigMap =
               FormApiUtil.getProfileConfig("locationCode", new RequestContext());
       Assert.assertNull(dataConfigMap);
+    }
+    else{
+      Map<String, Object>  formData;
+      try {
+        formData = new ObjectMapper()
+                .readValue(ProjectUtil.getConfigValue(JsonKey.USER_PROFILE_CONFIG_MAP), Map.class);
+      } catch (JsonProcessingException e) {
+        throw new RuntimeException(e);
+      }
+      Assert.assertNotNull(formData);
     }
   }
 
