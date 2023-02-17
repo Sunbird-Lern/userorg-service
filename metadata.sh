fdf50997eafd536831910ef826f253b0f999932b
anilgupta <ani2824@gmail.com>
2023-02-17 10:06:15 +0530

    Issue #LR-122 chore: Replaced learner with user-org

--- metadata.sh
+++ metadata.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # return version
-echo '{"name":"user_org_service","version":"1.15.0","org":"sunbird","hubuser":"purplesunbird"}'
+echo '{"name":"user-org-service","version":"1.15.0","org":"sunbird","hubuser":"purplesunbird"}'
