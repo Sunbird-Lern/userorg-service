82915113e93c31b74e64e49f89f699e62b830668
Jayaprakash8887 <jayaprakash.n@tarento.com>
2023-08-10 16:25:52 +0530

    Issue #LR-203 debug: Keycloak Service debug (#1217)
    * Issue #LR-203 debug: Keycloak Service debug
    
    * Issue #LR-203 debug: Keycloak Service debug
    
    * Issue #LR-203 debug: Keycloak Service debug
    
    * Issue #LR-203 debug: Keycloak Service debug
    
    ---------
    
    Co-authored-by: Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>

--- core/platform-common/src/main/java/org/sunbird/sso/impl/KeyCloakServiceImpl.java
+++ core/platform-common/src/main/java/org/sunbird/sso/impl/KeyCloakServiceImpl.java
@@ -154,9 +154,17 @@ public class KeyCloakServiceImpl implements SSOManager {
               + keycloak.toString()
               + " || "
               + keycloak.serverInfo());
+      logger.info(
+          context,
+          "makeUserActiveOrInactive: keycloak: "
+              + keycloak.toString()
+              + " || "
+              + keycloak.serverInfo());
       UserResource resource =
           keycloak.realm(KeyCloakConnectionProvider.SSO_REALM).users().get(fedUserId);
+      logger.info("makeUserActiveOrInactive: resource: " + resource.toString());
       UserRepresentation ur = resource.toRepresentation();
+      logger.info("makeUserActiveOrInactive: ur: " + ur.isEnabled());
       ur.setEnabled(status);
       resource.update(ur);
     } catch (Exception e) {
